a198d989b0c835e5a4cc996dec0511c284c8053d
ebisLab <ebaiden@techie.com>
2019-09-03 17:14:27 -0400

    arranged get and post syntax

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ server.use(express.json()); //teaches express how to parse JSON from body
 server.get('/', (req, res) => {
     res.send('info from body');
 });
+
+// //-*-*-*-* GET REQUEST  -*-*-*-*
+server.get('/api/users', (req, res) => {
+    dataB
+        .find()
+        .then(users => res.status(200).json(users)) //200 means good
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: 'The user information could not be retrieved.',
+            });
+        });
+});
+
 // -*-*-*-* POST REQUEST  -*-*-*-*
 server.post('/api/users', (req, res) => {
     //read info sent by client
@@ -52,59 +66,47 @@ server.post('/api/users', (req, res) => {
     // } else {
 }); // (/hubs) is a resource
 
-//-*-*-*-* GET REQUEST  -*-*-*-*
-server.get('/api/users', (req, res) => {
-    const userInfo = req.body;
-    dataB
-        .find() //promise
-        .then(users => {
-            res.status(200).json(users); //200 means good
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: 'The users information could not be retrieved.',
-            });
-        });
-
-    // res.send('info from body');
-    //(rez, res) ->route handler
-    // dataB
-    //     .find() //return a promise.
-    //     .then(dataB => {
-    //         // .json will convert the data passed to JSON
-    //         // also tells the client we're sending JSON through and HTTP header
-    //         res.status(200).json(dataB);
-    //     })
-    //     .catch(err => {
-    //         res.status(500).json({ message: 'error getting the list of hubs' });
-    //     });
-});
-
 // //-*-*-*-* GET REQUEST  -*-*-*-*
 server.get('/api/users', (req, res) => {
-    dataB.find().then(users => res.status(200).json(users));
+    dataB
+        .find()
+        .then(users => res.status(200).json(users)) //200 means good
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: 'The user information could not be retrieved.',
+            });
+        });
 });
 
 // //-*-*-*-* GET REQUEST  -*-*-*-*
 server.get('/api/users/:id', (req, res) => {
     const hubsID = req.params.id;
 
-    dataB.findById(hubsID).then(user => {
-        console.log('user', user);
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(404).json({
-                error: 'The user with the specified ID does not exist.',
-            });
-        }
+    dataB
+        .findById(hubsID)
+        .then(user => {
+            console.log('user', user);
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({
+                    error: 'The user with the specified ID does not exist.',
+                });
+            }
 
-        // else {
-        //     res.status(400).json({
-        //         errorMessage: 'Please provide name and bio for the user.',
-        //     });
-        // }
-    });
+            // else {
+            //     res.status(400).json({
+            //         errorMessage: 'Please provide name and bio for the user.',
+            //     });
+            // }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: 'The user information could not be retrieved.',
+            });
+        });
 });
 
 const port = 5000;
