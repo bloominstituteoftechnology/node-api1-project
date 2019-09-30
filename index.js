c5930d3b4ae1daed54c24aa1bea1b66c1ea13bf5
Tgleason1980 <terryscoding@gmail.com>
2019-09-30 07:37:18 -0500

    did my first get and post endpoints

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+//middleware
+server.use(express.json());
+//request handler
+
+server.get('/users',(req, res)=>{
+    db.find()
+    .then(users =>{
+        res.json(users);
+    })
+    .catch(err =>{
+        res.status(500).json({
+            err: err
+        })
+    })
+});
+
+server.post('/users',(req, res)=>{
+    const newUser = req.body;
+    db.insert(newUser)
+    .then(user =>{
+        res.status(201).json(user);
+    })
+    .catch(err =>{
+        res.status(500).json({
+            err: err
+        })
+    })
+});
+
+
+
+server.listen(5000, () =>{
+    console.log('server is running on port 5000...');
+});
