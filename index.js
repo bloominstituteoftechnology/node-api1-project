357901ba4863a4ae3017fc1251ea1a38378013be
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-09-30 15:06:43 -0500

    get method for users endpoint complete

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ server.use(express.json());
 const db = require('./data/db.js');
 
 //get 
+server.get('/', (req, res) => {
+    res.send('Welcome!');
+});
 
+//get all seed users
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.send(users)
+    })
+    .catch(error => {
+        res.send(error, 'error in the get request');
+    });
+})
 
 
 
