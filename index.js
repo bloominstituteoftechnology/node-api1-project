1dba6aa145c135e4760cd487c160afaeec213235
Mike Harley <mharley12345@gmail.com>
2019-09-30 12:04:51 -0700

    adds get method

--- index.js
+++ index.js
@@ -7,3 +7,36 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/users', (req, res) => {
+    Users.find()
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: 'The users information could not be found.',
+        });
+      });
+  });
+  
+  server.get('/api/users/:id', (req, res) => {
+    Users.findById(req.params.id)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res
+            .status(404)
+            .json({ message: 'The user with the specified ID does not exist.' });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ errorMessage: 'The user information could not be found.' });
+      });
+  });
+  
+
+const port = 3333;
+server.listen(port, () => console.log(`\n *** Api listening on port ${port} ***\n`))
