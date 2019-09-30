f49c03313d1cdf5e653498b57e239597632d8a46
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-09-30 14:59:35 -0500

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//adding the model
+const db = require('./data/db.js');
+
+//get 
+
+
+
+
+
+
+
+//port 
+const port = 5000;
+server.listen(port, () => {
+    console.log(`\n** API is running on ${port}! \n`);
+})
