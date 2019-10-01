9a7744bcdecccab4fceec386ad6e62f0b1fd89d6
ian-sanchez <silkyhairbear@gmail.com>
2019-10-01 08:52:33 -0700

    started rendering

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+    res.send('This is Ians server')
+})
+
+server.listen(8000, () => console.log('server on 8000'));
