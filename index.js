11e91cc5315d4558facf980d9916a3be61e8d8df
milano12345 <dmilano857@gmail.com>
2019-09-30 21:54:46 -0500

    added delete functionality to backend

--- index.js
+++ index.js
@@ -41,5 +41,43 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
+server.post("/api/users", (req, res) => {
+  const userData = req.body;
+  if (!userData.name || !userData.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    dataBase
+      .insert(userData)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.json({ message: "error saving the user" });
+      });
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  dataBase.findById(id).then(user => {
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    } else {
+      dataBase
+        .remove(id)
+        .then(hub => {
+          res.status(201).json(hub);
+        })
+        .catch(error => {
+          res.status(500).json({ message: "error deleting the hub" });
+        });
+    }
+  });
+});
+
 const port = 8000;
 server.listen(port, () => console.log("/nserver running/n"));
