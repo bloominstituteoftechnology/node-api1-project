bd425107de33bbcc34a1088c6f76e832c36dfe9d
sooshe <sooshe@sooshebot.com>
2019-10-28 16:11:07 -0500

    init commit, add data to index file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require('express'); 
+const server = express(); 
+
+server.get('/', (req, res) => {
+    res.send('Hello Node23!!!!');
+});
+
+
+const port = 8000; 
+server.listen(port, () => console.log('\n=== API on port 8000 ===\n'))
+
