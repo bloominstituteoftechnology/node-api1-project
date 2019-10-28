6a2403512c33023b9f761eaaf131e7ec1eeed354
unknown <neduvinci@gmail.com>
2019-10-28 14:14:25 +0100

    Added endpoint for retrieving all users

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 // implement your API here
 const express = require('express');
 const cors = require('cors');
+const db = require('./data/db');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+app.get('/api/users', getAllUsers)
 app.get('*', handleRequest)
 app.post('*', handleRequest)
 app.put('*', handleRequest)
 app.delete('*', handleRequest)
 app.patch('*', handleRequest)
 
+function getAllUsers(req, res) {
+    db.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+
+            console.log(error)
+        })
+}
 function handleRequest(req, res) {
     res.json('You have reached the end of the internet')    
 }
