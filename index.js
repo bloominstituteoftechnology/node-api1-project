2c9aa8f772c058b3a7ea2d94a254e40b058632fc
unknown <neduvinci@gmail.com>
2019-10-28 13:34:05 +0100

    Initial commit, basic server seetup

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('*', handleRequest)
+app.post('*', handleRequest)
+app.put('*', handleRequest)
+app.delete('*', handleRequest)
+app.patch('*', handleRequest)
+
+function handleRequest(req, res) {
+    res.json('You have reached the end of the internet')    
+}
+
+app.listen(process.env.port || 7000, () => {
+    console.log(`listening on port ${process.env.port || 7000}`);
+})
