a30c1ae476484fc867da3337a5de21962dd60cba
sooshe <sooshe@sooshebot.com>
2019-10-28 17:12:20 -0500

    update getin index file and change port to 7000

--- index.js
+++ index.js
@@ -3,21 +3,35 @@ const express = require('express');
 const db = require('./data/db.js');
 const server = express(); 
 
-server.get('/', (req, res) => {
-    res.send('Node API1 Project, Ahoy!');
+server.use(express.json()); //teaches express how to read json, needed for POST and PUT to work
+
+// server.get('/', (req, res) => {
+//     res.send('Node API1 Project, Ahoy!');
+// });
+
+server.get('/api/users', (req,res) => {
+    db.find() 
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => {
+        // res.send('An error, sorrrry.') --doing .json instead per today's lecture
+        res.json({error: 'failed to get lambda from db'});
+    });
 });
 
-server.get('/lambda', (req,res) => {
-    db.find() 
+server.post('/lambda', (res,req) => {
+    const lambdaInfo = req.body;
+
+    db.insert(lambdaInfo)
     .then(lambda => {
         res.json(lambda);
     })
     .catch(err => {
-        // res.send('An error, sorrrry.') --doing .json instead per today's lecture
-        res.json({error: 'failed to get lambda from db'})
+        res.json({error: 'failed to insert lambda from db'});
     });
 });
 
-const port = 9000; 
-server.listen(port, () => console.log('\n=== API on port 9000 ===\n'));
+const port = 7000; 
+server.listen(port, () => console.log('\n=== API on port 7000 ===\n'));
 
