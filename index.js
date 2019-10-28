cb1a01c1cb211c8a1f4a00668724483eb9069bbd
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-28 16:59:51 -0400

    post request

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const db = require('./data/db.js');
+
+server.post('/api/users', (req, res) => {
+    const newUsers = req.body;
+    users.add(newUsers)
+        .then(users => res.status(201).json(users))
+        .catch(err => {
+            res.status(500).json({
+                err: err,
+                message: 'There was an error while saving the user to the database'
+            })
+        })
+})
