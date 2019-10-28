e7a4427a268b62a2cfef5f82006e78501265dafa
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-28 15:12:35 +0100

    created endppoints to get all users

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+const db = require('./data/db')
+
+app.use(express.json())
+
+server.get('/api/users', (res, req) => {
+    db.find()
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(error => {
+            res.status(500).json({ success: false, error })
+        })
+})
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('LIstening on' + process.env.PORT || 3000)
+})
+
