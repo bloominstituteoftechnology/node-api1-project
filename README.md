59b5d9f1d8352ee5c312362b34969e32d7b43062
Gatrrr <davidgatrmacaulay@gmail.com>
2019-10-28 16:14:37 -0700

    MVP reached

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ Inside `index.js` add the code necessary to implement the following _endpoints_:
 
 #### Endpoint Specifications
 
-When the client makes a `POST` request to `/api/users`:
+When the client makes a `POST` request to `/api/users`: DONE 
 
 - If the request body is missing the `name` or `bio` property:
 
@@ -69,7 +69,7 @@ When the client makes a `POST` request to `/api/users`:
   - respond with HTTP status code `400` (Bad Request).
   - return the following JSON response: `{ errorMessage: "Please provide name and bio for the user." }`.
 
-- If the information about the _user_ is valid:
+- If the information about the _user_ is valid: 
 
   - save the new _user_ the the database.
   - return HTTP status code `201` (Created).
@@ -80,14 +80,14 @@ When the client makes a `POST` request to `/api/users`:
   - respond with HTTP status code `500` (Server Error).
   - return the following JSON object: `{ error: "There was an error while saving the user to the database" }`.
 
-When the client makes a `GET` request to `/api/users`:
+When the client makes a `GET` request to `/api/users`: DONE
 
 - If there's an error in retrieving the _users_ from the database:
   - cancel the request.
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The users information could not be retrieved." }`.
 
-When the client makes a `GET` request to `/api/users/:id`:
+When the client makes a `GET` request to `/api/users/:id`: DONE
 
 - If the _user_ with the specified `id` is not found:
 
@@ -99,7 +99,7 @@ When the client makes a `GET` request to `/api/users/:id`:
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ error: "The user information could not be retrieved." }`.
 
-When the client makes a `DELETE` request to `/api/users/:id`:
+When the client makes a `DELETE` request to `/api/users/:id`: DONE 
 
 - If the _user_ with the specified `id` is not found:
 
--- index.js
+++ index.js
@@ -1,1 +1,93 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello Captain');
+});
+
+server.get('/users', (req, res) => {
+    db.find()
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        console.log('error', err);
+        res.status(500).json({ error: 'failed to get users from db'});
+    });
+});
+
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ error: ' failed to find user from db'})
+        });
+});
+
+ server.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+        .then(count => {
+            res.status(200).json({ message: `user with id ${id} deleted`})
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ error: 'failed to delete user from db'})
+        });
+ });
+
+ server.post('/users', (req, res) => {
+    const userInformation = req.body;
+    console.log('user info', userInformation);
+
+    db.insert(userInformation)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            console.log('error', err)
+            res.status(500).json({ error: "failed to add user to the database"})
+        });
+});
+
+server.put('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const userInformation = req.body;
+    console.log('user info', userInformation);
+    console.log(id, userInformation)
+    
+    db.update(id, userInformation)
+    .then(count => {
+                if (count === 1 ){
+                    db.findById(id)
+                    .then(user => {
+                        res.status(200).json(user)
+                    })
+                    .catch(err => {
+                        console.log('error', err);
+                        res.status(500).json({ error: ' failed to find user from db'})
+                    });
+                }
+                
+            })
+            .catch(err => {
+                console.log('error', err);
+                res.status(500).json({ error: ' failed to find user from db'})
+            });
+});
+
+ const port = 8000;
+ server.listen(port, ()=> console.log('api on port 8000'));
