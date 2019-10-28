7758ac67c6027e088d83258deb9a6999e551683b
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-28 17:18:23 -0400

    GET user by ID

--- index.js
+++ index.js
@@ -26,3 +26,24 @@ server.get('/api/users', (req, res) => {
             })
         })
 })
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    users.findById(id)
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                err: err,
+                message: 'The user information could not be retrieved.'
+            })
+        })
+})
