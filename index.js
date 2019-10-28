2691e371ccd72c94edb52394ef8eda3522aa2fb6
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-28 14:25:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+  
+// implement your API here
+//get express to run api like data
+const express = require('express')
+// set up server to depend on express
+const server = express()
+// import db to run commands
+const db = './db.js'
+
+// define port to run on 
+const port = 5000
+server.listen(port, () => console.log('\n RUNNING ON 5000 SERVER \n'))
+
+// run server listen upon npm run server
+server.get('/', (req, res) => {
+    res.send('User DB is Open')
+})
