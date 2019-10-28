b3663dbe04be752e7f0e13e231104baa7127e397
sooshe <sooshe@sooshebot.com>
2019-10-28 18:47:44 -0500

    update post text and fix issue in insomnia

--- index.js
+++ index.js
@@ -5,31 +5,36 @@ const server = express();
 
 server.use(express.json()); //teaches express how to read json, needed for POST and PUT to work
 
-// server.get('/', (req, res) => {
-//     res.send('Node API1 Project, Ahoy!');
-// });
 
 server.get('/api/users', (req,res) => {
     db.find() 
     .then(users => {
         res.status(200).json(users);
     })
-    .catch(err => {
+    .catch(error => {
         // res.send('An error, sorrrry.') --doing .json instead per today's lecture
+        console.log('An error', error)
         res.status(500).json({errorMessage: "failed to get users from db"});
     });
 });
 
-server.post('/api/users', (res,req) => {
-    db.insert()
+server.post('/api/users', (req,res) => {
+    user = {
+        ...req.body,
+    };
+   
+    db.insert(user)
     .then(users => {
-        res.status(200).json(users);
+        res.status(201).json(users);
     })
-    .catch(err => {
+    .catch(error => {
         res.status(400).json({errorMessage: "Please provide name and bio for the user."});
     });
 });
 
-const port = 7000; 
-server.listen(port, () => console.log('\n=== API on port 7000 ===\n'));
+const port = 6000; 
+server.listen(port, () => console.log('\n=== API on port 6000 ===\n'));
 
+
+
+//in insomnia the post error came from not setting a header on the post side -- when you set it up, the name and bio you entered posted an id for the user
