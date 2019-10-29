289c452d9c3b0b1b324bab7c31d550a73724f38f
sooshe <sooshe@sooshebot.com>
2019-10-28 20:11:17 -0500

    completed delete post

--- index.js
+++ index.js
@@ -6,17 +6,8 @@ const server = express();
 server.use(express.json()); //teaches express how to read json, needed for POST and PUT to work
 
 
-server.get('/api/users', (req,res) => {
-    db.find() 
-    .then(users => {
-        res.status(200).json(users);
-    })
-    .catch(error => {
-        // res.send('An error, sorrrry.') --doing .json instead per today's lecture
-        res.status(500).json({ error: "The users information could not be retrieved." });
-    });
-});
 
+//----- .post('api/users) -----//
 server.post('/api/users', (req,res) => {
     // one way of doing this
     // user = {
@@ -38,7 +29,51 @@ server.post('/api/users', (req,res) => {
     });
 });
 
+//----- .get('api/users) -----//
+server.get('/api/users', (req,res) => {
+    db.find() 
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(error => {
+        // res.send('An error, sorrrry.') --doing .json instead per today's lecture
+        res.status(500).json({ error: "The users information could not be retrieved." });
+    });
+});
 
+
+//----- .get('api/users/:id) -----//
+server.get('api/users/:id', (req,res) => {
+    const {id} = req.params
+    db.findById(id)
+        .then(user => {
+            if (user === []) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            } else {
+            res.status(200).json(user)
+        }})
+        .catch(error => {
+            res.status(500).json({ error: "The user information could not be retrieved." });
+        });
+});
+
+//----- .delete('api/users/:id) -----//
+server.delete('api/users/:id', (req,res) => {
+    const {id} = req.params;
+
+    db.remove(id)
+    .then(user => {
+        if (user === []) {
+            res.status(404).json({message: 'The user with the specified ID does not exist!'})
+        } else {
+        res.status(204).json({message: 'Success, the user has been deleted!'})
+    }})
+    .catch(error => {
+        res.status(500).json({ error: "The user could not be removed" });
+    });
+});
+
+//----- .put('api/users/:id) -----//
 server.put('/api/users/:id', (req, res) => {
     const { id } = req.params;
     const editedUser = {
@@ -59,32 +94,13 @@ server.put('/api/users/:id', (req, res) => {
     });
 })
 
-server.get('api/users/:id', (req,res) => {
-    const {id} = req.params
-    db.findById(id)
-        .then(user => {
-            if (user === []) {
-                res.status(404).json({ message: "The user with the specified ID does not exist." });
-            } else {
-            res.status(200).json(user)
-        }})
-        .catch(error => {
-            res.status(500).json({ error: "The user information could not be retrieved." });
-        });
-})
 
-server.delete('api/users/:id', (req,res) => {
-    const {id} = req.params;
 
-    db.remove(id)
-    .then(user => {
-        res.status(204).json({message: 'Success'})
-    })
-    .catch(err => res.send(err));
-})
+
+
 const port = 6000; 
 server.listen(port, () => console.log('\n=== API on port 6000 ===\n'));
 
 
 
-//in insomnia the post error came from not setting a header on the post side -- when you set it up, the name and bio you entered posted an id for the user
+//misc note on using insomnia -- in insomnia the post error occurred from not setting a header on the "post" side before clicking "send". When you set up the post with JSON, the name and bio you entered post an id for the user
