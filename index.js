b8fcca3a610b0cdb6b274f61b95717d8de05c502
sooshe <sooshe@sooshebot.com>
2019-10-28 19:46:16 -0500

    update post and findbyid with if-else statements

--- index.js
+++ index.js
@@ -13,25 +13,70 @@ server.get('/api/users', (req,res) => {
     })
     .catch(error => {
         // res.send('An error, sorrrry.') --doing .json instead per today's lecture
-        console.log('An error', error)
-        res.status(500).json({errorMessage: "failed to get users from db"});
+        res.status(500).json({ error: "The users information could not be retrieved." });
     });
 });
 
 server.post('/api/users', (req,res) => {
-    user = {
-        ...req.body,
-    };
+    // one way of doing this
+    // user = {
+    //     ...req.body,
+    // };
+
+    // other way of doing this (saw this most ofen in help)
+    const { name, bio } = req.body;
    
-    db.insert(user)
+    db.insert(req.body)
     .then(users => {
+        if (user === []) {
+            res.status(500).json({ error: "There was an error while saving the user to the database" });
+        } else {
         res.status(201).json(users);
-    })
+    }})
     .catch(error => {
         res.status(400).json({errorMessage: "Please provide name and bio for the user."});
     });
 });
 
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const editedUser = {
+        ...req.body,
+    };
+
+    db.update(id, editedUser)
+    .then(edited => {
+        res.status(203).json(edited);
+    })
+    .catch(error => {
+        res.status(400).json({ message: "The user with the specified ID does not exist." });
+    });
+})
+
+server.get('api/users/:id', (req,res) => {
+    const {id} = req.params
+    db.findById(id)
+        .then(user => {
+            if (user === []) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            } else {
+            res.status(200).json(user)
+        }})
+        .catch(error => {
+            res.status(500).json({ error: "The user information could not be retrieved." });
+        });
+})
+
+server.delete('api/users/:id', (req,res) => {
+    const {id} = req.params;
+
+    db.remove(id)
+    .then(user => {
+        res.status(204).json({message: 'Success'})
+    })
+    .catch(err => res.send(err));
+})
 const port = 6000; 
 server.listen(port, () => console.log('\n=== API on port 6000 ===\n'));
 
