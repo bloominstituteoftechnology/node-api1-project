7df4416618e5f6a22e50f2e97626d0565073c28a
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-28 20:50:55 -0700

    added another server.get

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-// implement your API here
+console.log('its alive');
 
-console.log("n/=== It's alive ===/n")
-//import express from 'express' // ES modules
-const express = require('express'); //commonjs modules, and it is equivalent to the line above
+const express = require('express');
+
+const db = require('./data/db');
 
 const server = express(); //creates a server
 
-//.request/route handlers
-
-//handles GET requests to / on localhost:8000
 server.get('/', (req, res) => {
-    res.send('Hello node')
+    res.send('hello new node 23');
 })
 
-server.post('/echo', (req, res) => {
-    const data = req.body;
-
-    res.json(data);
+server.get('/api/users', (req, res) => {
+    const users = req.params
 })
 
-const port = 8000;
-server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    users.findById(id)
+    .then(user => {
+        if(user === []) {
+            res.status(404).json({ message: "Can not find user with this id"})
+        } else {
+            res.json(user)
+        }
+    })
+    .catch(err => {
+        console.log('find by id error', err);
+        res.status(500).json({ error: "information for this user is non-existant"})
+    })   
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+    .then(user => {
+        res.status(200).json({ message: 'user with id ${id} deleted' })
+    })
+    .catch(err => {
+        console.log('error', err);
+        res.status(404).json({ error: 'the user with this id does not exist'});
+    })
+})
+
+const port = 5000;
+server.listen(port, () => console.log('\n--- api is on ===\n'))
