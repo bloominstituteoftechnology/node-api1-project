f327439d99b866fec9f95225fa544dec1f1872f8
David.Johnson <davidt.johnson75@yahoo.com>
2019-10-28 20:08:19 -0500

    MVP met

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
+const dbase = knex(knexConfig.development);
 
 module.exports = {
   find,
@@ -11,29 +11,29 @@ module.exports = {
 };
 
 function find() {
-  return db('users');
+  return dbase('users');
 }
 
 function findById(id) {
-  return db('users')
+  return dbase('users')
     .where({ id: Number(id) })
     .first();
 }
 
 function insert(user) {
-  return db('users')
+  return dbase('users')
     .insert(user)
     .then(ids => ({ id: ids[0] }));
 }
 
 function update(id, user) {
-  return db('users')
+  return dbase('users')
     .where('id', Number(id))
     .update(user);
 }
 
 function remove(id) {
-  return db('users')
+  return dbase('users')
     .where('id', Number(id))
     .del();
 }
--- index.js
+++ index.js
@@ -1,1 +1,106 @@
 // implement your API here
+
+const express = require("express");
+const dbase = require("./data/db.js");
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("This code is working just fine!");
+});
+
+server.get("/api/users", (req, res) => {
+  dbase
+    .find()
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => {
+      res.status(550).json({error: "That user was not found."});
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  dbase
+    .findById(id)
+    .then(user => {
+      if (!user) {
+        res
+          .status(404)
+          .json({error: "That user ID doesn't exist on this server."});
+      } else {
+        res.json(user);
+      }
+    })
+    .catch(error => {
+      res.status(400).json({error: "The users description is not available."});
+    });
+});
+
+server.post("/api/users", (req, res) => {
+    const userDesc = req.body;
+    if (!userDesc.name || !userDesc.bio) {
+      res
+        .status(400)
+        .json({error: "Enter a name and bio for this user." });
+    } else {
+      dbase
+        .insert(userDesc)
+        .then(user => {
+          res.status(201).json(user);
+        })
+        .catch(err => {
+          res.json({error: "The user was not added!" });
+        });
+    }
+  });
+  
+  server.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    dbase.findById(id)
+    .then(user => {
+      if (!user) {
+        res
+          .status(404)
+          .json({error: "That user ID doesn't exist on this server."});
+      } else {
+        dbase
+          .remove(id)
+          .then(hub => {
+            res.status(201).json(hub);
+          })
+          .catch(error => {
+            res.status(500).json({error: "There was an error deleting the user."});
+          });
+      }
+    });
+  });
+
+  server.put("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    const modify = req.body;
+    dbase.findById(id).then(user => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "That user ID doesn't exist on this server."});
+      } else if (!modify.name || !modify.bio) {
+        res
+          .status(400)
+          .json({ error: "Please enter a user name and bio."});
+      } else {
+        dbase
+          .update(id, modify)
+          .then(hub => {
+            res.status(200).json(hub);
+          })
+          .catch(error => {
+            res.status(500).json({ message: "That user was not modified!"});
+          });
+      }
+    });
+  });
+
+const port = 8000;
+server.listen(port, () => console.log("Your server is running on port 8000."));
