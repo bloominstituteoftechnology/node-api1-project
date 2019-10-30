eeda008df4d1d16654b480493bf495109f741b1b
Dave Kane <davelkane@gmail.com>
2019-10-30 13:26:56 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// implement your API here
+const db = require('./data/db')
+
+const express = require('express')
+
+const server = express()
+
+server.listen(4534, () => {
+  console.log('=== server listening on port 4534 ===')
+})
+
+server.use(express.json())
+
+
