3ab3e4834c07cf63d88b5e0c558fedfa5c45fdf0
lizdoyle <lizdoyle1121@gmail.com>
2019-10-30 22:09:34 -0400

    commented out code to start

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+
+// const http = require('http');
+
+// const hostname= '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", 'text-plain');
+//     res.end('Hello')
+
+// })
+
+// server.listen(port, hostname, () => {
+//     console.log(`server listening on port http://${hostname}:${port}`)
+// }
