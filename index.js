4bfff413735c8b51ddfe45f1d44e17663db4cff6
Carlos Mitchell <kakismitchell@gmail.com>
2019-10-30 23:14:56 -0400

    get /find created

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+
+const db = require('./data/db.js');
+const express = require('express');
+
+const server = express();
+
+server.listen(4000, ()=> {
+   console.log('===server listening on port 4000====');
+});
+
+
+server.get('/', (request, response)=>{
+ response.send('Hello world')
+})
+
+server.get('/find', (req, res) => {
+  db.find()
+      .then(find => {
+          res.status(200).json(find);
+      })
+      .catch(err => {
+          res.status(500).json({ sucess: false, err });
+      });
+});
+
