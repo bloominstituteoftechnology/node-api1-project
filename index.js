9b1e8576ef5292c17d8335750c94da0899e9a59c
lizdoyle <lizdoyle1121@gmail.com>
2019-10-30 22:23:32 -0400

    basic start to server setup in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // implement your API here
+//const http = requires('http');
+//commented out require of standard http information
 
-// const http = require('http');
+const express = require('express');
 
 // const hostname= '127.0.0.1';
 // const port = 3000;
@@ -11,7 +13,20 @@
 //     res.end('Hello')
 
 // })
+// html version with plain txt is commented out. working code is using express
+
+const server = express();
 
 // server.listen(port, hostname, () => {
 //     console.log(`server listening on port http://${hostname}:${port}`)
 // }
+
+//using hostname and port set with standard http request, takes a port and a callback function below:
+
+server.listen(5000, () => {
+    console.log('===server listening on port 5000===');
+});
+
+
+
+console.log("hello world")
