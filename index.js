a38a0ccd3b5b4bf98255c40b9bcae990d854fb00
Jashele T <jashele@Jasheles-Air.domain>
2019-11-03 13:41:44 -0700

    the server is running properly

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require("express");
+const db = require('./data/seeds/users.js');
+
+const server = express();
+
+server.listen(4000, () => {
+   console.log("server listening on port 4000")
+});
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+   res.send("The server is running properly...")
+});
+
