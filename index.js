c13ed145fbdc5a372263d6040a4c28533a2afacf
Jashele T <jashele@Jasheles-Air.domain>
2019-11-03 16:51:04 -0700

    post request is now working it adds to the api however when testing in postman it only previews the new id cant see the whole new object weird

--- index.js
+++ index.js
@@ -1,81 +1,83 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/db.js');
-
+const userData = require("./data/db.js");
 
 const server = express();
 
-server.listen(4000, () => {
-  console.log('server listening on port 4000');
-});
-
 server.use(express.json());
 
-
-server.get('/', (req, res) => {
-  res.send('The server is running properly...');
-});
-
-
+const port = 5000;
+server.listen(port, () => 
+console.log(`\n*** API on port ${port} ***\n`))
 
 //CRUD -------------------
 
+// POST
+server.post("/users", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    userData.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            'There was an error while saving the user to the database',
+        });
+      });
+  }
+});
+
 // GET
 server.get('/users', (req, res) => {
-  db.find()
+  userData.find()
     .then(users => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(500).json({ success: false, err });
+      res.status(500).json({ errorMessage: "The users could not be retrieved" });
     });
 });
 
-// POST
-server.post('/users', (req, res) => {
-  const userInfo = req.body;
+// // DELETE
+// server.delete('/users/:id', (req, res) => {
+//   const { id } = req.params;
 
-db.add(userInfo)
-   .then((user) => {
-      res.status(201).json({ success: true, user})
-   })
-   .catch((err) => {
-      res.status(500).json({ success: false, err})
-   });
-});
+//   db.remove(id)
+//     .then(deletedUser => {
+//       if (deletedUser) {
+//         res.status(204).end();
+//       } else {
+//         res.status(404).json({ message: `I could not find id=${id}` });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ success: false, err });
+//     });
+// });
 
+// // PUT
+// server.put('/users/:id', (req, res) => {
+//   const id = req.params.id;
+//   const userInfo = req.body;
 
-// DELETE
-server.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  db.remove(id)
-    .then(deletedUser => {
-      if (deletedUser) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({ message: `I could not find id=${id}`});
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ success: false, err });
-    });
-});
-
-// PUT
-server.put('/users/:id', (req, res) => {
-  const id = req.params.id;
-  const userInfo = req.body;
-
-  db.update(id, userInfo)
-    .then(user => {
-      if (user) {
-        res.status(200).json({ success: true, user });
-      } else {
-        res.status(404).json({ success: false, message: `id ${id} does not exist` });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ success: false, err });
-    });
-});
+//   db.update(id, userInfo)
+//     .then(user => {
+//       if (user) {
+//         res.status(200).json({ success: true, user });
+//       } else {
+//         res
+//           .status(404)
+//           .json({ success: false, message: `id ${id} does not exist` });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ success: false, err });
+//     });
+// });
