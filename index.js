39bf09708963e2d9808db9a4cb25c5729d5e5038
Jashele T <jashele@Jasheles-Air.domain>
2019-11-03 14:07:39 -0700

    added CRUD operations, postman test failed

--- index.js
+++ index.js
@@ -1,15 +1,92 @@
-const express = require("express");
-const db = require('./data/seeds/users.js');
+const db = require('./data/seeds/users');
+
+const express = require('express');
+
 
 const server = express();
 
 server.listen(4000, () => {
-   console.log("server listening on port 4000")
+  console.log('server listening on port 4000');
 });
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-   res.send("The server is running properly...")
+server.get('/api', (req, res) => {
+  res.send('The server is running properly...');
 });
+
+
+//CRUD -------------------
+
+// GET
+server.get('/users', (req, res) => {
+   db.find()
+   .then(users => {
+      res.status(200).json(users);
+   })
+   .catch(err => {
+      res.status(500).json({
+         message: err,
+         success: false
+      })
+   })
+});
+
+
+// POST
+server.post('/users', (req, res) => {
+   const userInfo = req.body;
+
+   db.add(userInfo)
+      .then((user) => {
+         res.status(201).json({
+            success: true,
+            user
+         })
+         .catch((err) => {
+            res.status(500).json({ 
+               success: false,
+               err
+            })
+         })
+      })
+});
+
+// DELETE
+server.delete('/users/:id', (req, res) => {
+   const {id} = req.params;
+
+   db.remove(id)
+   .then(deletedUser => {
+      if(deletedUser) {
+         res.status(204).end();
+         
+      } else {
+         res.status(404).json({ message: `I could not find id={id}`})
+      }
+   })
+   .catch(err => {
+      res.status(500).json({ success: false, err})
+   })
+
+});
+
+// PUT
+server.put('/users/:id', (req, res) => {
+   const id = req.params.id;
+   const userInfo = req.body;
+
+   db.update(id, userInfo)
+   .then(user => {
+      if(user) {
+         res.status(200).json({ success: true, user});
+      } else {
+         res.status(404).json({ success: false, message: `id ${id} does not exist`})
+      }
+   })
+   .catch(err => {
+      res.status(500).json({ success: false, err})
+   })
+});
+
 
