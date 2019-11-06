56baa40dbbcd623e4ea35ecea90b11b3f4b247c6
Carlos Mitchell <kakismitchell@gmail.com>
2019-11-05 22:08:33 -0500

    MVP

--- index.js
+++ index.js
@@ -66,3 +66,52 @@ server.get('/api/users/:id', (req, res) => {
           .json({ errorMessage: 'user could not be retrieved.' });
       });
   });
+
+  server.delete('/api/users/:id', (req, res) => {
+    Users.remove(req.params.id)
+      .then(count => {
+        if (count && count > 0) {
+          res.status(200).json({
+            message: 'the user was deleted.',
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: 'The user with the specified ID does not exist.' });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ errorMessage: 'The user could not be removed' });
+      });
+  });
+  
+  server.put('/api/users/:id', (req, res) => {
+    const { name, bio } = req.body;
+  
+    if (!name || !bio) {
+      res
+        .status(400)
+        .json({ errorMessage: 'Please provide name and bio for the user.' });
+    } else {
+      Users.update(req.params.id, req.body)
+        .then(user => {
+          if (user) {
+            res.status(200).json(user);
+          } else {
+            res
+              .status(404)
+              .json({
+                message: 'The user with the specified ID does not exist.',
+              });
+          }
+        })
+        .catch(() => {
+          res.status(500).json({
+            errorMessage: 'The user information could not be modified.',
+          });
+        });
+    }
+  });
+  
+  const port = 5000;
+  server.listen(port, () => console.log(`\n*** API on port ${port} ***\n`));
