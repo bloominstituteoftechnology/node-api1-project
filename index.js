fe9ed69ab104e4c49d85214cf9c8cc7335dca60f
Anthony Sutton <anthony.sutton95@gmail.com>
2019-12-02 12:03:10 -0800

    Updated response messages and user can be removed from the database

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.get(`/users`, (req, res) => {
     })
     .catch(error => {
       console.log('Error on GET /users', error);
-      res.status(500).json({errorMessage: 'Error obtaining list of users'});
+      res.status(500).json({error: "The users information could not be retrieved."});
     });
 });
 
@@ -31,12 +31,28 @@ server.get('/users/:id', (req, res) => {
       if (user) {
         res.status(200).json(user);
       } else {
-        res.status(404).json({message: `Unable to find user with id of ${id}`});
+        res.status(404).json({message: "The user with the specified ID does not exist."});
       }
     })
     .catch(error => {
       console.log(`Error on GET /users/${id}`);
-      res.status(404).json({errorMessage: `Error obtaining id ${id}`});
+      res.status(404).json({error: "The user information could not be retrieved."});
+    });
+});
+
+server.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(removed => {
+      if (removed) {
+        res.status(200).json({message: 'The user was removed.'})
+      } else {
+        res.status(400).json({message: "The user with the specified ID does not exist."});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({error: "The user could not be removed."});
     });
 });
 
