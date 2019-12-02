76a59fe93521d1ce2d327ecb26c4ab447bd7bd6a
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-02 15:33:01 -0600

    get requests and post requesting working

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 // implement your API here
 const express = require('express');
 
-const server = express();
-const port = 5000; 
+const db = require('./data/db.js');
 
-server.get('/', (req, res) => {
-    res.send({ api: 'testing api is running...'});
+const server = express();
+
+
+server.use(express.json());
+
+
+//GET 
+server.get('/api/users', (req, res) => {
+
+    db.find()
+    .then(users =>{
+        res.status(200).json(users);
+    })
+    .catch( err => {
+        console.log('error on GET /users', err);
+        res.status(500).json({errorMessage: "The users information could not be retrieved."})
+    })
 });
 
+//GET 
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+    .then(users =>{
+      if (users){
+        res.status(200).json(users);
+      } else { 
+          res.status(404).json({ message: "The user with the specified ID does not exist." })
+      };
+    })
+    .catch( err => {
+        console.log('error on GET /users', err);
+        res.status(500).json({errorMessage: "The users information could not be retrieved."})
+    })
+});
+
+
+//post
+server.post('/api/users', (req, res) => {
+    const users = req.body; 
+    db.insert(users)
+    .then(data => {
+        if('name' === ' ' || 'bio' === ' '){
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+        } else {
+            res.status(201).json(data);
+        }
+    })
+    .catch(err => {
+        console.log('error on GET /api/users', err);
+        res.status(500).json({errorMessage: "There was an error while saving to the database" });
+    });
+});
+
+//delete
+
+
+
+
+
+
+
+const port = 5000; 
 server.listen(port, () => {
     console.log(`\n API running on port ${port} \n`);
 });
