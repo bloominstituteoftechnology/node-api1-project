0b9a006b33557f23d4a04d89918d58d337595126
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-02 13:51:02 -0600

    installed dependencies, set up and tested server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+const port = 5000; 
+
+server.get('/', (req, res) => {
+    res.send({ api: 'testing api is running...'});
+});
+
+server.listen(port, () => {
+    console.log(`\n API running on port ${port} \n`);
+});
