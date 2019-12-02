6a6f086df191d301ae2ed93e840b2a7151bd38f4
Anthony Sutton <anthony.sutton95@gmail.com>
2019-12-02 11:45:04 -0800

    Created server and installed express dependencies

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+const port = 4000;
+
+server.use(express.json());
+
+server.get('/', (req, res) => {});
+
+server.listen(port, () => console.log(`\n***Server running on port ${port}***\n`));
