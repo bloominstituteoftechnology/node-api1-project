cd39bb0d7eb26a2f7cf9fae375bacd3afb2ab46e
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-02 15:45:52 -0600

    delete request successful

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ server.get('/api/users', (req, res) => {
         res.status(200).json(users);
     })
     .catch( err => {
-        console.log('error on GET /users', err);
+        console.log('error on GET/api/users', err);
         res.status(500).json({errorMessage: "The users information could not be retrieved."})
     })
 });
 
-//GET 
+//GET by Id
 
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
@@ -35,7 +35,7 @@ server.get('/api/users/:id', (req, res) => {
       };
     })
     .catch( err => {
-        console.log('error on GET /users', err);
+        console.log('error on GET/api/users/:id', err);
         res.status(500).json({errorMessage: "The users information could not be retrieved."})
     })
 });
@@ -47,19 +47,34 @@ server.post('/api/users', (req, res) => {
     db.insert(users)
     .then(data => {
         if('name' === ' ' || 'bio' === ' '){
-            res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+            res.status(400).json({ message: "Please provide name and bio for the user."})
         } else {
             res.status(201).json(data);
         }
     })
     .catch(err => {
-        console.log('error on GET /api/users', err);
+        console.log('error on POST/api/users', err);
         res.status(500).json({errorMessage: "There was an error while saving to the database" });
     });
 });
 
 //delete
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
 
+    db.remove(id)
+    .then(removed => {
+        if(removed){
+            res.status(200).json({ message: 'user removed successfully.', removed });
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist."});
+        }
+    })
+    .catch(err => {
+        console.log('error on DELETE/api/users/:id', err);
+        res.status(500).json({errorMessage: "The user could not be removed"});
+    });
+});
 
 
 
