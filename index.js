95d035e912c557c5c7a1a83b3d8a25ddf6851949
Michael Jordan <Insurikai@gmail.com>
2019-12-02 10:40:21 -0800

    finished

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+const port = 8000;
+server.use(express.json());
+server.use((req, res, next) => {
+  console.log(req.url, req.body);
+  next();
+});
+server.get('/', (req, res)=>{
+  console.log('Hello')
+  res.send('Hello')
+})
+server.post('/api/users', (req, res) => {
+  db.insert(req.body).then(id => {
+    res.send(id);
+  });
+});
+server.get('/api/users', (req, res) => {
+  db.find().then(users => {
+    res.send(users);
+  });
+});
+server.get('/api/users/:id', (req, res) => {
+  db.findById(req.params.id).then(user => {
+    res.send(user);
+  });
+});
+server.delete('/api/users/:id', (req, res) => {
+  db.remove(req.params.id).then(count => {
+    res.status(200).send(`Removed ${count} user(s).`)
+  });
+});
+server.put('/api/users/:id', (req, res) => {
+  db.update(req.params.id, req.body).then(count => {
+    res.send(`Made ${count} change(s).`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`\n Running on port ${port} \n`);
+});
