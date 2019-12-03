50420488168ab45f8e000bfa2f97475e717995b5
Ale <alearanibar@gmail.com>
2019-12-02 22:08:10 -0800

    notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // implement your API here
 const express = require('express');
 const db = require('./data/db.js')
+// ^---could be called what ever i choose db
+
 
 const server = express();
 const port = 8000
@@ -21,7 +23,7 @@ server.get('/api/users', (req, res) => {
     .catch(error => {
         console.log('error on GET /hubs', error)
         res
-            .status(500)
+            .status(500) ////(Server Error).
             .json({ errorMessage: `couldn't get the data`})
     })
 })
@@ -30,17 +32,19 @@ server.post ('/api/users', (req, res) => {
     const { name, bio } = req.body
         if (!name || !bio) {
             res
-                .status(400)
+                .status(400) //(Bad Request).
                 .json({ errorMessage: "Please provide name and bio for the user." })
         } else {
             db.insert(req.body)
             .then(user => {
-                res.status(201).json(user)
+                res
+                .status(201) //(Created).
+                .json({message:`added ${name} with a id of ${user.id}`})
             })
             .catch(error => {
                 console.log( 'POST not working', error)
                 res
-                    .status(500)
+                    .status(500) //(Server Error).
                     .json({ error: "There was an error while saving the user to the database" })
             })
         }
@@ -50,17 +54,18 @@ server.get ('/api/users/:id', (req, res) => {
     db.findById(req.params.id)
         .then(user => {
             if (user) {
-                res.status(200).json(user)
+                res.status(200) //request was received
+                .json(user)
             } else {
                 res
-                    .status(404)
+                    .status(404) // (Not Found).
                     .json({ message: "The User with the specified ID doesn't exist." })
             }
         })
         .catch(error => {
             console.log('GET not working', error)
             res
-                .status(500)
+                .status(500) ////(Server Error).
                 .json({ errorMessage: "The User information couldn't be retrieved." })
         })
 })
@@ -70,18 +75,18 @@ server.delete('/api/users/:id', (req, res) => {
         .then(erase => {
             if(erase && erase > 0) {
             res
-            .status(200)
-            .json({deleteMessage: 'id was deleted'})
+            .status(200) //request was received
+            .json({deleteMessage: `id ${erase} was erased`})
         } else {
             res
-                .status(404)
+                .status(404) // (Not Found).
                 .json({ message: 'The user with the specified ID does not exist.' });
         }
     })
         .catch(error => {
             console.log('delete didnt work', error)
             res
-                .status(500)
+                .status(500) //(Server Error).
                 .json({ errorMessage: 'The user could not be removed' })
         })
 })
@@ -90,23 +95,24 @@ server.put('/api/users/:id', (req, res) => {
     const { name, bio } = req.body
     if(!name || !bio) {
         res
-            .status(400)
+            .status(400) //(Bad Request).
             .json({ errorMessage: 'Please provide name and bio for the user.' })
     } else {
         db.update(req.params.id, req.body)
             .then(user => {
                 if (user) {
-                    res.status(200).json(user)
+                    res.status(200) //request was received
+                    .json({message:`edited ${name} new bio ${bio}`})
                 } else {
                     res
-                        .status(404)
+                        .status(404) // (Not Found).
                         .json({ message: "The User with the specified ID does not exist." })
                 }
             })
             .catch(error => {
                 console.log(`PUT not working`, error)
                 res
-                    .status(500)
+                    .status(500) //(Server Error).
                     .json({ errorMessage: "The User information couldn't be modified." })
             })
     }
