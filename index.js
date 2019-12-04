b8b249e646a651a2731d220521462a406c2ebe0a
vy3191 <venkyafhs@gmail.com>
2019-12-03 23:05:11 -0500

    Done get users request

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const app = express();
 app.use(express.json());
 const PORT = 8080;
 const hostname = '127.0.0.1';
-const database = require("./data/db.js");
+const db = require("./data/db");
 
-app.use(express.json());
 
 app.get("/", (req,res) => {
+     console.log('woring',db)
     res.status(200).json({"msg": "App is working now"});
 });
 
 app.get("/api/users", (req,res) => {
-    const users = database.find();
-    res.status(200).json(users);
+    db.find()
+      .then( response => {
+         if(response) {
+            res.status(200).json(response);
+         } else {
+            res.status(404).json({msg:"Not Found"})
+         }
+      })
+      .catch( err => {
+          res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+      })
 });
 
 app.listen(PORT,hostname, () => {
