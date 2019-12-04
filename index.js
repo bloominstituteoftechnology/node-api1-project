0a76c6c8d757b9f73a7d574b40d3ba035749b258
vy3191 <venkyafhs@gmail.com>
2019-12-03 22:45:05 -0500

    checking the get requrest working

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require('express');
+const app = express();
+app.use(express.json());
+const PORT = 8080;
+const hostname = '127.0.0.1';
+const database = require("./data/db.js");
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.status(200).json({"msg": "App is working now"});
+});
+
+app.get("/api/users", (req,res) => {
+    const users = database.find();
+    res.status(200).json(users);
+});
+
+app.listen(PORT,hostname, () => {
+   console.log(`app running at http://${hostname}:${PORT}`);
+})
