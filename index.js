2677f1bf9ed85dce267960c2d171976cf4890f11
derpWanda <derpWanda@gmail.com>
2019-12-04 15:36:00 -0600

    set server, listen, and api/users; working

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+const express = require("express")
+let db = require("./data/db.js")
+const server = express()
+
+//middleware
+server.use(express.json())
+
+server.get("/", (request, response) => {
+    console.log("ip:", request.ip)
+
+    response.json({ message: "we in here!" })
+})
+
+server.get("/api/users", (request, response) => {
+    db.find().then(users => {
+        response.status(200).json(users)
+    }).catch(error => {
+        response.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    })
+})
+
+server.listen(9000, () => console.log('the server is alive'))
