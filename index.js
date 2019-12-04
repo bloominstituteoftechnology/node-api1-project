4530cd450d2a101c6c6fc649486747de95e3dec7
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-12-04 10:34:17 -0600

    Added the 'node index.js'  script to the package.json file.

--- index.js
+++ index.js
@@ -8,23 +8,28 @@ const app = express()
 app.use(express.json())
 
 
+app.get('/', (req, res) => {
+    console.log('ip: ', req.ip)
+    res.status(200).json({ message: "Welcome to Node/Express!"})
+})
+
 //getting a list of all users.
 
-app.get('/api/users', (req, res) => {
+app.get('/users', (req, res) => {
     //I want to see the IP of the request.
     console.log('ip: ', req.ip)
     //we're returning the entire database object in the request.
     res.json (db)
 })
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
     const user = db.find(row => row.id === req.params.id)
     user 
         ? res.json(user)
         : res.status(404).json({ message: "The user with the specified ID does not exist." })
 })
 
-app.post('/api/users/', (req, res) => {
+app.post('/users/', (req, res) => {
     if(!req.body.name || !req.body.bio) {
         return res.status(400).json({ error: "Need a user name and bio!" })
     }
@@ -38,7 +43,7 @@ app.post('/api/users/', (req, res) => {
     res.status(201).json(newUser)
 })
 
-app.delete('/api/users/:id', (req, res) => {
+app.delete('/users/:id', (req, res) => {
     const user = db.find(row => row.id === req.params.id)
     user 
         //if I'm doing this correctly, we should get back everything that /doesn't/ match the ID of the user I'm providing.
@@ -50,5 +55,5 @@ const port = 8080
 const host = "127.0.0.1" //this is another way of saying localHost.
 
 app.listen(port, host, () => {
-    console.log(`server running at http://${host}:${port}`)
+    console.log(`server running at http://${host}:${port}/api`)
 })
