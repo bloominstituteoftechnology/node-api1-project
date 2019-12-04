bb4dd7e48825acb0931b0c1ad18eb54bd736d8f9
vy3191 <venkyafhs@gmail.com>
2019-12-03 23:23:15 -0500

    Done with findById

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/api/users", (req,res) => {
          if(response) {
             res.status(200).json(response);
          } else {
-            res.status(404).json({msg:"Not Found"})
+            res.status(404).json({msg:"Users Not Found"})
          }
       })
       .catch( err => {
@@ -26,6 +26,21 @@ app.get("/api/users", (req,res) => {
       })
 });
 
+app.get('/api/users/:id', (req,res) => {
+      const {id} = req.params;
+      db.findById(id)
+        .then(response => {
+             console.log('Line 33', response)
+            if(response.name) {
+              res.status(200).json(response);
+            } else {
+              res.status(400).json({ message: `The user with the specified ${id} does not exist.` });
+            }
+        })
+        .catch(err => {
+              res.status(500).json({ errorMessage: `The user with ${id} information could not be retrieved.` })
+      })
+})
 app.listen(PORT,hostname, () => {
    console.log(`app running at http://${hostname}:${PORT}`);
 })
