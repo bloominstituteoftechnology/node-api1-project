c02f0f6285579f69c51bd64b15d2e64394ffae55
spettigrew <sara.pettigrew5@gmail.com>
2019-12-04 21:27:12 -0700

    added some if/else to .get

--- index.js
+++ index.js
@@ -13,21 +13,29 @@ app.get('/', (req, res) => {
 })
 
 app.get('api/users', (req, res) => {
-    res.json(db)
+    if(user) {
+        res.json(db)
+    } else {
+        res.status(500).json({ error: "The users information could not be retrieved." })
+    }
 })
 
 app.get('api/users/:id', (req, res) => {
     const user = db.find(row => row.id === req.params.id)
-        if (user) {
+        if (user.id) {
+            res.json(user.id)
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        } if (user) {
             res.json(user)
         } else {
-            res.status(404).json({ error: 'User not found' })
+            res.status(500).json({ error: "The user information could not be retrieved." })
         }
 })
 
 app.post('api/users', (req, res) => {
     if (!req.body.name) {
-        return res.status(400).json({ error: 'Need a user name' })
+        return res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
     }
     const newUser = {
         id: String(db.length + 1),
@@ -35,6 +43,10 @@ app.post('api/users', (req, res) => {
     }
     db.push(newUser)
     res.status(201).json(newUser)
+
+    //  else {
+    //     res.status(500).json({ error: "There was an error while saving the user to the database" })
+    // }
 })
 
 // app.put('/users/:id', (req, res) => {
