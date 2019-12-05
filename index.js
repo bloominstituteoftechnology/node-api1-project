8915f8dd5d90dba1245b49dc95b235a7b4c78926
spettigrew <sara.pettigrew5@gmail.com>
2019-12-04 18:01:46 -0700

    packages installed and notes added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // implement your API here
+
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = {
+         id: 1, 
+         name: 'Samwise Gamgee',
+    }
+    // {
+    //     id: 2,
+    //     name: 'Frodo Baggins',
+    // }
+
+    res.status(200).json(hobbits)
+})
+
+server.get('/')
+
+
+server.listen(5000, () => {
+    console.log('API running on port 5000')
+})
