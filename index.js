833b5d2ce179750468849ee390dbbaa726997298
spettigrew <sara.pettigrew5@gmail.com>
2019-12-04 18:40:16 -0700

    .get, .post, .delete added to index.js

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 // implement your API here
 
 const express = require('express')
+let db = require('./data/db')
 
 const app = express()
 
-// server.get('/', (req, res) => {
-//     res.send('Hello World')
-// })
+app.use(epxress.json())
 
-// server.get('/hobbits', (req, res) => {
-    
-// })
+app.get('/', (req, res) => {
+    console.log('ip:', req.ip)
+    res.json({ message: 'Welcome!' })
+})
 
-// server.get('/')
+app.get('/users', (req, res) => {
+    res.json(db)
+})
+
+app.get('/users/:id', (req, res) => {
+    const user = db.find(row => row.id === req.params.id)
+        if (user) {
+            res.json(user)
+        } else {
+            res.status(404).json({ error: 'User not found' })
+        }
+})
+
+app.post('/users', (req, res) => {
+    if (!req.body.name) {
+        return res.status(400).json({ error: 'Need a user name' })
+    }
+    const newUser = {
+        id: String(db.length + 1),
+        name: req.body.name,
+    }
+    db.push(newUser)
+    res.status(201).json(newUser)
+})
+
+// app.put('/')
+
+app.delete('/users/:id', (req,res) => {
+    const user = db.find(row => row.id === req.params.id)
+    if (user) {
+        db = db.filter(row => row.id !== req.params.id)
+        res.json(user)
+    } else {
+        res.status(404).json({ error: 'User not found' })
+    }
+})
 
 const port = 5000
 const host = '127.0.0.1'
