bef918fe635a7a20de85dd5c73f8759468d9ffa7
Raul Ortiz <raulortizars@gmail.com>
2019-12-05 16:21:41 -0600

    2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+
+const express = require("express");
+
+const user = require("./data/db.js");
+
+const port = 5000;
+const host = "127.0.0.1";
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "" });
+});
+
+// Write endpoints to perform the following queries
+// Inside index.js add the code necessary to implement the following endpoints:
+
+//| POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                                              |
+// | GET    | /api/users     | Returns an array of all the user objects contained in the database.                                                               |
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                                                  |
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                                            |
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**. |
