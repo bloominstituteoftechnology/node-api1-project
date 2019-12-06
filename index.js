3fc310dad291d9651c713d6abb90ec00a12d9eb7
spettigrew <sara.pettigrew5@gmail.com>
2019-12-05 18:05:10 -0700

    changes/notes added to index.js .get/.put/.delete

--- index.js
+++ index.js
@@ -7,33 +7,44 @@ const app = express()
 
 app.use(express.json())
 
+// GET is read data
 app.get('/', (req, res) => {
     console.log('ip:', req.ip)
-    res.json({ message: 'Welcome!' })
+    res.json({ message: 'Welcome to Node Express!' })
 })
 
-app.get('api/users', (req, res) => {
-    if(user) {
-        res.json(db)
-    } else {
-        res.status(500).json({ error: "The users information could not be retrieved." })
-    }
-})
+app.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+       res.json(users)
+    })
+        .catch( error => {
+            res.status(500).json({ message: "The users information could not be retrieved."  })
+        })
+}
 
-app.get('api/users/:id', (req, res) => {
-    const user = db.find(row => row.id === req.params.id)
-        if (user.id) {
-            res.json(user.id)
-        } else {
-            res.status(404).json({ message: "The user with the specified ID does not exist." })
-        } if (user) {
-            res.json(user)
-        } else {
-            res.status(500).json({ error: "The user information could not be retrieved." })
+app.get('/api/users/:id', (req, res) => {
+    const user = db.findById(req.params.id)
+    .then(data => {
+        if (data) {
+         res.json(data)
         }
-})
+    } else {
+       res.status(404).json({ error: "The user information could not be retrieved." })
+    }
 
-app.post('api/users', (req, res) => {
+        // if (user.id) {
+        //     res.json(user.id)
+        // } else {
+        //     res.status(404).json({ message: "The user with the specified ID does not exist." })
+        // } if (user) {
+        //     res.json(user)
+        // } else {
+        //     res.status(500).json({ error: "The user information could not be retrieved." })
+        // }
+})
+// POST, create data
+app.post('/api/users', (req, res) => {
     if (!req.body.name) {
         return res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
     }
@@ -48,23 +59,37 @@ app.post('api/users', (req, res) => {
     //     res.status(500).json({ error: "There was an error while saving the user to the database" })
     // }
 })
+// PUT update data
+    app.put('/api/users/:id', (req, res) => {
+     const{name, bio } = req.body{
+        if(!name || !bio) {
+            return res.status(400).json({ error: "Please provide name and bio for the user" })
+        }
+        db.findById(req.params.id)
+        .then(user => {
+            return db.update(req.params.id, { name, bio })
+        } 
+        res.status(404).json({ error: "The user with the specified ID does not exist" })
+        })
+        .then(() => db.findById(req.params.id))
+        .then (data => res.json(data))
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be modified" })
+        })
 
-// app.put('/users/:id', (req, res) => {
-//     .then( => {
-
-// })
-//     .catch( => {
-
-//     })
-
-app.delete('api/users/:id', (req,res) => {
-    const user = db.find(row => row.id === req.params.id)
-    if (user) {
-        db = db.filter(row => row.id !== req.params.id)
-        res.json(user)
-    } else {
-        res.status(404).json({ error: 'User not found' })
-    }
+// DELETE data
+app.delete('/api/users/:id', (req,res) => {
+    db.findById(req.params.id)
+    .then(user => {
+        if(user) {
+            return db.remove{req.params.id}
+        }
+        res.status(404).json({ error: "The user with the specified ID does not exist" })
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        res.status(500).json ({ error: "The user could not be removed"})
+    })
 })
 
 const port = 5000
@@ -73,10 +98,3 @@ const host = '127.0.0.1'
 app.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`)
 })
-
-// .then(response => {
-//             return res.status(200).json({
-//                     url: `/api/users/${id}`,
-//                     user: response
-//                 })
-//         })
