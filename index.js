5688cadff2418ec91689a3d55ff3f7f41554c19f
Robert Gordon <Komeuppance@gmail.com>
2020-01-01 21:35:58 -0800

    Initial server setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+let db = require('./data/db');
+
+const app = express();
+app.use(express.json());
+
+const hostname = '127.0.0.1';
+const port = 5001;
+
+app.get('/api/users', (req, res) =>{
+    console.log('remote:', req.ip)
+    res.json(db.find())
+})
+
+app.listen(port, hostname, ()=>{
+    console.log(`Express Server running at http://${hostname}:${port}`);
+})
