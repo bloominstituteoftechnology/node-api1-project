458eed5365717bd8808f84f9c232a1b6f153df4f
Robert Gordon <Komeuppance@gmail.com>
2020-01-01 21:45:34 -0800

    Get by id request working

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const port = 5001;
 
 app.get('/api/users', (req, res) =>{
     console.log('remote:', req.ip)
-    res.json(db.find())
+    db.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'No data found'})
+        })
+})
+
+app.get('/api/users/:id', (req, res) =>{
+    console.log('remote:', req.ip)
+    db.findById(req.params.id)
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'No data found'})
+        })
 })
 
 app.listen(port, hostname, ()=>{
