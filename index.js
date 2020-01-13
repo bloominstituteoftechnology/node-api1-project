d0df628cb2209dcdcc2d86364caab72edc49c641
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-13 17:02:43 -0500

    edit, add, get by id..notworking

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //import express from 'express'
 const express = require('express');
 
-const Users = require('./data/db.js');
+const Users = require('./data/db');
 
 const server = express();
 
@@ -13,13 +13,12 @@ server.use(express.json()); //needed to parse JSON
 //routes/endpoints
 
 //GET to '/'
-// server.get('/', function(request,response){
-//     response.send({hello: 'web 25!, welcome to the Backend!'});
+// server.get('/', function(request,res){
+//     res.send({hello: 'web 25!, welcome to the Backend!'});
 // })
 
 //GET list of Users
 server.get('/api/users', (req, res) => {
-// read data from database(Users)
 Users.find() //reutrn a promise
 .then(users => {
     res.status(200).json(users);
@@ -33,35 +32,36 @@ Users.find() //reutrn a promise
 
 //Get user by ID
 server.get('/api/users/:id', (req, res) => {
-    Users.find() //reutrn a promise
+    Users.findById() //reutrn a promise
     .then(users => {
         res.status(200).json(users);
     })
     .catch(er => {
         console.log(er)
         //handle the error
-        res.status(500).json({errorMessage: 'sorry, error getting Users List'})
+        res.status(500).json({errorMessage: "The user information could not be retrieved."})
       })
     })
 
 
 //create a USER
 server.post('/api/users', (req, res) => {
-    const hubData = req.body;
-    // never trust the client, validate the data. for now we trust the data for the demo
-    Users.add(hubData)
+    const userData = req.body;
+    // never trust the client, validate the data.
+    Users.insert(userData)
       .then(user => {
         res.status(201).json(user);
       })
       .catch(error => {
         console.log(error);
         // handle the error
+        res.status(400).json({
+            errorMessage: "ADD USER - Please provide name and bio for the user."});
         res.status(500).json({
-          errorMessage: 'sorry, we ran into an error creating the user',
+            errorMessage: "There was an error while saving the user to the database",
         });
-      });
   });
-  
+})
   //delete a User
 
   server.delete('/api/users/:id', (req,res)=> {
@@ -70,30 +70,42 @@ server.post('/api/users', (req, res) => {
       .then(deleted => {
     // res.status(204).end();
     res.status(200).json(deleted);
+    res.status(404).json({
+            message: "The user with the specified ID does not exist.",
+          });
+
       })
+        // 
       .catch(error => {
         console.log(error);
         // handle the error
+       
         res.status(500).json({
           errorMessage: 'error removing the user',
         });
       });
   })
 
-  //update a Hub : extra exercise
+
+  //update a User: extra exercise
 
 // .put
+
 server.put('/api/users/:id', (request, response) => {
     const id = request.params.id;
     const userData = request.body;
     Users.update(id, userData)
     .then(updated => {
-       response.status(204).json(updated)
+        response.status(200).json(updated)
+        //response.status(204).json(updated)
+        response.status(404).json({message: "The user with the specified ID does not exist."}) 
     })
     .catch(error => {
         console.log(error);
         //handle the error
-        response.status(500).json({errorMessage: 'Sorry, we ran into an error updating the users.'})
+        response.status (400).json({ errorMessage: " EDIT USER - Please provide name and bio for the user." })
+        response.status(500).json({ errorMessage: "The user information could not be modified." })
+        
     })
 })
 
