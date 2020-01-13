eb64c471d02f6532c6957fffe588317cc8b4e760
jordan <jordan@jordans-MacBook-Air.local>
2020-01-13 20:54:04 +0700

    all requests made

--- index.js
+++ index.js
@@ -24,4 +24,83 @@ server.post("/api/users", (req, res) => {
       .json({ errorMessage: "Please provide name and bio for the user." });
 });
 
-// get request
+// get requests
+
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
+    });
+});
+
+// Delete request
+
+server.delete("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  Users.remove(userId)
+    .then(user => {
+      if (user && user > 0) {
+        res.status(200).json({
+          message: "the user was deleted."
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "The user could not be removed" });
+    });
+});
+
+// Put request
+
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: "The user information could not be modified."
+        });
+      });
+  }
+});
