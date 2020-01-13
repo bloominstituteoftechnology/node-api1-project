302bc99268299ff3fdc8fa894bc0fe300819370c
jarrodskahill <jarrodskahill@gmail.com>
2020-01-13 13:34:12 -0800

    created get for getting user by id

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const server = express();
 const Db = require("./data/db")
+
+//
+
 server.use(express.json());
 
 
@@ -10,8 +13,10 @@ server.get("/", function(request, response) {
 })
 
 server.post("/api/users", (req, res) => {
-    const userData = req.body;
-    Db.insert(userData)
+    const {name, bio} = req.body;
+     if (!name || !bio)
+     return res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+    Db.insert({name, bio})
     .then(user => {
         res.status(201).json(user);
     })
@@ -19,6 +24,31 @@ server.post("/api/users", (req, res) => {
         console.log(error)
         res.status(400).json({errorMessage: "Please provide name and bio for the user." })
     })
+})
+
+server.get("/api/users", (req, res) => {
+    Db.find()
+    .then(allUsers => {
+        console.log('All Users', allUsers)
+        res.status(200).json(allUsers)
+    })
+    .catch(error => {console.log(error);
+    res.status(500).json({errorMessage: "The users information could not be retrieved."})}
+    )
+})
+
+server.get("/api/users/:id", (req, res)=> {
+    const id = req.params.id
+    Db.findById(id)
+    .then(user => {
+        if(user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {console.log(error);
+    res.status(404).json({ message: "The user with the specified ID does not exist." })})
 })
 
 
