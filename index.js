4249677239c93cf74c5b7002f367178c4096da97
AbdelI <homeworkoracle@protonmail.com>
2020-01-13 15:01:14 +0100

    put request working

--- index.js
+++ index.js
@@ -77,3 +77,19 @@ app.delete("/api/users/:id", (req, res) => {
 app.listen(8000, () => {
   console.log("listening on 8000");
 });
+
+app.put("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const modifiedUser = req.body;
+
+  try {
+    const updatedUser = await update(id, modifiedUser);
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: `User with id ${id} does not exist` });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
