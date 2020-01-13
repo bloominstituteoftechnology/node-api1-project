2edb82979163b091799e4dc2c8ce6180409c6137
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-13 17:00:42 -0500

    delete functionality

--- index.js
+++ index.js
@@ -27,18 +27,33 @@ server.get("/api/users", (req, res) => {
 
 // create a new user
 server.post("/api/users", (req, res) => {
-    const userData = req.body
+    const userData = req.body;
     Data.insert(userData) // passing in the request body
     .then(newUser => {
-        res.status(201).json(newUser)
+        res.status(201).json(newUser);
     })
     .catch(err => {
         console.log(err);
         res.status(400).json({
-            errorMessage: "Please provide name and bio for the user."
+            errorMessage: "Please provide name and bio for the user"
+        });
+    });
+});
+
+// deletes a user
+server.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    Data.remove(id)
+    .then(deleted => {
+        res.status(200).json(deleted);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(404).json({
+            errorMessage: "The user with the specific ID does not exist"
         })
     })
-})
+});
 
 const port = 7000;
 server.listen(port, () => console.log(`server listening on port ${port}`));
