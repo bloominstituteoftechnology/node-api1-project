93032088f3ff48a86c6a41e81b237f23716c8564
Babatunde <babatundea15@gmail.com>
2020-01-13 15:29:22 +0100

    I worked on getting all users

--- data/db.js
+++ data/db.js
@@ -9,29 +9,29 @@ module.exports = {
   update,
   remove,
 };
-
+// GET /users
 function find() {
   return db('users');
 }
-
+// GET /users/2
 function findById(id) {
   return db('users')
     .where({ id: Number(id) })
     .first();
 }
-
+// POST /users {payload}
 function insert(user) {
   return db('users')
     .insert(user)
     .then(ids => ({ id: ids[0] }));
 }
-
+// PUT /users/id/{payload}
 function update(id, user) {
   return db('users')
     .where('id', Number(id))
     .update(user);
 }
-
+// DELETE /users/id
 function remove(id) {
   return db('users')
     .where('id', Number(id))
--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 // implement your API here// install express and cors (npm packages)
 // STEP 1: flesh out a dummy server
 // STEP 2: flesh out the five endpoints (will use those helpers)
-// imports 
-const express = require('express')
-const cors = require('cors')
+// imports
+const express = require("express");
+const cors = require("cors");
+
+const { find, findById, insert, update, remove } = require("./data/db");
 
 // instantiate an express app
-const app = express()
+const app = express();
 // plug extra functionality to our app
 // we need to be able to read req.body
-app.use(express.json())
+app.use(express.json());
 // we need to enable CORS so this server works for all origins
-app.use(cors())
+app.use(cors());
+
+app.get("/users", (req, res) => {
+  // Get all users, we do not need any extra information such as id.
+  find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack
+      });
+    });
+});
+// we need code to spin up the server and just have it listen for incoming
+app.listen(3000, () => {
+    console.log('listening on 3000');
+  })
