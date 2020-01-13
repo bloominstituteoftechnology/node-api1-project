28c3146d17fe717d6e5e720fd38cc2362bdab686
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-13 15:19:00 +0000

    Delete is working

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 app.get('/api/users', (req, res) => {
     find()
         .then(users => {
+            
             res.status(200).json(users)
         })
         .catch(error => {
@@ -35,6 +36,41 @@ app.get('/api/users', (req, res) => {
                 stack: error.stack,
             })
         })
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    remove(id)
+        .then(data => {
+            if(data) {
+                res.status(202).json(`user with id ${id} got deleted`)
+            } else {
+                res.status(404).json( `The user with the specified id ${id} does not exist.`)
+            }
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+
+});
+
+app.post('/api/users', (req, res) => {
+    
+    const {name, bio} = req.body;
+
+    if(!name || !bio) {
+        res
+            .status(400)
+            .json({errorMessage: "Please provide name and bio for the user."});
+    } else {
+    insert(user)
+        .then( users => {
+            res.status(201).json(users)
+    })
+        .catch(error => {
+            res.status(500).json({errorMessage: "There was an error while saving the user to the database", error});
+        })
+}
 });
 
 app.listen(PORT, hostname, () => {
