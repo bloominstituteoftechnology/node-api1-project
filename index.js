f3cbb5910836df532cff39454cdce517bebc1f13
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-13 16:12:58 -0500

    server with express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+    res.send("Hello world from Express!");
+  });
+
+const port = 7000;
+server.listen(port, () => console.log(`server listening on port ${port}`));
