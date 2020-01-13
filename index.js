e643ecb7050b9fd2484978972231ab84bbe15052
jarrodskahill <jarrodskahill@gmail.com>
2020-01-13 12:25:56 -0800

    created a post adding users to the data base

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // implement your API here
 const express = require('express');
 const server = express();
+const Db = require("./data/db")
 server.use(express.json());
 
+
 server.get("/", function(request, response) {
-    response.send({})
+    response.send({test:'This is a test'})
 })
+
+server.post("/api/users", (req, res) => {
+    const userData = req.body;
+    Db.insert(userData)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+    })
+})
+
+
 const port = 8000;
 
 server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`))
