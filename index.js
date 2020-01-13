7259d963037760ec1c95b2fc2caa0472fd04c278
niklasbec <niklasbeckerr@gmail.com>
2020-01-13 13:32:41 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+
+const express = require('express')
+const cors = require('cors')
+
+const { find, findById, insert, update, remove } = require('./data/db')
+
+const server = express()
+
+server.use(express.json())
+
+server.use(cors())
+
+server.get('/hubs', (req, res) => {
+    find()
+    .then(hubs => {
+        console.log(res)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+})
