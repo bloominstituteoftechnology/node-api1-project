fd43aa2d99f7dfe2c35652f8685ffcba4ee36fda
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-01-13 12:42:33 +0000

    Initial commit get request working

--- data/db.js
+++ data/db.js
@@ -9,29 +9,29 @@ module.exports = {
   update,
   remove,
 };
-
+//get
 function find() {
   return db('users');
 }
-
+//get
 function findById(id) {
   return db('users')
     .where({ id: Number(id) })
     .first();
 }
-
+//post
 function insert(user) {
   return db('users')
     .insert(user)
     .then(ids => ({ id: ids[0] }));
 }
-
+//put
 function update(id, user) {
   return db('users')
     .where('id', Number(id))
     .update(user);
 }
-
+//delete
 function remove(id) {
   return db('users')
     .where('id', Number(id))
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+
+//imports
+const express = require('express');
+const cors = require('cors');
+const PORT = 5025;
+const hostname ='127.0.0.1';
+
+const { find, findById, insert, update, remove} = require ('./data/db');
+
+// express app
+
+const app = express()
+
+// extra functionality we need to be able to read req.body
+app.use(express.json())
+
+//enable cors
+
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({"message": "App runs"});
+});
+
+app.listen(PORT, hostname, () => {
+    console.log(`App running http://${hostname}:${PORT}`)
+})
+
