66c04d33fd995025050f7851939a803e82a4da08
AbdelI <homeworkoracle@protonmail.com>
2020-01-13 14:00:51 +0100

    perfomring crud operations,get initial array

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // implement your API here
+
+const express = require("express");
+const cors = require("cors");
+
+const { findById, find, add, remove, update } = require("./data/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+  find()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack
+      });
+    });
+});
+
+app.listen(8000, () => {
+  console.log("listening on 8000");
+});
