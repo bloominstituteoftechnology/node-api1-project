b19b35fc3fe1c8d818b0a5b6ba26ba10006a376a
Joseph Graham <Joseph@Chasingthefiks.net>
2020-01-13 16:16:25 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// implement your API here
+console.log("Running");
+
+const express = require("express");
+
+const Hubs = require("./data/hubs-model.js");
+
+const server = express();
+const port = 8000;
+server.use(express.json());
+
+//get users
+server.get("/api/users", (req, res) => {
+    Hubs.find()
+        .then(hub => {
+            res.status(201).json(hub);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                errorMessage: "ERROR RETURNING USER LIST"
+            });
+        });
+});
+//post users
+server.post("/api/users", (req, res) => {
+    const hubData = req.body;
+    Hubs.add(hubData)
+        .then(hub => {
+            res.status(201).json(hub);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                errorMessage: "ERROR ADDING USER"
+            });
+        });
+});
+
+server.get("/", function(req, res) {
+    res.send({ hello: "Web 25!" });
+});
+
+//delete user
+server.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+
+    Hubs.remove(id)
+        .then(deleted => {
+            // res.status(204).end();
+            res.status(200).json(deleted);
+        })
+        .catch(error => {
+            console.log(error);
+            // handle the error
+            res.status(500).json({
+                errorMessage: "ERROR DELETING USER"
+            });
+        });
+});
+
+server.listen(port, () => console.log(`\n ** api on port: ${port} **\n`));
