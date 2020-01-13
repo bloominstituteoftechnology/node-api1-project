addbaa12267a5a89f95a1ea30f283a2cecbca595
jarrodskahill <jarrodskahill@gmail.com>
2020-01-13 12:03:31 -0800

    implemented basics to run API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+server.get("/", function(request, response) {
+    response.send({})
+})
+const port = 8000;
+
+server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`))
