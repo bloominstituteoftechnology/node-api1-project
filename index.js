3bc31a4dc8017006d2edf67a832c1371599b2767
wais <waisalmakaleh@gmail.com>
2020-01-13 22:23:43 -0500

    find is working

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+const express = require( 'express' );
+const server = express();
+const Db = require( './data/db' );
+server.use( express.json() );
+
+
+server.get( '/', function ( request, response )
+{
+    response.send( { name: "Wais Almakaleh" } );
+}
+
+);
+
+
+server.get('/api/db', (req, res)=> {
+    Db.find().then(db => {
+        console.log(res)
+        res.status(200).json(db)
+    })
+})
+
+
+const port = 8000;
+server.listen( port, () => console.log( `\n ** api on port: ${ port } ** \n` ) );
+
