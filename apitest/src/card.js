8d9c69c3389ec3eb74f1ad4dfd8b02d74cfabb24
jarrodskahill <jarrodskahill@gmail.com>
2020-01-13 17:09:25 -0800

    added a delete button

--- apitest/src/card.js
+++ apitest/src/card.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
 const Card = ({data}) => {
+
+    const deleteData = e => {
+        axios
+        .delete(`http://localhost:8000/api/users/${data.id}`)
+    }
     return (
         <div className='card'>
             <h2>Name: {data.name}</h2>
             <p>Bio: {data.bio}</p>
             <p>Date: {data.created_at}</p>
+            <button className="delete" onClick={deleteData} >Delete</button>
         </div>
     )
 }
