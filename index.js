5f09097d69f800974cb1c13cb03993c2b2f0db3b
Anthony Lopez <anthonybeconscious@gmail.com>
2020-01-14 00:29:12 -0600

    mvp

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ server.get('/', (req, res)=>{
     res.send('LambdaSchool Project')
 });
 
-server.post('api/users', (req, res)=>{
+server.post('/api/users', (req, res)=>{
     const userInfo = req.body;
 
     console.log("user info", userInfo);
     if (!userInfo.name || !userInfo.bio) {
-        res.status(400).json({errorMessage: "Name and bio needed"})
+        res.status(400).json({ errorMessage: "Name and bio needed" })
         console.log("400 please provide name and bio for the user")
-    }else{
+    } else {
         db.insert(userInfo)
         .then(user =>{
             res.status(201).json(user)
@@ -28,7 +28,53 @@ server.post('api/users', (req, res)=>{
     }
 });
 
-server.get('api/users')
+
+server.get('/api/users', (req, res)=>{
+    db.find()
+    .then(users =>{
+        res.status(200).json(users)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({error: 'Could not retrieve user info'})
+    })
+})
+
+server.delete('/api/users/:id', (req, res)=>{
+    const id = req.params.id;
+
+    db.remove(id)
+    .then(count => {
+        count === 1 && res.status(200).json({ message: `User Deleted` })
+        count !== 1 && res.status(400).json({ message: 'The user with this ID doesnt exist' })
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({ error: 'The user couldnt be removed' })
+    })
+})
+
+server.put('/api/users/:id', (req, res)=>{
+    const id = req.params.id
+    const newUser = req.body
+
+    if (!newUser.name || !newUser.bio){
+        res.status(400).json({ errorMessage: 'Please provide name and bio for the user' })
+    }else{
+        db.update(id, newUser)
+        .then(user =>{
+            if (user) {
+                res.status(200).json(user)
+            }else{
+                res.status(404).json({ message: 'The user with the specified ID does not exist.' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'The user info couldnt be modified' })
+        })
+    }
+})
 
 
 const port = 8000;
