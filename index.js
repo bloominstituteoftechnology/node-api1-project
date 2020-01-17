ab24dbad2a1728bd39955568e7c51712e747f0c5
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-17 10:20:59 -0800

    server created on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const db = require('./data/db.js');
+
+const express = require('express');
+
+const server = express();
+
+server.listen(5000, () => {
+    console.log('=== server listening on port 5000 ===');
+});
+
+server.get('/', (req, res) => {
+    res.send('Hello World..')
+})
