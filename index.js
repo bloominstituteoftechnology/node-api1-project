2f7a8da1a9ce6bd7da4ea62cdf4e3da9a0ddaab1
Hiter Harris <hiterharris@comcast.net>
2020-02-10 15:22:53 -0500

    GET, POST, DELETE, PUT complete

--- index.js
+++ index.js
@@ -18,5 +18,42 @@ server.get('/api/users', (req, res) => {
         .catch(error => console.log(error))
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    Db.findById(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => console.log(error))
+})
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    Db.insert(userInfo)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(error => console.log(error))
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    Db.remove(id)
+        .then(remove => {
+            req.status(200).json(remove);
+        })
+        .catch(error => console.log(error))
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    Db.update(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => console.log(error))
+})
+
+
 const port = 5000;
 server.listen(port, () => console.log(`Server listening on port ${port}`));
