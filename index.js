cfbc2df9423441f4000f2d087caa90551ca1f43e
wesley.moody <wgm3190@gmail.com>
2020-02-10 15:20:29 -0600

    one typo

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ server.get("/api/users", (req, res) => {
     });
 });
 
-server.get("/api/users/:id", (rec, res) => {
+server.get("/api/users/:id", (req, res) => {
   const id = req.params.id;
   Db.findById(id)
     .then(user => {
@@ -56,33 +56,3 @@ server.get("/api/users/:id", (rec, res) => {
 
 const port = 5001;
 server.listen(port, () => console.log(`n** API on port ${port} \n`));
-
-// add a hub
-// axios.post(url, data, options); the data will be in body of the request
-
-//   console.log(body, req);
-
-// // view a list of hubs
-// server.get("/api/hubs", (req, res) => {
-//   //got and get the hubs from the DB
-//   Hubs.find()
-//     .then(hubs => {
-//       res.status(200).json(hubs);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({ errorMessage: "oops" });
-//     });
-// });
-
-//   const hubInfo = req.body;
-//   // validate the data, and if the data is valid save it
-//   Hubs.add(hubInfo)
-//     .then(hub => {
-//       res.status(201).json(hub);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({ errorMessage: "oops" });
-//     });
-// });
