fe7d428a8c4a84d314a5a0560e77a0cbebda58b6
Hiter Harris <hiterharris@comcast.net>
2020-02-10 15:08:58 -0500

    project set up, get request

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// implement your API here
+const express = require('express');
+
+const Db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({response: 'Hello world!!'})
+})
+
+server.get('/api/users', (req, res) => {
+    Db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => console.log(error))
+})
+
+const port = 5000;
+server.listen(port, () => console.log(`Server listening on port ${port}`));
