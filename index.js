fc3a27684e2d17f8a7d7a9f553c597c77f32c340
wesley.moody <wgm3190@gmail.com>
2020-02-10 15:18:35 -0600

    created getUserById

--- index.js
+++ index.js
@@ -11,14 +11,46 @@ server.get("/", (req, res) => {
   res.json({ hello: "Web 26" });
 });
 server.post("/api/hubs", (req, res) => {
-  const userData = req.body;
-  Db.insert(userData)
+  const { name, bio } = req.body;
+  if (!name || !bio)
+    return res
+      .status(400)
+      .json({ errorMessage: "Name and bio required for user" });
+  Db.insert({ name, bio })
     .then(user => {
       res.status(201).json(user);
     })
     .catch(error => {
       console.log(error);
       res.status(400).json({ errorMessage: "Name and bio required for user" });
+    });
+});
+
+server.get("/api/users", (req, res) => {
+  Db.find()
+    .then(allUsers => {
+      console.log("All Users", allUsers);
+      res.status(200).json(allUsers);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: "Could not retrieve user info." });
+    });
+});
+
+server.get("/api/users/:id", (rec, res) => {
+  const id = req.params.id;
+  Db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ errorMessage: "No user exists with that ID" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({ errorMessage: "No user exists with that ID" });
     });
 });
 
