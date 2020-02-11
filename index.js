ddf57a446e5324f147b7803d533846e8516fcc57
ElkBranch <elggren.spencer@gmail.com>
2020-02-11 13:12:04 -0700

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+const Users = require('./data/db');
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+    Users.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err => console.log(err))
+});
+
+server.get('/api/users/:id', (req, res) => {
+    Users.findById(':id')
+})
+
+const port = 5000;
+server.listen(port, () => console.log('Hurray!'));
