ee1aaa97ebc2901cd7a11c108c071ac0352d918c
wesley.moody <wgm3190@gmail.com>
2020-02-11 14:29:53 -0600

    guided project

--- node-api1-project/src/App.js
+++ node-api1-project/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Card from './Card';
-import Users from './Users';
-import './App.css';
+import React from "react";
+import Card from "./Card";
+import Users from "./Users";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Users</h1>
       <Users />
       <Card />
--- node-api1-project/src/Card.js
+++ node-api1-project/src/Card.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-const Card = ({data}) => {
-
+const Card = ({ data }) => {
   const deleteData = e => {
-    axios
-    .delete(`http://localhost:5001/api/users/${data.id}`)
-  }
+    axios.delete(`http://localhost:5001/api/users/${data.id}`);
+  };
   return (
     <div className='card'>
-      {/* <h2>Name: {data.name}</h2>
+      <h2>Name: {data.name}</h2>
       <p>Bio: {data.bio}</p>
-      <p>Date: {data.created_at}</p> */}
-      <button className='delete' onClick={deleteData}>Delete</button>
+      <p>Date: {data.created_at}</p>
+      <button className='delete' onClick={deleteData}>
+        Delete
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
