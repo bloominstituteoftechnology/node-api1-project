e2669e1bd99826e3d267745b0c270a999eb7c277
wesley.moody <wgm3190@gmail.com>
2020-02-10 18:30:30 -0600

    pulling user info now need to render for stretch

--- node-api1-project/src/App.js
+++ node-api1-project/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './Card';
+import Users from './Users';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Puppies are cute</h1>
-      <Card></Card>
+      <h1>Users</h1>
+      <Users />
+      <Card />
     </div>
   );
 }
--- node-api1-project/src/Users.js
+++ node-api1-project/src/Users.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Card from './Card';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
 
-const User  = () => {
-  const [user, setUser] = useState([])
+const User = () => {
+  const [user, setUser] = useState([]);
   useEffect(() => {
-    axios.get(`http://localhost:5001/api/users`)
-    .then(res => {
-      console.log(res)
-      setUser(res.data)
-    })
-  }, 0)
+    axios.get(`/api/users`).then(res => {
+      console.log(res);
+      setUser(res.data);
+    });
+  }, []);
 
-    return (
-      <div className='cardHolder'>
-        {user.map(user => (
-          <div key={user.id}>
-            <Card data={user} />
-          </div>
-        ))}
-      </div>
-    )
-}
+  return (
+    <div className='cardHolder'>
+      {user.map(user => (
+        <div key={user.id}>
+          <Card data={user} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default User;
