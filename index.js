eee06b4a356abf0530a6f74d4fbb042d2c94aab0
Alison Law <alisparklaw@gmail.com>
2020-03-09 13:12:23 +0000

    post/insert/add

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const helpers = require('./data/db.js')
+const helpers = require("./data/db.js");
 // we will write our API with Express (barebones lib for building web servers - Sinatra)
 // STEP 1, install express
 // STEP 2, import express, commonjs syntax instead of native ES modules
-const express = require('express')
+const express = require("express");
 // STEP 3 instantiate app
-const app = express()
+const app = express();
 // STEP 4 "turn on" the ability of the app to read req.body as json
-app.use(express.json())
+app.use(express.json());
 // STEP 5 decide a port number
-const PORT = 3333
+const PORT = 3333;
 // STEP 6 make an endpoint on "/hello" [GET] that sends back a json { "message": "hello" }
-app.get('/hello', (
-  req, // req is the "objectified" version of the actual http request (piece of paper with 3 parts...)
-  res, // res is the toolbox that allows to shape a response and send it back to the client
-) => {
-  // set a success code of 200
-  // send back a json response
-  console.log(req.headers);
-  res
-    .status(200)
-    .json({ message: 'hello' })
-})
+app.get(
+  "/hello",
+  (
+    req, // req is the "objectified" version of the actual http request (piece of paper with 3 parts...)
+    res // res is the toolbox that allows to shape a response and send it back to the client
+  ) => {
+    // set a success code of 200
+    // send back a json response
+    console.log(req.headers);
+    res.status(200).json({ message: "hello" });
+  }
+);
 // STEP 6.5: build an endpoint [GET] "/users" and returns ALL users in the database
 // we will take advantage of the "find" helper from the db.js file inside of data
-app.get('/users', async (req, res) => {
-  const users = await helpers.find()
+app.get("/users", async (req, res) => {
+  const users = await helpers.find();
   console.log(users);
-  res
-    .status(200)
-    .json({ users })
-})
+  res.status(200).json({ users });
+});
 // STEP 6.6 build and endpoint [GET] "/users/:id"
 // sends back the user with the given id
 // we can find the id inside the req object
 // the id can be found inside req.params.id
 app.get("/users/:id", (req, res) => {
-  const { id } = req.params
-  helpers.findById(id)
+  const { id } = req.params;
+  helpers
+    .findById(id)
     .then(user => {
       if (!user) {
-        res.status(404).json({ message: 'No user with id ' + id })
+        res.status(404).json({ message: "No user with id " + id });
       } else {
-        console.log(user)
-        res.status(200).json(user)
+        console.log(user);
+        res.status(200).json(user);
       }
       // IMPORTANT COMMENT
       // if we forget to res.json at some point
@@ -53,17 +53,33 @@ app.get("/users/:id", (req, res) => {
     })
     .catch(error => {
       console.log(error);
-    })
+    });
   // the id can be found inside req.params.id
-})
+});
 // STEP 6.7 build a [POST] on "/users"
 // to create a new user using the body of the request
-app.post('/users', (req, res) => {
-  // when a client does 
+app.post("/users", async (req, res) => {
+  // when a client does
   // axios.post('http://localhost:3333/users', user).then().catch()
-  const payload = req.body
-})
+
+  const payload = req.body;
+  helpers
+    .insert(payload)
+    .then(user => {
+      if (!user) {
+        res.staus(404).json({ message: "fail!" + id });
+      } else {
+        res.status(200).json({ message: "added to DB" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "could not create user"
+      });
+    });
+});
 // STEP 7 make the express app listen on PORT
 app.listen(PORT, () => {
-  console.log(`Great! Listening on ${PORT}`)
-})
+  console.log(`Great! Listening on ${PORT}`);
+});
