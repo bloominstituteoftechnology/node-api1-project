7df64eb18a003a65636432452c0da848ca1b3be6
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-03-09 15:12:40 -0500

    GET/POST

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const shortid = require("shortid");
+
+const server = express();
+const PORT = 5000;
+
+let users = []
+
+server.use(express.json());
+
+server.get("/hello", (req, res) => {
+  res.status(201).json({ hello: "Jason" });
+});
+
+server.post("/api/users", (req, res) => {
+    const usersInfo = req.body
+    usersInfo.id = shortid.generate()
+    users.push(usersInfo)
+    res.status(201).json(usersInfo)
+})
+
+server.get("/api/users", (req, res) => {
+    res.status(200).json(users)
+})
+
+server.listen(PORT, () =>
+  console.log(`\n ** API on http://localhost:${PORT}  **\n`)
+);
