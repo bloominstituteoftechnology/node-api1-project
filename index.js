f8d874acc6afce5669a113848e1392b91b5c7f85
alexis villaraza <alexis.borja@gmail.com>
2020-03-09 13:48:46 -0700

    added post api/users

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const shortid = require("shortid");
+
+const server = express();
+
+let users = [];
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running..." });
+});
+
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  userInfo.id = shortid.generate();
+  users.push(userInfo);
+  res.status(201).json(userInfo);
+});
+
+const PORT = 5000;
+server.listen(PORT, () =>
+  console.log(`/n ** API on http://localhost:${PORT} **\n`)
+);
