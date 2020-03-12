352b3c5f5c22ef278763a1279a0a438d0a93088e
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-03-11 21:44:42 -0700

    Initial commit.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.listen(4000, () => {
+    console.log('server listening on port 4000...');
+});
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Hello beautiful!')
+});
