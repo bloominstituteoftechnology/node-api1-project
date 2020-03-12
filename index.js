5020523ba2a19624be5627fd38a9e656b72ae4a0
Wade-Coplen <wadecoplen@gmail.com>
2020-03-12 10:44:43 -0700

    III- PUT working and name updated in object. Message 200

--- index.js
+++ index.js
@@ -38,6 +38,37 @@ server.post('/users', (req, res) => {
     })
 })
 
+server.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
+    db.remove(id)
+    .then(deleted => {
+        if(deleted) {
+            res.status(204).end();        
+        } else {
+            res.status(404).json({message: "The user with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "The user could not be removed"})
+    })
+})
 server.put('/users/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
 
+    db.update(id, changes)
+    .then(updated => {
+        if(updated) {
+            res.status(200).json({success: true, updated});        
+        } else if (updated !== changes.id) {
+            res.status(404).json({message: "The user with the specified ID does not exist." })
+        } else if(updated !== changes.name && changes.bio) {
+            res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+        } else if(updated !== changes) {
+            res.status(500).json({})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "The user could not be removed"})
+    })
 })
