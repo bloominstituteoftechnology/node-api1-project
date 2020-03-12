344304124bfd4725c6318f984cf98f3e101b3727
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-03-11 21:53:34 -0700

    Implemented a GET request to /api/users

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send('Hello beautiful!')
 });
+
+//When the client makes a GET request to /api/users
+//Return an array users.
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(user => {
+            res.status(200).json({user})
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err});
+        });
+});
