d3fb203fd633f759e56407074d50428fdc261357
frances-hansen <francesjuniperhansen@gmail.com>
2020-04-06 12:54:08 -0700

    user id get request

--- index.js
+++ index.js
@@ -24,5 +24,21 @@ server.get("/", (req, res) => {
 
 server.get("/users", (req, res) => {
     res.json(users);
+    if (users) {
+        res.status(200).json(users);
+    } else {
+        res.status(500).json({errorMessage: "The users information could not be retrieved."})
+    }
+});
+
+server.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const user = users.find((user) => user.id == id);
+
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(500).json({errorMessage: "The users information could not be retrieved."})
+    }
 });
 
