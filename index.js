bf95a494d24a0e7b2fbbcbe21b2279d0ead411ab
JW Nicholson <john.nicholson812@gmail.com>
2020-04-06 15:35:28 -0400

    verified server is running on port 5000

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const shortid = require('shortid');//genrate unique id's
-
 //console.log(shortid.generate());
+
+const server = express();
+
+
+
+
+
+const port = 5000;//run server on port 5000
+server.listen(port, () => console.log(`\n***** api on port ${port} *****\n`));
