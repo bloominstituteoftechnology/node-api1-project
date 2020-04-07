b2e19d2e358215624bd925208fe139e66d08f2dc
Gregory Hawman <g.hawman@gmail.com>
2020-04-07 16:12:50 -0700

    the .post works now

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ let users = [
 
 server.use(express.json());
 
+// .GETs
 server.get('/', (req, res) => {
     res.json({ api: 'hi from the api' });
 });
@@ -32,7 +33,7 @@ server.get('/api/users', (req, res) => {
     if(users) {
         res.status(200).json(users);
     } else {
-        res.status(404).json({ errorMessage: 'Users not found' });
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." });
     }
 });
 
@@ -43,11 +44,35 @@ server.get('/api/users/:id', (req, res) => {
 
     if(user) {
         res.status(200).json(user);
+    } else if (user) {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." });
     } else {
-        res.status(404).json({ message: "The user with the specified ID does not exist." });
+        res.status(404).json({ errorMessage: "The user with the specified ID does not exist." });
     }
 });
 
+
+// .POSTs
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    const { name, bio } = req.body;
+
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else if (userInfo) {
+        users.push(userInfo);
+        res.status(201).json(users);
+    } else  {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" });
+    }
+});
+
+// DELETEs
+server.post('/api/users', (req, res) => {
+
+});
+
+// PUTs
 server.post('/api/users', (req, res) => {
 
 });
