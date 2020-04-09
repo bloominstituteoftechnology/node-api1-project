f6d69f9aa001aa7bd7841180177e9f8b17a32a38
Michael Lowe <mrlowememphis@yahoo.com>
2020-04-09 15:23:57 -0500

    datsbase.js added

---
+++ database.js
@@ -0,0 +1,47 @@
+const shortid = require("shortid")
+
+let users = [
+	{ id: "1", name: "Jane Doe", bio: "Not Tarzan's Wife, another Jane" },
+	{ id: "2", name: "John Doe", bio: "A man with no idea where he came from" },
+	{ id: "3", name: "Jack Doe", bio: "Johns long lost father" },
+]
+
+function getUsers() {
+	return users
+}
+
+function getUserById(id) {
+	return users.find(u => u.id === id)
+}
+
+function createUser(data) {
+	const payload = {
+		id: String(users.length + 1),
+		...data,
+	}
+
+	users.push(payload)
+	return payload
+}
+
+function updateUser(id, data) {
+	const index = users.findIndex(u => u.id === id)
+	users[index] = {
+		...users[index],
+		...data,
+	}
+
+	return users[index]
+}
+
+function deleteUser(id) {
+	users = users.filter(u => u.id != id)
+}
+
+module.exports = {
+	getUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+} 
