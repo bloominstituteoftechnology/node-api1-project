39318f75dfeea0739af86ca62aeb6601041b0aec
Your name <a.m.verdine@gmail.com>
2020-04-10 13:20:13 -0400

    Build out getUser & getUserById request handlers/Successfully test getUser & getUserById handlers with Insomnia

---
+++ database.js
@@ -0,0 +1,45 @@
+let users = [
+  { id: "1", name: "Jane Doe", bio: "Not Tarzan's Wife, another Jane" },
+  { id: "2", name: "John Doe", bio: "Last name Doe...not Wick" },
+  { id: "3", name: "Jack Doe", bio: "Not the star of 24" },
+];
+
+function getUsers() {
+  return users;
+}
+
+function getUserById(id) {
+  return users.find((u) => u.id === id);
+}
+
+function createUser(data) {
+  const payload = {
+    id: String(users.length + 1),
+    ...data,
+  };
+
+  users.push(payload);
+  return payload;
+}
+
+function updateUser(id, data) {
+  const index = users.findIndex((u) => u.id === id);
+  users[index] = {
+    ...users[index],
+    ...data,
+  };
+
+  return users[index];
+}
+
+function deleteUser(id) {
+  users = users.filter((u) => u.id != id);
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./database");
 
 const server = express();
 
@@ -6,6 +7,35 @@ server.use(express.json());
 
 server.get("/", (req, res) => {
   res.json({ message: "Hello from the Server!" });
+});
+
+server.get("/api/users", (req, res) => {
+  const users = db.getUsers();
+
+  if (users) {
+    res.json(users);
+  } else {
+    res.status(500).json({
+      errorMessage: "The users information could not be retrieved.",
+    });
+  }
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = db.getUserById(userId);
+
+  if (user) {
+    res.json(user);
+  } else if (!user) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved.",
+    });
+  }
 });
 
 server.listen(5000, () => {
