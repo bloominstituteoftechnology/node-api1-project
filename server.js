dcbafedb0cec22ec1b9ebf836964acb1e15787da
anthony2698 <carrilloanthony2698@gmail.com>
2020-04-13 10:38:02 -0400

    Added heroku.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
 
 const server = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`\n <=== Server listening on port ${port}! ===> \n`);
