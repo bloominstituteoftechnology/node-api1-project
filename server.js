63e68a701e34e7c70ac3e9bbb82350c71192177c
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-04 18:45:48 -0400

    Figured out how to use a try catch block to handle errors

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const server = express();
 
 server.use(express.json());
+
+// let users = [
+//   {
+//     id: shortid.generate(),
+//     name: "Jane Doe",
+//     bio: "Not Tarzan's Wife, another Jane",
+//   },
+// ];
+
+server.get("/api/users", (req, res) => {
+  // if (users.count < 1) {
+  //   res
+  //     .status(500)
+  //     .json({ errorMessage: "The users information could not be retrieved." });
+  // } else {
+  //   res.status(200).json(users);
+  // }
+  try {
+    res.status(200).json(users);
+  } catch (err) {
+    console.error("\nERROR", err);
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
+  }
+});
 
 server.get("/", (req, res) => {
   res.json({ api: "Up and running!" });
