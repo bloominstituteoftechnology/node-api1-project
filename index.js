80fad304cb1975401ca48ed4696c1a8c63af1175
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-04 13:09:49 -0700

    push and get

--- index.js
+++ index.js
@@ -2,15 +2,54 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const router = express.Router();
+const shortid = require("shortid")
 
 router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  // res.sendFile(path.join(__dirname+'/index.html'));
+  res.json({api: "running...!"});
+  res.send('welcome');
 })
 
-app.use('/', router);
+// app.use('/', router);
+app.use('/', express.json());
 app.listen(process.env.port || 1073)
 
 console.log('running at Port 1073')
+
+let users = [{
+  id: 1,
+  name: "Jane Doe",
+  bio: "Not Tarzan's wife, another Jane."
+}]
+// POST
+app.post('/api/users', (req, res) => {
+  const body = req.body;
+
+  if(!body.name || !body.bio) {
+    res.status(400).json({
+      errorMessage: 'Please provide name and bio for the user'
+    })
+  } else if (body.name && body.bio) {
+    body.id = shortid.generate();
+    users.push(body)
+     res.status(201).json(body)
+  } else {
+   res.status(500).json({errorMessage: "ERROR! cannot save user to database!"})
+  }
+})
+
+// GET
+app.get('/api/users', (req, res) => {
+  res.json(users)
+
+  if(!users) {
+    res.status(500).json({errorMessage: "The users information could not be retrieved"})
+  } else {
+    res.status(200).json();
+  }
+})
+
+
 // const http = require('http');
 // const fs = require('fs');
 
