3cd101981018f36a13a8039fa499892c08ccb7ea
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-05 07:24:26 -0400

    Get user by id works and displays all error codes appropriately. If only I could find someone to teach me instead of having to learn how to do this all by myself. Good thing I'm not paying thousands of dollars to be taught this stuff.

--- server.js
+++ server.js
@@ -5,20 +5,39 @@ const server = express();
 
 server.use(express.json());
 
-let users = [
-  {
-    id: shortid.generate(),
-    name: "Jane Doe",
-    bio: "Not Tarzan's Wife, another Jane",
-  },
-];
+// let users = [
+//   {
+//     // id: shortid.generate(),
+//     id: 1,
+//     name: "Jane Doe",
+//     bio: "Not Tarzan's Wife, another Jane",
+//   },
+// ];
 
 server.get("/api/users", (req, res) => {
   try {
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({
-      errorMessage: "There was an error while saving the user to the database",
+      errorMessage: "The users information could not be retrieved",
+    });
+  }
+});
+
+server.get("/api/users/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    user = users.filter((user) => user.id === id);
+    if (user.length > 0) {
+      res.status(200).json(user);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    }
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved",
     });
   }
 });
