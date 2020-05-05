b839d01b70390dcbb0f5a4d9ec91c5e72e4a54f5
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-05 10:55:26 -0400

    DELETE endpoint now working as it should.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// brings in mdules. we use require instead of import
 const express = require("express");
 const shortid = require("shortid");
 
+// brings in express
 const server = express();
 
+// shows express how to use JSON
 server.use(express.json());
 
+// trying a number instead of a string
+let userId = 3;
+
+// since we don't have a real db here is some starting data
 let users = [
   {
     // id: shortid.generate(),
@@ -19,6 +26,7 @@ let users = [
   },
 ];
 
+// gets all users from db. if db cannot be found returns an error in the catch.
 server.get("/api/users", (req, res) => {
   try {
     res.status(200).json(users);
@@ -29,10 +37,11 @@ server.get("/api/users", (req, res) => {
   }
 });
 
+// gets a user from db by their id. If no match for id is found then returns an error msg. If db is not found returns an error msg
 server.get("/api/users/:id", (req, res) => {
   try {
     const id = Number(req.params.id);
-    user = users.filter((user) => user.id === id);
+    let user = users.filter((user) => user.id === id);
     if (user.length > 0) {
       res.status(200).json(user);
     } else {
@@ -47,12 +56,14 @@ server.get("/api/users/:id", (req, res) => {
   }
 });
 
+// creates a new user in the db. if not enough info is provided an error msg is returned. if db cannot be found an error is returned
 server.post("/api/users", (req, res) => {
   try {
     let userInfo = {
-      id: shortid.generate(),
+      id: userId,
       ...req.body,
     };
+    userId++;
     if (userInfo.name && userInfo.bio) {
       users.push(userInfo);
       res.status(201).json(userInfo);
@@ -68,6 +79,27 @@ server.post("/api/users", (req, res) => {
   }
 });
 
+// deletes a user from the db. checks to see if the id sent matched any ids in the db. if it does then it deletes the user and responds. if not then is says user doesnt exist. If the db dousnt respond it returns a meassge that the user could not be returned.
+server.delete("/api/users/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    let user = users.filter((user) => user.id !== id);
+    if (user.length != users.length) {
+      users = user;
+      res.status(200).json(users);
+    } else {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist.",
+      });
+    }
+  } catch {
+    res.status(500).json({
+      errorMessage: "The user could not be removed",
+    });
+  }
+});
+
+// generic get request that returns the server is running
 server.get("/", (req, res) => {
   res.json({ api: "Up and running!" });
 });
