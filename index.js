00656bcdf9144d86b95efa68eac32c7ef209f285
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-05-08 19:04:04 -0700

    more stuff

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const express = require("express");
 const server = express();
 
-server.post("/users", (req, res) => {
-    const newUser = createUser({
-        name: req.body.name,
-        bio: req.body.bio
-    });
-    res.status(200).json(newUser);
-})
+server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.send("Hello World");
-});
+let users = [
+    { 
+        id: 1, 
+        name: "Jane Doe", 
+        bio: "Not Tarzan's Wife, another Jane" 
+    }
+]
 
 server.get("/api/users", (req, res) => {
-    const users = [
-        { 
-            id: 1, 
-            name: "Jane Doe", 
-            bio: "Not Tarzan's Wife, another Jane" 
-        },
-        { 
-            id: 2, 
-            name: "John Doe", 
-            bio: "Doe a Deer"
-        }
-    ]
     res.status(200).json(users);
-})
+});
 
-server.use(express.json());
+server.get("/api/users/:id", (req, res) => {
+    res.status(201).json(users);
+});
 
 server.listen(8000, () => console.log("API running on port 8000"));
