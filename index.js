b9bac3d92a4e822261a4ca2eba22da43f3a796e6
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-07-06 18:41:46 -0400

    Added get call.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// Pulling server from server.js
 const server = require("./server");
 
+// Fixed port for server to run on
 const PORT = 5000;
 
+// Listening for server
 server.listen(PORT, () => {
     console.log(`\n <== Running on port ${PORT} ==> \n`);
 });
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+// Capturing Express
 const express = require("express");
 
+// Capturing Database Access
+const db = require("./data/db");
+
+// Creating an instance of express
 const server = express();
 
+// Get call to "/"
 server.get("/", (req, res) => {
-    res.status(200).json("API Running...");
+    res.status(200).json("API Running..."); 
 });
 
+//Get call for all users
+server.get("/api/users", (req, res) => {
+    db.find()
+        .then(response => {
+            res.status(200).json({ messsage: response });
+        })
+        .catch(error => {
+            res.status(500).json({ message: error });
+        });
+});
+
+// exporting server
 module.exports = server;
