3965623997d43295a07b598e37466b66eb507361
Your Name <you@example.com>
2020-07-06 19:14:03 -0400

    put delete and post

--- server.js
+++ server.js
@@ -29,17 +29,83 @@ server.get("/users", (req, res) => { // GET gets all users
 server.get("/users/:id", (req, res) => {
     let userID = req.params.id;
 
-    users = users.filter(user => user.id === userID)
-    if (!users[0]) {
+    let user = users.filter(user => user.id === userID)
+    if (!user[0]) {
         res.status(404).json({ error: `User with id ${userID} is not found`})
-    } else if (!users) {
+    } else if (!user) {
         res.status(500).json({ errMessage: "Could not process action" })
     } else {
-        res.status(200).json(users);
+        res.status(200).json(user);
     }
     
 })
 
+server.post("/users", (req, res) => {
+    let newUser = req.body;
+    newUser.id = shortID.generate();
+    if(!newUser.name || !newUser.bio) {
+        res.status(400).json({ message: "Missing information"});
+    } else if(!newUser) {
+        res.status(500).json({ error: "Could not process request"})
+    } else {
+        users.push(newUser);
+        res.status(201).json(newUser);
+    }
+})
+
+server.delete("/users/:id", (req, res) => {
+    let userID = req.params.id;
+    let deleted = users.find(user => user.id === userID)
+
+    if(!deleted) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else if(deleted) {
+        res.status(200).json(users = users.filter(user => user.id !== userID))
+    } else {
+        res.status(500).json({ errorMessage: "The user could not be removed" })
+    }
+});
+
+// server.put("/users/:id", (req, res) => {
+//     let userID = req.params.id;
+//     let updates = req.body;
+//     let toBeUpdated = users.map(user => user.id === userID)
+
+//     if(!toBeUpdated) {
+//         res.status(404).json({ message: "The user with the specified ID does not exist." })
+//     } else if(!updates.name || !updates.bio) {
+//         res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+//     } else if (toBeUpdated) {
+//         toBeUpdated = {
+//             // ...toBeUpdated,
+//             name: req.body.name,
+//             bio: req.body.bio
+//         }
+//     } else {
+//         res.status(500).json({ errorMessage: "The user information could not be modified." })
+//     }
+
+//     res.status(200).json()
+// })
+
+server.put("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    let found = users.find(h => h.id === id);
+
+    if (!found) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else if(!changes.name || !changes.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else if(found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+    } else {
+        res.status(500).json({ errorMessage: "The user information could not be modified." })
+    }
+
+});
 
 server.get("/", (req, res) => {
     res.status(200).json({ api: "UP" })
