f82ebc05111b56d4868181e96b3d675f3c8b3bbe
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-29 18:23:37 -0400

    end points functional

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express"); // import the express package
 const server = express(); // creates the server
 
 const data = require("./database");
+const { deleteUser } = require("./database");
 
 
 // handle requests to the root of the api, the / route
@@ -15,11 +16,24 @@ const users = data.getUsers()
 res.json(users)
 });
 
+server.post("/api/users", (req, res, next) => {
+    const user = data.createUsers()
+    res.json(user)
+})
+
 server.get("/api/users/:id", (req, res, next) => {
   const user = data.getUsersById(req.params.id)
   res.json(user)
 });
 
+server.put("api/users/:id", (req, res, next)=>{
+    const user = data.updateUser(req.params.id)
+    res.json(user)
+})
+
+server.delete("api/users/:id", (req, res, next)=>{
+    deleteUser(req.params.id)
+})
 // watch for connections on port 5000
 server.listen(5000, () =>
   console.log("Server running on http://localhost:5000")
