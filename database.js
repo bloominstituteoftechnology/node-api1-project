84d82ecfe4d39a6e0e8ba3723b8b8a3895cd4b99
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-29 17:44:35 -0400

    Database added

---
+++ database.js
@@ -0,0 +1,47 @@
+let users = [
+  {
+    id: "0", // hint: use the shortid npm package to generate it
+    name: "Bob Doe", // String, required
+    bio: "Not Tarzan's Wife, another Jane", // String, required
+  },
+  {
+    id: "1", // hint: use the shortid npm package to generate it
+    name: "Jane Gorilla", // String, required
+    bio: "Not Tarzan's Wife, another Jane", // String, required
+  },
+  {
+    id: "2", // hint: use the shortid npm package to generate it
+    name: "Jane Doe", // String, required
+    bio: "Tarzan's Wife, another Jane", // String, required
+  },
+];
+
+function getUsers() {
+  return users;
+}
+
+function getUsersById(id) {
+  return users.find((u) => u.id === id);
+}
+
+function createUsers(data) {
+  const payload = {
+    id: String(users.length + 1),
+    ...data,
+  };
+  users.push(payload);
+  return payload;
+}
+
+function updateUser(id, data) {
+  const index = users.findIndex((u) => u.id === id);
+  users[index] = {
+    ...users[index],
+    ...data,
+  };
+  return users[index];
+}
+
+function deleteUser(id){
+    users = users.filter(u => u.id !== id)
+}
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express"); // import the express package
-
+const db = require('./database')
 const server = express(); // creates the server
 
 
-const users = ['one']
+const users = ['one', 'two', 'three']
 // handle requests to the root of the api, the / route
 server.get("/", (req, res) => {
   res.send("Hello from Express");
 });
 
-server.get("/api/users", (req, res) => {
-  res.send("Hello from Express", users);
+server.get("/api/users", (req, res, next) => {
+  res.send("Hello from users");
 });
 
-server.get("/api/users/:id", (req, res) => {
-  res.send("Hello from Express");
+server.get("/api/users/:id", (req, res, next) => {
+  res.send(users[req.params.id])
 });
 
 // watch for connections on port 5000
