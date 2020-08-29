2a6c08d26d0efb72eba4147a3dac435f7d51b30c
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-29 17:13:53 -0400

    Server get

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express'); // import the express package
+const express = require("express"); // import the express package
 
 const server = express(); // creates the server
 
+
+const users = ['one']
 // handle requests to the root of the api, the / route
-server.get('/', (req, res) => {
-  res.send('Hello from Express');
+server.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+server.get("/api/users", (req, res) => {
+  res.send("Hello from Express", users);
+});
+
+server.get("/api/users/:id", (req, res) => {
+  res.send("Hello from Express");
 });
 
 // watch for connections on port 5000
 server.listen(5000, () =>
-  console.log('Server running on http://localhost:5000')
+  console.log("Server running on http://localhost:5000")
 );
