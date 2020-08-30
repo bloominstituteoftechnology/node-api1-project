966e9c0e3f98445a9e74fe31e56bb54dd8750081
royeradames <royeraadames@gmail.com>
2020-08-30 17:26:19 -0400

    A Node.js and Express API that performs CRUD operations on users.

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
+	{ id: "1", name: "Jane Doe", bio:'Being a TL is fun', },
+	{ id: "2", name: "John Doe", bio:'Lambda is challenging but rewarding', },
+	{ id: "3", name: "Jack Doe", bio:'Thanks to lambda, I now move into the tech industry.', },
 ]
 
 function getUsers() {
@@ -26,7 +26,7 @@ function updateUser(id, data) {
 	const index = users.findIndex(u => u.id === id)
 	users[index] = {
 		...users[index],
-		...data,
+		...data
 	}
 	
 	return users[index]
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+
 //this is pulling the dependency from 'node_modules' now, instead of the stdlib
 const express = require('express')
 // using './' imports a local file rather than a third-party dependency 
 const db = require('./database')
+const { isString } = require('util')
 
 //creates a new express server
 const server = express()
@@ -14,13 +16,17 @@ server.get('/', (req, res) => {
     res.json({ message: 'Hello, world' }) //? why JSON (javascript object notation) instead of html? Is more consitent than html. JSON is the standard format of send data back and fort 
 })
 
-server.get('/users', (req, res) => {
+server.get('/api/users', (req, res) => {
     //gets a list of usersfrom our 'fake' database
     const users = db.getUsers()
-    res.json(users)
+    const noUsers = users === []
+    if (noUsers) res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+    })
+    else res.json(users)
 })
 
-server.get('/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     //the param variable matches up to the name of our URL param above
     const id = req.params.id
     //get a specific user by their ID from the 'fake' database
@@ -30,30 +36,78 @@ server.get('/users/:id', (req, res) => {
     //a user ID that doesn't exist in the database
     //*expect user to do crazy things
     if (user) res.json(user)
-    else res.status(404).json({ message: 'User not found' })
+    else res.status(404).json({ message: "The user with the specified ID does not exist." })
+
+    // ?If there's an error in retrieving the user from the database. How?
 })
 
-server.post('/users', (req, res) => {//*you can have the same url if they are different methods
-    const newUser = db.createUser({
-        name: req.body.name
-    })
+server.post('/api/users', (req, res) => {//*you can have the same url if they are different methods
+    const newUserData = {
+        name: String(req.body.name),
+        bio: String(req.body.bio),
+    }
+    const newUser = db.createUser(newUserData)
+    const isNameBioHere = (newUser.name === '' || newUser.bio === '') ? false : true
 
-    res.status(201).json(newUser) //* 201 means resource was succesfully created. Note browsers only make get request. You will need postman to make the post request
+    if (isNameBioHere) {
+        res.status(201).json({newUser}
+        ) //* 201 means resource was succesfully created. Note browsers only make get request. You will need postman to make the post request
+
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+        })
+    }
+    // ?If there's an error while saving the user. What does this means?
 })
 
-server.delete('/users/:id', (req, res) => {
+server.delete('/api/users/:id', (req, res) => {
     const user = db.getUserById(req.params.id)
 
-    if(user){//dont forget to think of all the possible senerios that can go wrong
+    if (user) {//dont forget to think of all the possible senerios that can go wrong
         db.deleteUser(req.params.id)
         //since we have nothing to return back to the client, send a 204 with an empty response.
         // 204 just means 'success but we have nothing to return'
-        res.status(204).end()
+        res.status(200).json({ message: 'succesfully delete user', user })
     } else {
         res.status(404).json({
             message: 'user not found',
         })
     }
+    //? If there's an error in removing the user from the database?
+})
+
+server.put('/api/users/:id', (req, res) => {
+    //check if the id is valid
+    const currentUser = db.getUserById(req.params.id)
+    if (currentUser) {
+        //put the request data for update in it's on variable
+        const newUserInformation = {
+            name: String(req.body.name),
+            bio: String(req.body.bio),
+        }
+        // check if it's data is valid string
+        const missingNameOrBio = newUserInformation.name === '' || newUserInformation.bio === ''
+
+        if (missingNameOrBio) {
+            res.status(400).json({
+                errorMessage: "Please provide name and bio for the user."
+            })
+        } else {
+            const updatedUser = db.updateUser(currentUser.id, newUserInformation)
+            res.status(200).json({currentUser, updatedUser})
+
+            // ?If there's an error when updating the user: but how?
+            // respond with HTTP status code 500.
+            // return the following JSON object: { errorMessage: "The user information could not be modified." }.
+        }
+    } else {
+        res.status(404).json({
+            message: 'User not found',
+        })
+    }
+
+
 })
 server.listen(8080, () => {//8080 is a standard to write local services
     console.log('Server started on port 8080')
