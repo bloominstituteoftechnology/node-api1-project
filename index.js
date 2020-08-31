967dd5020a893f8807fc552d256cb40f196d866d
MMendes24 <mmbusiness1@protonmail.com>
2020-08-31 15:55:33 -0400

    GET and POST completely built out.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const shortid = require('shortid')
 const express = require('express')
 const server = express()
 
@@ -5,25 +6,47 @@ server.use(express.json())
 
 let users = [
     {
-        id: new Date(),
+        id: shortid.generate(),
         name: "John Frum",
         bio: "Destined to come through for you."
     },
     {
-        id: new Date(),
+        id: shortid.generate(),
         name: "Baba Yaga",
         bio: "Hails from Russia, takes her vodka cold."
     },
     {
-        id: new Date(),
+        id: shortid.generate(),
         name: "Prestor John",
         bio: "Wisest man in Ethiopia, and the competition is pretty stiff over there."
     },
 ]
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "Hey dude, server on." })
+})
+
 server.get('/api/users', (req, res) => {
-    res.status(200).json({data: users})
+    if (users) {
+        res.status(200).json({data: users})
+    } else {
+        res.status(500).json({message: "The users information could not be retrieved."})
+    }
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if (user.bio && user.name) {
+        users.push({
+            id: shortid.generate(),
+            ...user
+        })
+        res.status(201).json({data: users})
+    } else {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    }
+
 })
 
 const port = 8000
-server.listen(port, () => console.log("Server prime, online"))
+server.listen(port, () => console.log("Server prime, online!"))
