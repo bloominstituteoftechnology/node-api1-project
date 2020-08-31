df33c190f80e3f24d9030a276a9e49c76fa07680
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-31 18:17:10 -0400

    Solved

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.get("/", (req, res) => {
 server.get("/api/users", (req, res) => {
   const users = db.getUsers();
   if (users) {
-    res.status(200).json(users);
+    res.status(200).json(users).end();
   } else {
     res
       .status(500)
@@ -25,9 +25,12 @@ server.get("/api/users/:id", (req, res) => {
   if (user) {
     res.json(user);
   } else {
-    res.status(404).json({
-      message: { message: "The user with the specified ID does not exist." },
-    });
+    res
+      .status(404)
+      .json({
+        message: { message: "The user with the specified ID does not exist." },
+      })
+      .end();
   }
 });
 
@@ -37,51 +40,60 @@ server.post("/api/users", (req, res) => {
   if (user.name === "" || user.name === "") {
     res
       .status(400)
-      .json({ errorMessage: "Please provide name and bio for the user." });
+      .json({ errorMessage: "Please provide name and bio for the user." })
+      .end();
   }
   if (user) {
     db.createUsers(user);
-    res.status(201).json(user);
+    res.status(201).json(user).end();
   } else {
-    res.status(500).json({
-      errorMessage: "There was an error while saving the user to the database",
-    });
+    res
+      .status(500)
+      .json({
+        errorMessage:
+          "There was an error while saving the user to the database",
+      })
+      .end();
   }
 });
 
-server.put("api/users/:id", (req, res) => {
-  const id = String(req.params.id)
-  const user = req.body
+server.put("/api/users/:id", (req, res) => {
+  const id = String(req.params.id);
+  const user = db.getUsersById(id);
+  const newData = req.body;
 
+  if (newData.bio === "" || newData.name === "") {
+    res
+      .status(400)
+      .json({ message: "Bad request, please fill in all thingies" }).end;
+  }
+  if (user && newData.name && newData.bio) {
+    db.updateUser(id, newData);
+    res.status(200).json(newData).end();
+  }
+  if (!user) {
+    res.status(404).json({ message: "User not found" }).end();
+  } else {
+    res.status(500).json({ message: "There was an error updating user" }).end();
+  }
+});
 
-  res.json({message: "We got a put request"});
+server.delete("/api/users/:id", (req, res) => {
+  console.log(req);
+  console.log(req.params);
+  const id = req.params.id.toString();
+  console.log(id);
+  const user = db.getUsersById(id);
   if (user) {
-    db.updateUser(id, user)
-    res.status(200).json(user)
-  } if(!user) {
+    db.deleteUser(user);
+    res.status(204).json({ message: "User successfully deleted" });
+  }
+  if (!user) {
     res.status(404).json({ message: "User not found" });
   } else {
-    res.status(500).json({message: "There was an error updating user"})
+    res.status(500).json({ message: "User unable to be deleted" });
   }
-  
-});
-
-server.delete("api/users/:id", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  // const id = req.params.id.toString();
-  // console.log(id);
-  // const user = db.getUsersById(id);
-  // if (user) {
-  //   db.deleteUser(user);
-  //   res.status(204).json({ message: "User successfully deleted" });
-  // }
-  // if (!user) {
-  //   res.status(404).json({ message: "User not found" });
-  // } else {
-  //   res.status(500).json({ message: "User unable to be deleted" });
-  // }
-res.json({message: 'Received delete request'})
+  res.json({ message: "Received delete request" });
 });
 // watch for connections on port 5000
 server.listen(5000, () =>
