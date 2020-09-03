4312b801d4b004a3dbcd412a3990608e1855d853
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-09-02 19:23:52 -0700

    all my code got messed up )-:

--- index.js
+++ index.js
@@ -11,14 +11,39 @@ let users = [
     }
 ]
 
-// START !!
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}...`);
+});
+
+// START
 
 server.get("/api/users", (req, res) => {
-    res.status(200).json(users);
+    if(!users) {
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+    } else {
+        res.status(200).json(users);
+    }
+    
+});
+
+server.post("/api/users", (req, res) => {
+    const user = req.body;
+
+    if(!username || !user.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        users.push(user);
+        res.status(201).json({ user: user })
+    }
+
+    // { errorMessage: "There was an error while saving the user to the database" }
+
 });
 
 server.get("/api/users/:id", (req, res) => {
-    res.status(201).json(users);
-});
 
-server.listen(8000, () => console.log("API running on port 8000"));
+    res.status(500).json({ errorMessage: "The users information could not be retrieved."})
+
+});
