2ee791c4e3ba0834c60f961233497c46b4cdc62b
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-06 15:43:50 -0400

    Created /api/users POST endpoint

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const shortid = require('shortid');
+const bodyParser = require("body-parser");
 const app = express();
 
 const users = [];
 
+app.use(bodyParser.json());
+
 app.get("/api/users", (req, res)=>{
-    res.send(users);
+    res.status(200).json(users);
 });
+
+app.post("/api/users", (req, res)=>{
+    const {name, bio} = req.body;
+
+    if(!name || !bio) {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."});
+    }
+
+    const newUser = {
+        id: shortid.generate(),
+        name: name,
+        bio: bio
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
 
 app.listen(5000, ()=> console.log("App is running on port 5000"));
