58724bcc4ffe1b580ca1154d2dfd8c6defac42fe
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-09 03:17:03 -0400

    removed build folder from .gitignore

---
+++ client/build/precache-manifest.3d40c83ca2d4f8a6403e2de74a9b74ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33079fed5e6b2903afb70c98a795d676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6250e190c080fb43d560",
+    "url": "/static/css/main.8c40f5b5.chunk.css"
+  },
+  {
+    "revision": "dd4ab708a1f05e9fc9bf",
+    "url": "/static/js/2.d3149bf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3149bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6250e190c080fb43d560",
+    "url": "/static/js/main.8af36a82.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
