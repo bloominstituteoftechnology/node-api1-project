a871e4032b5e33f1784556c346008f4b619dd85e
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-28 22:05:12 -0700

    get /users/:id working

--- index.js
+++ index.js
@@ -23,12 +23,9 @@ server.post('/users', (req,res) => {
     res.status(400).json({errorMessage : "Please provide name and bio for the user."})
   } else {
     users.push({ id: shortid.generate(), name: data.name, bio: data.bio })
-    res.status(201).json({ data: users})
+      ? res.status(201).json({ data: users})
+      : res.status(500).json({errorMessage: "There was an error while saving the user to the database"});
   };
-  //I assume this is not relevant, or that I'm missing something to do it:
-// If there's an error while saving the user:
-// respond with HTTP status code 500 (Server Error).
-// return the following JSON object: { errorMessage: "There was an error while saving the user to the database" }.
 });
 
 //GET /users
@@ -39,6 +36,22 @@ server.get("/users", (req, res) => {
 // respond with HTTP status code 500.
 // return the following JSON object: { errorMessage: "The users information could not be retrieved." }.
 });
+
+//GET /users/:id
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const found = users.find(u => u.id == id)
+  if(!found) {
+    res.status(404).json({message: "The user with the specified ID does not exist."})
+  } else {
+    res.status(200).json({ data: found })
+  }
+  //again this makes no sense:
+//   If there's an error in retrieving the user from the database:
+// respond with HTTP status code 500.
+// return the following JSON object: { errorMessage: "The user information could not be retrieved." }.
+})
+
 
 const port = 5000;
 server.listen(port, () => console.log("server running ..."));
