b77231f08fd5216b9af461b4a6581f58ea60cd80
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-28 22:35:58 -0700

    MVP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const shortid = require('shortid');
-console.log(shortid.generate());
+// console.log(shortid.generate());
 const server = express();
 server.use(express.json());
 
@@ -8,7 +8,7 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: "My first API"})
 })
 
-const users = [
+let users = [
   {
     id: "1", // hint: use the shortid npm package to generate it
     name: "Jane Doe", // String, required
@@ -32,6 +32,8 @@ server.post('/users', (req,res) => {
 server.get("/users", (req, res) => {
   res.status(200).json({data: users });
   //Q: when would there be an error in retreiving users?? If there are none??
+  //poss solution: if (ln33) {ln33} else {error}
+  // (ln33 || error)
 //TODO: If there's an error in retrieving the users from the database:
 // respond with HTTP status code 500.
 // return the following JSON object: { errorMessage: "The users information could not be retrieved." }.
@@ -50,6 +52,41 @@ server.get("/users/:id", (req, res) => {
 //   If there's an error in retrieving the user from the database:
 // respond with HTTP status code 500.
 // return the following JSON object: { errorMessage: "The user information could not be retrieved." }.
+})
+
+//DELETE /users/:id
+server.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const found = users.find(u => u.id == id)
+  users = users.filter(u => u.id != id)
+  if(!found) {
+    res.status(404).json({message: "The user with the specified ID does not exist."})
+  } else {
+    res.status(200).json({ data: users })
+  };
+  //again this makes no sense:
+  //   If there's an error in retrieving the user from the database:
+  // respond with HTTP status code 500.
+  // return the following JSON object: { errorMessage: "The user information could not be retrieved." }.
+});
+
+//PUT /users/"id"
+server.put("/users/:id", (req,res) => {
+  const id = req.params.id;
+  const found = users.find(u => u.id == id)
+  const changes = req.body;
+
+  if (!found) {
+    res.status(404).json({ message: "The user with the specified ID does not exist." });
+  } else {
+    if(!changes.name || !changes.bio) {
+      res.status(400).json({errorMessage : "Please provide name and bio for the user."})
+    } else {
+          Object.assign(found, changes)
+          ? res.status(200).json({ data: users })
+          : res.status(500).json({errorMessage: "The user information could not be modified."});
+    };
+}
 })
 
 
