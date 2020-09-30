5523630211d197b76732ec53c71f74b8d8d6dd00
Dodger-42 <kevin.bing.barber@gmail.com>
2020-09-29 18:57:32 -0700

    setting up

---
+++ .github/datadase.js
@@ -0,0 +1,45 @@
+let users = [
+	{ id: "1", name: "Jane Doe" },
+	{ id: "2", name: "John Doe" },
+	{ id: "3", name: "Jack Doe" },
+]
+
+function getUsers() {
+	return users
+}
+
+function getUserById(id) {
+	return users.find(u => u.id === id)
+}
+
+function createUser(data) {
+	const payload = {
+		id: String(users.length + 1),
+		...data,
+	}
+
+	users.push(payload)
+	return payload
+}
+
+function updateUser(id, data) {
+	const index = users.findIndex(u => u.id === id)
+	users[index] = {
+		...users[index],
+		...data,
+	}
+	
+	return users[index]
+}
+
+function deleteUser(id) {
+	users = users.filter(u => u.id != id)
+}
+
+module.exports = {
+	getUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+}
