dd38f40c97bd4f091361b8cd0adc4b1c0e614230
LBU <lbu@LBUui-MacBook-Pro.local>
2020-11-30 15:42:50 -0500

    second commit, get, post, delete requests working fine

--- index.js
+++ index.js
@@ -10,21 +10,71 @@ let users = [
     {   id: shortid.generate(),
         name: 'Paul',
         bio: 'Hello Earthlings'
-    }
+    },
 ]
 
 const User = {
     getAll(){
         return users
+    },
+
+    getbyId(id){
+        return users.find(u => u.id === id)
+    },
+
+    createNew(user){
+        const newUser = { id: shortid.generate(), ...user}
+        users.push(newUser)
+        return newUser
+    },
+
+    delete(id){
+        const user = users.find(u => u.id === id)
+        if(user){
+            users = users.filter(u => u.id !== id)
+        }
     }
 }
 
 // endpoints for users
-server.get('/api/users', (req,res) => {
+server.get('/api/users', (req, res) => {
     const users = User.getAll()
     res.status(200).json(users)
     if (!users){
         return res.status(500).json({ errorMessage: "There was an error" })
+    }
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const user = User.getbyId(id)
+    if (user){
+        res.status(200).json(user)
+    }
+    else{
+        res.status(404).json({ errorMessage: "user not found with id " + id})
+    }
+})
+
+server.post('/api/users', (req, res) => {
+    const userFromClient = req.body
+    if (!userFromClient.name || !userFromClient.bio){
+        res.status(400).json({ errorMessage: 'name and bio are required!' })
+    }
+    else{
+        const newlyCreatedUser = User.createNew(userFromClient)
+        res.status(201).json(newlyCreatedUser)
+    }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const deleted = User.delete(id)
+    if (deleted){
+        res.status(200).json(deleted)
+    }
+    else{
+        res.status(404).json( { errorMessage: 'user not found with id ' + id})
     }
 })
 
