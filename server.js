615cc1bd8bdf99064acf7bd471863213d9e0e86a
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-12-02 18:57:28 -0600

    Hit MVP!

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { restart } = require('nodemon')
 
 const db = require('./data')
 
@@ -8,9 +9,16 @@ server.use(express.json())
 
 
 server.get('/api/users', (req,res) => {
+
     const users = db.getUsers()
+    if(!users) {
+        return res.status(500).json({
+            message:"There was an error while retriving the users"
+        })
+    }
     res.status(200).json(users)
 })
+
 
 server.post('/api/users', (req,res) => {
     if(!req.body.name) {
@@ -23,12 +31,23 @@ server.post('/api/users', (req,res) => {
         name: req.body.name,
     })
 
-    res.json(user)
+    res.status(201).json(user)
 })
 
 server.get('/api/users/:id', (req,res) => {
  const userId = req.params.id
  const users = db.getUserById(userId)
+
+ if(!userId) {
+     return res.status(404).json({
+        message: "The user with the specified ID does not exist." 
+     })
+ }
+ if(!users) {
+     return res.status(500).json({
+        errorMessage: "The user information could not be retrieved."  
+     })
+ }
  res.status(200).json(users)
 })
 
@@ -41,8 +60,36 @@ server.put('/api/users/:id', (req,res) => {
        })
    }
 
+   if(!req.params.id) {
+       return res.status(404).json({
+        message: "The user with the specified ID does not exist."
+       })
+   }
+
+   if(!user) {
+       return res.status(500).json({
+        errorMessage: "The user information could not be modified." 
+       })
+   }
    res.json(user)
  
+})
+
+server.delete('/api/users/:id', (req,res) => {
+    const user = db.deleteUser(req.params.id)
+
+    if(!req.params.id) {
+        return res.status(404).json({
+            message: "Invalid User"
+        })
+    }
+
+    if(!user) {
+        return res.status(500).json({
+            errorMessage: "The user could not be removed" 
+        })
+    }
+    res.json(user)
 })
 
 server.listen(4000, () => {
