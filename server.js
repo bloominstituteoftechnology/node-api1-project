1d2f98ee180fbb40da393034a1b702267d502840
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-12-02 18:20:31 -0600

    post request working

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const db = require('./data')
 
 const server = express()
 
+server.use(express.json())
+
+
 server.get('/api/users', (req,res) => {
     const users = db.getUsers()
     res.status(200).json(users)
+})
+
+server.post('/api/users', (req,res) => {
+    if(!req.body.name) {
+        return res.status(400).json({
+            message: "Need A Username"
+        })
+    }
+    
+    const user = db.createUser({
+        name: req.body.name,
+    })
+
+    res.json(user)
 })
 
 server.listen(4000, () => {
