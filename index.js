51a24188d59afc079aa17f38e1ada6e77c93852b
Tevvin Strong <strong.tevvin7395@gmail.com>
2020-12-05 12:27:02 -0500

    Imported shortid and created users array of objects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Imported express
 const express = require('express');
+const shortid = require('shortid');
 
 //Connected server = express
 const server = express();
@@ -12,7 +13,31 @@ server.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`);
 })
 
-//Test get
+//Testing server workds
 server.get('/', (req, res) => {
     res.json('<h1>Hello, World</h1>');
 })
+
+//Users
+let users = [
+    {
+        "id": shortid.generate(),
+        "name": "Tevvn Strong",
+        "bio": "Coder for life MAN!"
+    },
+    {
+        "id": shortid.generate(),
+        "name": "Sean Phillips",
+        "bio": "If your not first you're last!!!!"
+    },
+    {
+        "id": shortid.generate(),
+        "name": "David Matthew",
+        "bio": "Coder or die!!!"
+    },
+    {
+        "id": shortid.generate(),
+        "name": "Conor Cameron",
+        "bio": "Surf's up DUDE!!!"
+    }
+]
