18844d1923db6720789be3a31dbcf8d6161a43bd
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-02-08 17:48:47 -0500

    Imported Model into Server

--- api/server.js
+++ api/server.js
@@ -3,4 +3,7 @@ const express = require("express")
 const server = express();
 server.use(express.json());
 
+
+let user = require('./users/model')
+
 module.exports = {server}; // EXPORT YOUR SERVER instead of {}
