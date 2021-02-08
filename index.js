35b6284c03fbfb2c79e07fd2309df33e521287f2
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-02-08 17:25:46 -0500

    Working On Index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const server = require('./api/server');
 
+
+const express = require("express")
+const app = express();
+app.use(express.json())
+
 const port = 5000;
 
+
 // START YOUR SERVER HERE
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+})
