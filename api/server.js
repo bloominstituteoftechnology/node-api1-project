c166723e531bf0383ba6cc9ec2faac35fb414e0e
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-02-08 18:13:22 -0500

    Post Completed

--- api/server.js
+++ api/server.js
@@ -4,10 +4,17 @@ const server = express();
 server.use(express.json());
 let user = require('./users/model')
 
-server.post("/server/user", (req,res)=>{
-    const {name, bio} = req.body
-    if(!name || !bio) {
+server.post("/server/user", async (req,res)=>{
+    const user = req.body
+    if(!user.name || !user.bio) {
         res.status(400).json({message: "Please provide name and bio for the user" })
+    }else {
+        try {
+            const newUser = await User.create(user)
+            res.status(201).json(user)
+        } catch (error){
+            res.status(500).json({message: "There was an error while saving the user to the database"})
+        }
     }
 })
 
