104f656ec126b2e9847fe91cdbcfa0053f4a6c87
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-02-08 17:59:42 -0500

    Post Request Made

--- api/server.js
+++ api/server.js
@@ -2,8 +2,13 @@
 const express = require("express")
 const server = express();
 server.use(express.json());
-
-
 let user = require('./users/model')
+
+server.post("/server/user", (req,res)=>{
+    const {name, bio} = req.body
+    if(!name || !bio) {
+        res.status(400).json({message: "Please provide name and bio for the user" })
+    }
+})
 
 module.exports = {server}; // EXPORT YOUR SERVER instead of {}
