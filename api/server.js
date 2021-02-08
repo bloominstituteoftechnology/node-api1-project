fb00c516bcaf41542cd4e1952dfe0bcb3a154afb
dylan-rinella <rinelladylan@aim.com>
2021-02-08 17:15:06 -0500

    mvp... GP was not helpful at all

--- api/server.js
+++ api/server.js
@@ -1,3 +1,78 @@
+const express = require('express')
+const generate = require('shortid').generate
+
+//model function import
+const dbFunctions = require('./users/model')
+
+const server = express()
+server.use(express.json())
+
+const port = 5000;
+
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// const users = [
+//   {id:generate(), name:"bob", job:'clown'},
+//   {id:generate(), name:"tom", job:'mattress salesman'},
+//   {id:generate(), name:"steve", job:'racecar driver'},
+//   {id:generate(), name:"jake", job:'alcoholic'}
+// ]
+
+server.get('/api/users', (req,res) => {
+  dbFunctions.find()
+              .then(user => {
+                res.status(200).json(user)
+              })
+              .catch(err => {
+                res.status(500).json(err.message)
+              })
+})
+
+server.get('/api/users/:id', (req,res) => {
+  const id = req.params.id
+  dbFunctions.findById(id)
+            .then(user => {
+              res.status(200).json(user)
+            })
+            .catch(err => {
+              res.status(500).json(err.message)
+            })
+})
+
+server.post('/api/users', (req,res) => {
+  dbFunctions.insert(req.body)
+              .then(user => {
+                res.status(201).json(user)
+              })
+              .catch(err => {
+                res.status(500).json(err.message)
+              })
+})
+
+server.put('/api/users/:id', async (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  if(!changes.name || !changes.bio ) {
+    res.status(400).json({message:"All fields required!"})
+  } else {
+    try {
+      const updated = await dbFunctions.update(id, changes)
+      res.status(200).json(updated)
+    } catch (error) {
+      res.status(500).json({ message:error })
+    }
+  }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  dbFunctions.remove(id)
+              .then(user => {
+                res.status(200).json(user)
+              })
+              .catch(err => {
+                res.status(500).json(err.message)
+              })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 const server = require('./api/server');
+const port = 5000
 
-const port = 5000;
+server.listen(port, ()=> {
+  console.log(`Server is running on port: ${port} `)
+})
 
-// START YOUR SERVER HERE
+// const port = 5000;
+// const generate = require('shortid').generate
+// const users = [
+//   {id:generate(), name:"bob", job:'clown'},
+//   {id:generate(), name:"tom", job:'mattress salesman'},
+//   {id:generate(), name:"steve", job:'racecar driver'},
+//   {id:generate(), name:"jake", job:'alcoholic'}
+// ]
+
+// app.get('/api/users', (req, res) => {
+//   res.status(200).json(users)
+// })
+
+// app.get('/api/users/:id', (req,res) => {
+//   const id = req.params.id
+//   const user = users.find(user => user.id === id)
+//   if(!user){
+//     res.status(404).json({message: `ID: ${id} does not exist`})
+//   }else {
+//     res.status(200).json(user)
+//   }
+// })
+
+// app.post('/api/users', (req, res) => {
+//   const {name, job} = req.body
+//   if(!name || !job) {
+//     res.status(404).json({message: "404 Page not found"})
+//   }else {
+//     res.status(200)
+//   }
+// })
+
+
+
+
+
+// app.use('*', (req, res) => { //.use covers all types of requests. get/post/etc..
+//   res.status(404).json({message: '404, Page not Found )*:'})
+// })
+
+
