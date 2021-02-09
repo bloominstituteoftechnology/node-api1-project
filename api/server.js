7ec478ce9fa30e4057ccbd00da7d5206845068b1
Francis Bonacic <francisbonacic@icloud.com>
2021-02-08 17:10:02 -0800

    still working on it

--- api/server.js
+++ api/server.js
@@ -1,3 +1,48 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+const { generate } = require('shortid');
+const dbFunctions = require('./users/model');
+console.log(dbFunctions)
+
+
+
+const app = express();
+app.use(express.json())
+
+
+    app.post("/api/users", (req,res) => {
+    console.log("this is the re", req.body)
+    const user = req.body    
+    if (!user.name || !user.bio) {
+        res.status(400).json({message:"Please provide name and bio for the user" })
+    } else {
+        dbFunctions.insert(req.body)
+        .then( user => {
+            res.status(201).json(user) //do we need to return new user with id?
+        })
+        .catch(res.status(500).json({message: "There was an error while saving the user to the database"}))
+    }
+})
+
+
+// app.get("/",(req, res) => {
+//     dbFunctions.find()
+//     .then(users => {
+//         res.json(users)
+//     })
+//     .catch(res.status(500).json({message:"error"}))
+// })
+
+
+
+
+app.use("*", (req, res) => {
+    res.status(404).json({message: "404 Not Found)*:"})
+})
+
+    // app.post("/api/users",)
+
+
+
+module.exports = {app}; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const shortid = require('shortid');
 const server = require('./api/server');
 
 const port = 5000;
 
+
 // START YOUR SERVER HERE
+
+server.app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
