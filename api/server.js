32bd0e8b34a93e24945665e9c19efecd6d492938
Diego Roman <diegormnv209@gmail.com>
2021-02-08 18:54:48 -0800

    Delete works. Still need to work on put/edit

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const { generate } = require('shortid');
 const dbFunctions = require('./users/model')
 
 const app = express();
 app.use(express.json());
+
 
 // get all users
 app.get('/users', (req, res) =>{
@@ -15,7 +15,8 @@ app.get('/users', (req, res) =>{
     .catch(() =>{
         res.status(500).json({message: 'Could not retrieve users'})
     })
-})
+});
+
 
 //get user by specific id
 app.get('/users/:id', (req, res) =>{
@@ -25,8 +26,10 @@ app.get('/users/:id', (req, res) =>{
         user ? res.status(200).json(user) : res.status(404).json({message: 'User doe not exist'})
         }
     )
-})
+});
 
+
+// add new user
 app.post('/users', async (req, res) =>{
     const {name, bio} = req.body
     if(!name || !bio){
@@ -38,13 +41,47 @@ app.post('/users', async (req, res) =>{
         })
         .catch(() =>{
             res.status(500).json({message: 'Could not add user'})
-
-        })
-        
-        
+        })   
     }
+});
+
+
+// Will edit user
+app.put('/users/:id', async(req, res) =>{
+    const {name, bio} = req.body;
+    const { id } = req.params;
+
+        if(!name || !bio){
+                res.status(400).json({message: 'Name and bio are required'})
+            } else{
+                try{
+                    dbFunctions.update(id, {name, bio})
+                    const updatedUser = await dbFunctions.update(id, {name, bio})
+                    if(updatedUser) {
+                        res.status(200).json(updatedUser)
+                    } else{
+                        res.status(404).json({message: 'User does not exist'})
+                    }
+                } catch(e){
+                    res.status(500).json({error: `${e.message}`})
+                }
+                
+            } 
 })
 
+
+//will delete User
+app.delete('/users/:id', async(req, res) =>{
+    const { id } = req.params;
+    try{
+        dbFunctions.remove(id)
+        .then((user) =>{
+            user === null ? res.status(404).json({message: 'User does not exist'}) : res.status(200).json({message: `User of id: ${id} was deleted`})
+        })
+    }   catch(e){
+        res.status(500).json({message:`Server error: ${e}`})
+    }
+})
 
 
 
