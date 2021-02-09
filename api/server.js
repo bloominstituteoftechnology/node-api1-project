808974adbb26e649a3259ce9d5c99adcf2299e96
ty <tylerpmatthews1998@gmail.com>
2021-02-09 03:24:20 -0500

    fixed, connected to model

--- api/server.js
+++ api/server.js
@@ -1,80 +1,77 @@
 // BUILD YOUR SERVER HERE
-const express = require('express');
-const generateID = require('shortid');
+const express = require("express");
+const model = require("./users/model");
+const { json } = require("express");
 
 const server = express();
 server.use(express.json());
 
-let initializeUsers = [
-{ id: generateID.generate(), name: 'Ed Carter', bio: 'hero' },
-{ id: generateID.generate(), name: 'Mary Edwards', bio: 'super hero' },
-]
 //get all users
-server.get('/api/users', (req,res)=>{
-    res.status(200).json(initializeUsers)
-})
+server.get("/api/users", (req, res) => {
+  model
+    .find()
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500), json({ message: `${err.message}` });
+    });
+});
 //get a single user by id
-server.get('/api/users/:id', (req,res)=>{
-    const id = req.params.id;
-    const user = initializeUsers.find((user)=> user.id === id);
-    if(!user){
-        res.status(404).json({message: `ID ${id} does not exist`})
-    }else{
-        res.status(200).json(user)
-    }
-})
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  model
+    .findById(id)
+    .then((user) => {
+      res.status(200).json(user);
+      console.log(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `${err.message}` });
+    });
+});
 //post a new user
-server.post('/api/users', (req,res)=>{
-    const {name,bio} = req.body;
-    if(!name || !bio){
-        res.status(404).json({message: 'Name and Bio required'})
-    }else{
-        const newUser = {id: generateID(), name, bio}
-        initializeUsers.push(newUser)
-        res.status(201).json(newUser)
-    }
-})
+server.post("/api/users", (req, res) => {
+  model
+    .insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `${err.message}` });
+    });
+});
 
 //delete a user
 
-server.delete('/api/users/:id', (req,res)=>{
-    const id = req.params.id;
-    try{
-        if(!initializeUsers.find(user=> user.id === id)){
-            res.status(404).json({message: `User with ID ${id} not found`})
-        }else{
-            initializeUsers = initializeUsers.filter(user=> user.id !== id)
-            res.status(200).json({message: `User ${id} has been deleted`})
-        }
-    }catch(e){
-        res.status(500).json({message: `Server error ${e}`})
-    }
-})
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  model
+    .remove(id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `${err.message}` });
+    });
+});
 
 //edit a user
 
-server.put('/api/users/:id', (req,res)=>{
-    const id = req.params.id;
-    const {name, bio} = req.body;
-    const indexOfUser = initializeUsers.findIndex((user=> user.id === id))
+server.put("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  model
+    .update(id, req.body)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `${err.message}` });
+    });
+});
 
-    try{
-        if(indexOfUser != -1){
-            initializeUsers[indexOfUser] = {id, name, bio}
-            res.status(200).json({id,name,bio})
-        }else{
-            res.status(404).json({message: `No user with ID ${id}`})
-        }
-    }catch(e){
-        res.status(500).json({message: `Server error ${e}`})
-    }
-})
-
-
-server.use('*', (req,res)=>{
-    res.status(404).json({messsage: '404 not found'})
-})
-
-
+server.use("*", (req, res) => {
+  res.status(404).json({ messsage: "404 not found" });
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
