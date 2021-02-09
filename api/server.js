47bb8fa9555fb26753186bbc9974ad07fc89cbeb
Diego Roman <diegormnv209@gmail.com>
2021-02-08 16:39:16 -0800

    able to get list of users and individual user

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const dbFunctions = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const app = express();
+app.use(express.json());
+
+// get all users
+app.get('/users', (req, res) =>{
+    dbFunctions.find()
+    .then((users) =>{
+        res.status(200).json(users)
+    })
+    .catch(() =>{
+        res.status(500).json({message: 'Could not retrieve users'})
+    })
+})
+
+//get user by specific id
+app.get('/users/:id', (req, res) =>{
+    const { id } = req.params
+    dbFunctions.findById(id)
+    .then((user) =>{
+        user ? res.status(200).json(user) : res.status(404).json({message: 'User doe not exist'})
+        }
+    )
+})
+
+app.post
+
+
+
+
+app.use('*', (req, res) =>{
+    res.status(404).json({message: '404 not found'});
+})
+
+module.exports = app; // EXPORT YOUR SERVER instead of {}
