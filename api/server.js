c2af063d5717038c74a1018f58422379db664968
Angelika Blackburn <esonyei@gmail.com>
2021-02-10 15:56:12 -0500

    put and delete

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ server.get("/users/:id", (req, res) => {
 		res
 			.status(404)
 			.json({ message: "The user with the specified ID does not exist" });
-	} else if (!user) {
+	} else if (user !== undefined && !user) {
 		res
 			.status(500)
 			.json({ message: "The user information could not be retrieved" });
@@ -44,8 +44,50 @@ server.post("/users", (req, res) => {
 		res
 			.status(400)
 			.json({ message: "Please provide name and bio for the user." });
-	} else {
+	} else if (newUser.name !== "" || newUser.bio !== "") {
 		res.status(201).json(newUser);
+	} else {
+		res.status(500).json({
+			message: "There was an error while saving the user to the database",
+		});
+	}
+});
+
+server.put("/users/:id", (req, res) => {
+	const user = db.findById(req.params.id);
+
+	if (user.id === undefined) {
+		res
+			.status(404)
+			.json({ message: "The user with the specified ID does not exist" });
+	} else if (user.name === "" || user.bio === "") {
+		res
+			.status(400)
+			.json({ message: "Please provide name and bio for the user" });
+	} else if (!user) {
+		res
+			.status(500)
+			.json({ message: "The user information could not be modified" });
+	} else {
+		db.update(user.id, {
+			name: req.body.name,
+			bio: req.body.bio,
+		});
+		res.status(200).json(user);
+	}
+});
+
+server.delete("users/:id", (req, res) => {
+	const user = db.findById(req.params.id);
+
+	if (user) {
+		db.remove(user.id);
+	} else if (user === undefined) {
+		res
+			.status(404)
+			.json({ message: "The user with the specified ID does not exist" });
+	} else {
+		res.status(500).json({ message: "The user could not be removed" });
 	}
 });
 
