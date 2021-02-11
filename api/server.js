f7968bfb141ed5036ae512ce3a1712ab4800b5f0
dipeshkoirala <dipeshkoirala@gmail.com>
2021-02-10 19:24:06 -0500

    server setup and libraries installed

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express=require("express")
+
+const server=express()
+
+server.use(express.json())
+
+server.get('/',(req,res)=>{
+    res.send("Hello from server")
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
