a27abb34c03ef4ab64fbf94c9c463aafb25c3aa1
IsaacGHoward <howardisaac100@gmail.com>
2021-02-10 16:59:53 -0800

    GET individual user working now

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const db = require("./users/model");
 const server = express();
 
 server.use(express.json());
-server.use(express.urlencoded());
+server.use(express.urlencoded({extended: false}));
 
 server.get("/", (req,res) => {
   res.json({message: "Hello!"});
@@ -15,6 +15,9 @@ server.get("/api/users", (req,res) => {
   db.find()
   .then((response) => {
     res.json(response);
+  })
+  .catch(() => {
+    res.status(500).json({ message: "The users information could not be retrieved" });
   })
 })
 
@@ -30,14 +33,20 @@ server.post("/api/users", (req,res) => {
       res.status(500).json({ message: "There was an error while saving the user to the database" });
     })
   }
-  
-  /*
-  const newUser = db.insert({
-    name: req.body.name,
-    bio: req.body.bio
+})
+
+server.get("/api/users/:id", (req,res) => {
+  //res.json(req.params.id);
+  db.findById(req.params.id)
+  .then((response) => {
+    if(response)
+      res.json(response);
+    else
+      res.status(404).json({ message: "The user with the specified ID does not exist" });
   })
-  res.status(201).json(newUser);
-  */
+  .catch(() => {
+    res.status(500).json({ message: "The user information could not be retrieved" });
+  })
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
