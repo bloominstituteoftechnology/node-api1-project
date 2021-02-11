1563dda709a9dbcea7dd4eeb7dfafeb315f52fc6
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-02-10 21:00:46 -0500

    still working through

--- api/server.js
+++ api/server.js
@@ -16,11 +16,26 @@ server.post("/users",(req, res)=>{
     })
     res.status(201).json(newUser)
 })
-
-
 server.get ("/users", (req, res)=>{
     const users = db.find()
-    res.json(users)
+    users.then((user)=>{
+        res.json(user) 
+    })
+    .catch(()=>{
+        res.status(500).json({message:"The user information could not be retrieved"})
+        
+    })
+})
+server.get ("/users/:id", (req, res)=>{
+    const users = db.findById(req.params.id)
+    users.then((user)=>{
+        if(req.params.id){res.json(user)}
+        else{res.status(404).json({message:"The user with the specified id does not exist"})}
+    })
+    .catch(()=>{
+        res.status(500).json({message:"The user information could not be retrieved"})
+        
+    })
 })
 
 
