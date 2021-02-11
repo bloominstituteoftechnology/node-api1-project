6e7f3bc414cbe0f7d833c9f66cc2b07ad53c1f90
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-02-10 19:45:56 -0500

    started making my bare bones server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const db = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+server.use(express.json())
+
+server.get ("/", (req,res)=>{
+    res.json({message:"hello world"})
+})
+server.post("/users",(req, res)=>{
+    const newUser = db.insert({
+        id: db.shortid,
+        name:req.body.name,
+        bio:req.body.bio,
+    })
+    res.status(201).json(newUser)
+})
+
+
+server.get ("/users", (req, res)=>{
+    const users = db.find()
+    res.json(users)
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
