f26222390644ebdbabba504623f2d37cf5da96c5
IsaacGHoward <howardisaac100@gmail.com>
2021-02-10 17:17:09 -0800

    PUT request for user by ID working now

--- api/server.js
+++ api/server.js
@@ -61,4 +61,22 @@ server.delete("/api/users/:id", (req,res) => {
   })
 })
 
+server.put("/api/users/:id", (req,res) => {
+  if(!req.body.name || !req.body.bio)
+    res.status(400).json({ message: "Please provide name and bio for the user" });
+  else{
+    db.update(req.params.id, {...req.body})
+    .then((response) => {
+      if(response)
+        res.status(200).json(response);
+      else
+        res.status(404).json({ message: "The user with the specified ID does not exist" });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The user information could not be modified" });
+    })
+  }
+  
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
