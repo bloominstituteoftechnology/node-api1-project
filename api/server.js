8be1916fcaa3459630b288e29d07b51842dcb1a6
BryanPAnderson <AndersonBryan53@gmail.com>
2021-02-13 16:06:37 -0500

    MVP

--- api/server.js
+++ api/server.js
@@ -3,11 +3,21 @@ const db = require("./users/model")
 const server = express()
 //This is installing middleware to allow Express to parse incoming JSON request bodies
 server.use(express.json())
-server.use(cors())
+//server.use(cors())
 
 server.get("/", (req, res) => {
     res.json({
         message: "Hello, World!"
+    })
+})
+
+server.get("/users", (req, res) => {
+    db.find().then(users => {
+        res.status(200).json(users)
+    }).catch(err => {
+        res.status(500).json({
+            message: "The user is not found."
+        })
     })
 })
 
@@ -28,6 +38,7 @@ server.post("/users", (req, res) => {
             })
         })
     }
+})
 
 server.get("/users", (req, res) => {
     const user = db.find()
@@ -38,38 +49,32 @@ server.get("/users", (req, res) => {
     }
 })
 
-server.get("/users/:id", (req,res) => {
-    const users = db.find()
+server.get("/users/:id", async (req,res) => {
+    const users = await db.find()
     if(!req.params.id) {
         res.status(404).json({
             message: "The user with the specified ID does not exist"
         })
     }
-    db.findById(req.params.id -1).then(user => {
-        res.status(200).json(user)
+    console.log(users)
+    db.findById(users[req.params.id -1].id).then(user => {
+        console.log(req.params.id)
+        res.status(200).json({user})
     }).catch(err => {
         res.status(500).json({
             message: "The user information could not be retrieved"
         })
     })
-
-   /* if(!user) {
-        res.status(404).json({
-            message: "The user with the specified ID does not exist"
-        })
-    } else {
-        res.json(user)
-    } */
 })
 
-server.delete("/users/:id", (req, res) => {
-    const users = db.find()
+server.delete("/users/:id", async (req, res) => {
+    const users = await db.find()
     if(!req.params.id) {
         res.status(404).json({
             message: "The user with the specified ID does not exist"
         })
     }
-    db.findById(req.params.id -1).then(user => {
+    db.findById(users[req.params.id -1].id).then(user => {
         res.status(202).json(user)
     }).catch(err => {
         res.status(500).json({
@@ -78,19 +83,21 @@ server.delete("/users/:id", (req, res) => {
     })
 })
 
-server.put("/users/:id", (req, res) => {
-    const users = db.find()
+server.put("/users/:id", async (req, res) => {
+    const users =  await db.find()
+    const updatedUser = db.update(req.body.name, req.body.bio)
     if (!req.params.id) {
         res.status(404).json({
             message: "The user with the specified ID does not exist"
         })
-    } else if (!name || !body) {
-        res.status(400).json({
-            message: "Please provide name and bio for the user"
-        })
-    } else {
-        db.update(req.body.name, req.body.bio)
     }
+    db.findById(users[req.params.id -1].id).then(user => {
+        res.status(200).json(updatedUser)
+    }).catch(err => {
+        res.status(500).json({
+            message: "The user information could not be modified"
+        })
+    })
 })
 
 module.exports = server;
