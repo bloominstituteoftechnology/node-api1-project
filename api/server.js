098afa124c75c5b13a9a2a2dc18c826110cedff8
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-08 15:29:50 -0600

    Import express and User model into server.js, create server instance and export it

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // BUILD YOUR SERVER HERE
+const express = require('express') // import express
+const User = require('./users/model.js') // import model
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express() // instance of express
+
+server.use(express.json()) // express parses body of req as JSON
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
