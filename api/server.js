ef5ee8611b7b8e0ecf8c28508cbd95627635d848
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-09 01:48:16 -0800

    GET findById user is done

--- api/server.js
+++ api/server.js
@@ -37,4 +37,19 @@ server.post('/api/users', (req, res) => {
         })
 })
 
+// GET /api/users/:id
+server.get('/api/users/:id', (req, res) => {
+    const {id} = req.params
+    User.findById(id)
+        .then(user => {
+            // findById could return null if id is not found
+            if (!user) {
+                res.status(404).json({message: "The user with the specified ID does not exist" })
+            }
+            res.json(user);
+        }).catch(err => {
+            res.status(500).json({message: "The user information could not be retrieved"})
+        })
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
