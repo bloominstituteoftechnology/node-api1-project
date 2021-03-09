e43b8df2de4ca5b24bc80b4c12cf27915690d85f
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-09 01:54:38 -0800

    DELETE user works

--- api/server.js
+++ api/server.js
@@ -52,4 +52,20 @@ server.get('/api/users/:id', (req, res) => {
         })
 })
 
+// DELETE /api/users/:id
+
+server.delete('/api/users/:id', (req,res) => {
+    const {id} = req.params
+    User.remove(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({message: "The user with the specified ID does not exist" })
+            } else {
+                res.json(user)
+            }
+        }).catch(err => {
+            res.status(500).json({message: "The user could not be removed" })
+        })
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
