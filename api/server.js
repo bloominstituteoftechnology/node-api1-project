9deabade43a1b8b0ab875413a3b443332f3ac542
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-09 01:39:17 -0800

    POST new user works

--- api/server.js
+++ api/server.js
@@ -20,4 +20,21 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+// POST /api/users
+server.post('/api/users', (req, res) => {
+
+    const newUser = req.body;
+
+    if (!newUser.name || !newUser.bio) {
+        res.status(400).json({message: "Please provide name and bio for the user" });
+    }
+
+    User.insert(newUser)
+        .then(user => {
+            res.status(201).json(user)
+        }).catch(err => {
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
