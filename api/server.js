aa009ec30bb250f469e9c4938e256501056d7d89
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-03-09 01:28:55 -0800

    GET all users done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,23 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const User = require('./users/model');
+
+const server = express();
+
+server.use(express.json());
+
+// GET /api/users
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            //console.log(users);
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The users information could not be retrieved" })
+        })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
