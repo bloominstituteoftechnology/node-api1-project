01cb37ca8baafb1ef4b4e2b93b7d10850f2ff914
Alan Mir <alanpaulphotos@gmail.com>
2021-03-10 17:59:22 -0500

    Added end point for adding users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+
+server.post("/api/users", async (req, res) => {
+    const user = req.body;
+
+    if( !user.name || !user.bio ) {
+        res.status(400).json({ message: "Please provide name and bio for the user"});
+    } else {
+
+        try {
+            const newUser = await User.insert(user);
+            res.status(201).json(newUser);
+        } catch (err) {
+            console.error({error: err});
+            res.status(500).json({ message: "There was an error while saving the user to the database" });
+        };
+    };
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
