15d3f336a9058991051ee27d2f40702f73184798
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-10 23:32:40 -0500

    Put added, MVP met

--- api/server.js
+++ api/server.js
@@ -16,14 +16,14 @@ server.get('/api/users', async (req, res) => {
 
 server.get('/api/users/:id', async (req, res) => {
     const { id } = req.params;
-    
+
     try {
         const user = await Users.findById(id);
-        if(user){
+        if (user) {
             res.status(200).json(user);
         }
         else {
-            res.status(404).json({message : "User Not Found"})
+            res.status(404).json({ message: "User Not Found" })
         }
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -32,37 +32,57 @@ server.get('/api/users/:id', async (req, res) => {
 
 server.post('/api/users/', async (req, res) => {
     const user = req.body;
-    
-    if(!user.name || !user.bio){
-        res.status(400).json({message : "Must include Name and Bio"})
+
+    if (!user.name || !user.bio) {
+        res.status(400).json({ message: "Must include Name and Bio" })
     }
-    else{
+    else {
         try {
             const newUser = await Users.insert(user);
             res.status(201).json(newUser);
         }
-        catch(err) {
-            res.status(500).json({ error : err.message })
+        catch (err) {
+            res.status(500).json({ error: err.message })
         }
     }
 });
 
 server.delete('/api/users/:id', async (req, res) => {
     const { id } = req.params;
-    try{
+    try {
         const user = await Users.remove(id);
-        if(user){
-            res.status(200).json({ message : `${id} was deleted.`})
+        if (user) {
+            res.status(200).json({ message: `${id} was deleted.` })
         }
-        else{
-            res.status(404).json({message : "User Not Found"})
+        else {
+            res.status(404).json({ message: "User Not Found" })
         }
     }
-    catch(err){
-        res.status(500).json({ message : `User could not be removed.`, error : err.message })
+    catch (err) {
+        res.status(500).json({ message: `User could not be removed.`, error: err.message })
     }
 })
 
-
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    try {
+        if (!changes.name || !changes.bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        }
+        else {
+            const user = await Users.update(id, changes);
+            if (user) {
+                res.status(200).json({ message: user})
+            }
+            else {
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+            }
+        }
+    }
+    catch(err){
+        res.status(500).json({ message: "The user information could not be modified" });
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
