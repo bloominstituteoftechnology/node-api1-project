386c1a84369d7822c1173089f0e48cd2ec0b442c
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-03-10 20:12:07 -0800

    inital setup and some additional codes

--- api/server.js
+++ api/server.js
@@ -1,3 +1,14 @@
 // BUILD YOUR SERVER HERE
+// importing the Data model to interact with the data
+const Model = require('./users/model.js');
+// bringing express into the project
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json ({ hello : 'world' });
+});
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
