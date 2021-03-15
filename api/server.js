f40b04c282af908a5ea65b4437ee8211acf0591d
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-03-15 14:17:45 -0600

    updates

--- api/server.js
+++ api/server.js
@@ -5,4 +5,15 @@ const server = express();
 
 
 
+//CRUD
+
+//| POST   | /api/users     | Creates a user using the information 
+//sent inside the `request body`.  
+
+
+
+
+
+
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
