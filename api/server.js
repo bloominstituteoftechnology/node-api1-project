6b97af0c50c8e7589b6e6177e0a1bc2d0e942b90
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-04-05 13:11:09 -0700

    server running

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
