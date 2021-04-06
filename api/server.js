07da0b7f096ec42fe4eb31e66629fa4a33782c87
Cynthia Canada <77354197+cynthicrush@users.noreply.github.com>
2021-04-05 23:56:58 -0400

    homework changes

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const user = require('./users/model');
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.use('*', (req, res) => {
+  res.status(200).json({message:'No more hello world'})
+})
+
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
