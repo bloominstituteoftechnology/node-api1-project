4deb400d238217f3ed12a91660d9a33125c3022f
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-04-07 17:07:54 -0600

    server listening in index.js. installed express. built crud on server.js. not receiving users yet.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,69 @@
-// BUILD YOUR SERVER HERE
+const express = require("express")
+const db = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.get("/api/users", (req, res) => {
+    const users = db.find()
+    res.json(users)
+})
+
+server.get("/api/users/:id", (req, res) => {
+    const user = db.findById(req.params.id)
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404).json({
+            message: "user not found"
+        })
+    }
+})
+
+server.post("/api/users", (req, res) => {
+    const newUser = db.insert({
+        id: req.body.id,
+        name: req.body.name,
+        bio: req.body.bio
+    })
+    // if(newUser) {
+    //     res.json(user)
+    // } else {
+    //     res.status(404).json({
+    //         message: "user not found"
+    //     })
+    // }
+    res.json(newUser)
+})
+
+server.delete("/api/users/:id", (req, res) => {
+    const user = db.findById(req.params.id)
+
+	if (user) {
+		db.remove(user.id)
+		res.status(204).end()
+	} else {
+		res.status(404).json({
+			message: "user not found",
+		})
+	}
+})
+
+server.put("/api/users/:id", (req, res) => {
+    const user = db.findById(req.params.id)
+
+	if (user) {
+		const updatedUser = db.update(user.id, {
+			// use a fallback value so we don't accidentally set it to empty
+			name: req.body.name || user.name,
+		})
+		res.json(updatedUser)
+	} else {
+		res.status(404).json({
+			message: "user not found",
+		})
+	}
+})
+
+module.exports = server
