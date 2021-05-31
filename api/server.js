17ccd568b5732c700ef057fb1b7f4c8fa73b937f
Kaseem Bradley <kaseembradley@Kaseems-MacBook-Air.local>
2021-05-31 19:33:09 -0400

    created put request endpoint to update user

--- api/server.js
+++ api/server.js
@@ -4,11 +4,47 @@ const User = require("./users/model");
 const server = express();
 server.use(express.json());
 
+//Updates User
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({ message: "error" });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(404).json({ message: "provide shit" });
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body);
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error updating users", err: err.message });
+  }
+});
+
+//Deletes user
+server.delete("/api/users/:id", async (req, res) => {
+  const possibleUser = await User.findById(req.params.id);
+  try {
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "not found",
+      });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error deleting users", err: err.message });
+  }
+});
+
 //Creates New User
 server.post("/api/users", (req, res) => {
   const user = req.body;
   if (!user.name || !user.bio) {
-    res.status(422).json({
+    res.status(400).json({
       message: "name and bio required",
     });
   } else {
@@ -17,7 +53,7 @@ server.post("/api/users", (req, res) => {
     });
   }
 });
-
+//Fetches all users
 server.get("/api/users", (req, res) => {
   User.find()
     .then((users) => {
@@ -29,7 +65,7 @@ server.get("/api/users", (req, res) => {
         .json({ message: "error getting users", err: err.message });
     });
 });
-
+//Fetches user by id
 server.get("/api/users/:id", (req, res) => {
   User.findById(req.params.id)
     .then((users) => {
