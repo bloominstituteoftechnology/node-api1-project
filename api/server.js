66b50b2272819707e64b25df6fb1f0bdabd06b7e
Kaseem Bradley <kaseembradley@Kaseems-MacBook-Air.local>
2021-05-31 16:19:04 -0400

    installed dependencies and created server using express

--- api/server.js
+++ api/server.js
@@ -1,3 +1,6 @@
-// BUILD YOUR SERVER HERE
+//BUILD YOUR SERVER HERE
+const express = require("express");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 5000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+  console.log("listening on", port);
+});
