56e0e295c833d35b8453de11e41de60af499ef80
Kaseem Bradley <kaseembradley@Kaseems-MacBook-Air.local>
2021-05-31 17:15:17 -0400

    created git request

--- api/server.js
+++ api/server.js
@@ -3,4 +3,12 @@ const express = require("express");
 
 const server = express();
 
+server.get("/api/users", (req, res) => {
+  res.json("users");
+});
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
