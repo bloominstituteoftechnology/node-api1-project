10a7bf8249cd6e7214093023e18a45937c47b6f9
Kaseem Bradley <kaseembradley@Kaseems-MacBook-Air.local>
2021-05-31 18:13:23 -0400

    made post request endpoint for creating a new user

--- api/server.js
+++ api/server.js
@@ -2,6 +2,21 @@
 const express = require("express");
 const User = require("./users/model");
 const server = express();
+server.use(express.json());
+
+//Creates New User
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(422).json({
+      message: "name and bio required",
+    });
+  } else {
+    User.insert(user).then((newCreatedUser) => {
+      res.status(201).json(newCreatedUser);
+    });
+  }
+});
 
 server.get("/api/users", (req, res) => {
   User.find()
@@ -18,7 +33,11 @@ server.get("/api/users", (req, res) => {
 server.get("/api/users/:id", (req, res) => {
   User.findById(req.params.id)
     .then((users) => {
-      res.json(users);
+      if (!users) {
+        res.status(404).json({ message: "user does not exist" });
+      } else {
+        res.json(users);
+      }
     })
     .catch((err) => {
       res
