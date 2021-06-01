7db0e091def4a1cc8091e42d2adb3d85ab4ac7ce
Adamh1990 <68036525+Adamh1990@users.noreply.github.com>
2021-05-31 21:43:40 -0500

    server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+module.exports = server; // EXPORT YOUR SERVER instead of {}
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const server = require('./api/server');
 
 const port = 5000;
 
-// START YOUR SERVER HERE
+console.log("Hello you");
