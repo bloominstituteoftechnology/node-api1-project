53b406c5391604c3556f12d694eee4832d7d71ea
samuel singletary <codysingletary92@gmail.com>
2021-06-01 13:25:37 -0400

    more devDependencies

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const server = require('./api/server');
+const express = require('express');
 
-const port = 5000;
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+server.get()
 
 // START YOUR SERVER HERE
+server.listen(8000, () =>{
+    console.log('API running on port 8000')
+})
+
+
+// | Method | URL            | Description                                                                                            |
+// | ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
+// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
+// | GET    | /api/users     | Returns an array users.                                                                                |
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
