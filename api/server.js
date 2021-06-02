acd52fda3e35ba9ee65a60c2427e8fc8e1a81525
nickcabuno <nickcabuno17@gmail.com>
2021-06-02 01:53:37 -0400

    api1

--- api/server.js
+++ api/server.js
@@ -1,3 +1,39 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model.js')
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>API</h>
+    `);
+  });
+
+  server.post('/api/users', (req, res) => {
+    User.insert(req.body)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: "Please provide name and bio for the user",
+                error: err.message,
+        })
+        })
+  });
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The users information could not be retrieved",
+                error: err.message,
+        })
+        })
+  });
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
