3b4177de82d1ea6ed7dfd26399834e87fb3d6adc
Lakendria <kendra939@gmail.com>
2021-06-01 21:57:18 -0400

    Created a server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,7 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const User = require("./users/model.js") 
+
+const server = express()
+module.exports = server; // EXPORT YOUR SERVER instead of {}
