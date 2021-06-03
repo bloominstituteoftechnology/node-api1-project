747818596b7e23f67d517f4a4881ea560fced90a
Molly Brown <mollybrown0516@gmail.com>
2021-06-02 21:35:18 -0400

    Reviewed the class lecture, and planned out my project in psuedocode

--- api/server.js
+++ api/server.js
@@ -1,3 +1,6 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const Users = require("./model")
+//const { destructured methods } = require("./model");
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+console.log("it works!") //testing nodemon
+
+
 const server = require('./api/server');
 
 const port = 5000;
 
 // START YOUR SERVER HERE
+/*
+server.listen(1234, ()=>{
+    console.log("Server running on 1234")
+})*/ 
+
+
+//server - has the CRUD get, post, put, delete
+//api/model.js - has the async/await (These are the 'actions' for the get, post, put, delete)
+//index.js - has just the server listener to start the server
