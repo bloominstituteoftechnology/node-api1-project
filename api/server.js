09e3e1188e6ae450f1beba38e7d0115245bd6a6d
Saeed Khan <saeedkhan@ghost.hsd1.co.comcast.net>
2021-06-02 18:01:52 -0600

    mvp complete!

--- api/server.js
+++ api/server.js
@@ -82,7 +82,11 @@ server.delete("/api/users/:id", async (req, res) =>{
     try{
         const { id } = req.params
         const deletedUser = await User.remove(id)
-        res.status(200).json(deletedUser)
+        if(!deletedUser){
+            res.status(404).json("User not found")
+        }else{
+            res.status(200).json(deletedUser)
+        }
     } catch(err){
         res.status(500).json({message: err.message})
     }
