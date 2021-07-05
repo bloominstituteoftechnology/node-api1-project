a3a2a6654e635ae4eab2f5f6cb49f4fb04f71ace
cynthiaannberg <cynthia.ann.berg@me.com>
2021-07-05 16:12:57 -0600

    node1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server');
+const server = require('./api/server')
 
-const port = 5000;
+const PORT = 5000
 
-// START YOUR SERVER HERE
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
