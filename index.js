7ee6cdcc84546a8bd3732f0001b98452c8df69bd
Stan T <stan@Stans-Mac-mini.local>
2021-07-07 11:23:54 -0700

    initial project setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
+const express = require("express");
+const generate = require("shortid").generate;
+
+const app = express();
+app.use(express.json());
 
 const port = 5000;
 
+console.log("node just run me me me");
 // START YOUR SERVER HERE
