a091808a599c92d0016d408ac8aa829e30588f72
cyamos2 <cyamos2@yahoo.com>
2021-07-07 14:02:16 -0400

    finished for now

--- api/server.js
+++ api/server.js
@@ -1,3 +1,30 @@
 // BUILD YOUR SERVER HERE
+const model = require('./users/model');
+const express = require('express');
+const server = express();
+server.use(express.json())
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res) => {
+    model.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    model.findById(id)
+    .then(user => {
+        user
+        ? res.status(200).json(user)
+        : res.status(404).json({message: `no user with id ${id}`})
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
