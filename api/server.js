80dee21307f63c4aebce40cb94e81d132ed24497
Fry-Fr <ryjlwdg89@gmail.com>
2021-07-07 02:07:43 -0400

    express server running

--- api/server.js
+++ api/server.js
@@ -1,3 +1,5 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+module.exports = server; // EXPORT YOUR SERVER instead of {}
