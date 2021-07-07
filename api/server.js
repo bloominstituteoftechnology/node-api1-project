5aa0ec5fd6e8e19b03977d2ed8c511117042fdd4
Fry-Fr <ryjlwdg89@gmail.com>
2021-07-07 02:55:05 -0400

    endpoints to get users and users/:id

--- api/server.js
+++ api/server.js
@@ -1,5 +1,32 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const model = require('./users/model');
+
 const server = express();
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+    model.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    model.findById(id)
+    .then(user => {
+        user
+        ? res.status(200).json(user)
+        : res.status(404).json({message: `no user with id ${id}`})
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
