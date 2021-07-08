2730fb7ba0dfcf78bea8b2b4929043bb2c58093c
Stan T <stan@Stans-Mac-mini.local>
2021-07-07 18:04:23 -0700

    404 npoint

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const generate = require("shortid").generate;
 const app = express();
 app.use(express.json());
 
-const port = 5000;
+const PORT = 1234;
+
+const users = () => [
+  { id: shortid.generate(), name: "Ed Carter", bio: "hero" },
+  { id: shortid.generate(), name: "Mary Edwards", bio: "super hero" },
+];
 
 console.log("node just run me me me");
 // START YOUR SERVER HERE
+app.get((req, res) => {
+  res.status("new status");
+});
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "404 Not found )*:" });
+});
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
