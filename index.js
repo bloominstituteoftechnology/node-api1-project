076dc0322688fc1d4fdf4b2e2b7999e652087b51
Stan T <stan@Stans-Mac-mini.local>
2021-07-07 18:25:00 -0700

    clean up bug

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ app.use(express.json());
 
 const PORT = 1234;
 
-const users = () => [
-  { id: shortid.generate(), name: "Ed Carter", bio: "hero" },
-  { id: shortid.generate(), name: "Mary Edwards", bio: "super hero" },
+let office = [
+  { id: generate(), name: "Ed Carter", bio: "hero" },
+  { id: generate(), name: "Mary Edwards", bio: "super hero" },
 ];
 
 const apiRequestLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 2, // limit each IP to 2 requests per windowMs
+  max: 3, // limit each IP to 3 requests per windowMs
   message: "Your limit exceeded, too many requests, please come back later",
 });
 app.use(apiRequestLimiter);
 
 console.log("node just run me me me");
+
 // START YOUR SERVER HERE
 app.get((req, res) => {
   res.status("new status");
 });
-// app.get("*", (req, res) => {
-//   res.status(404).json({ message: "404 Not found )*:" });
-// });
 
-app.get("/users");
+app.get("/users", (req, res) => {
+  res.status(200).json(office);
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "404 Not found )*:" });
+});
 
 app.listen(PORT, () => {
-  console.log(`server running on port: ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
