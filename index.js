bd9969e921c0633ffbf46df9227776e4a03ae329
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-07-15 22:40:00 -0400

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // START YOUR SERVER HERE
 server.listen(port, () => {
