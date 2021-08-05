9c6f47885e8887430aa4b585e666bdf07bd393f4
Marina Monsivais <minamonsi@me.com>
2021-08-04 21:32:17 -0500

    Edited scripts, got the server running, & imported the model file

--- api/server.js
+++ api/server.js
@@ -1,4 +1,20 @@
 // BUILD YOUR SERVER HERE
+//importing express
+const express = require("express")
+//creates the server
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const Users = require('../api/users/model')
+//global midleware
+server.use(express.json())
 
+
+
+//ENDPOINTS
+//"*" works with any request
+server.use("*", (req,res)=>{
+    res.status(404).json({message:"Not Found"})
+})
+
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const server = require('./api/server');
 
 const port = 5000;
 // START YOUR SERVER HERE
+//This is what will run your server and .listen sets your server up to listen for requests
+server.listen(5000, ()=>{
+    console.log("Server up and running on port 5000")
+})
