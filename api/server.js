9fdf2c162c5e86c55fcfccc9cb93ce978bc512f7
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-02 19:43:20 +0200

    90%, final tests

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // BUILD YOUR SERVER HERE
 
+const { json } = require('express')
 const express = require('express')
 const User = require('./users/model')
 
@@ -7,7 +8,32 @@ const server = express()
 server.use(express.json())
 
 
-server.put('/api/users/:id', )
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+              const updatedUser = await User.update(req.params.id, req.body)
+              res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            err: err.message,
+            stack: err.stack
+        })
+    }
+})
+
 
 server.delete('/api/users/:id', async (req, res) => {
     try {
