49e80b80cb8e731eb98f77c5bf564f783b373d00
nicholastorr <nick.torres213@gmail.com>
2021-09-01 21:33:17 -0400

    get all users

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const server = require('./api/server');
 
+const Users = require('./api/users/model');
+
 const port = 3000;
 
-server.get('/', (req, res) => {
+
+
+//test function
+server.get('/', (req,res) => {
     res.status(200);
     res.type('text/plain')
     res.send('hello world frome xpress')
+})
+
+
+
+
+//get all users
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(dogs => {
+        res.status(200).json(dogs);
+    })
+    .catch(error => {
+        res.status(500).json ({error: error.message});
+    })
+
 })
 
 // START YOUR SERVER HERE
