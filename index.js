a6965a60af5096a1391489d13c5c615c4ffee271
nicholastorr <nick.torres213@gmail.com>
2021-09-01 21:02:58 -0400

    set up server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const server = require('./api/server');
 
-const port = 5000;
+const port = 3000;
+
+server.get('/', (req, res) => {
+    res.status(200);
+    res.type('text/plain')
+    res.send('hello world frome xpress')
+})
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`server running on http://localhost${port}`)
+})
