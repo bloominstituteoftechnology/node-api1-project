adc0a664ae073f7f6ae7f8d164558886b08ecd35
ACarrill42 <austincarrill@gmail.com>
2021-09-01 21:36:58 -0500

    working on server.js, finished all but .put

--- api/server.js
+++ api/server.js
@@ -1,6 +1,27 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-// const User = require('./users/model');
+const User = require('./users/model');
 
-const server = express;
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', async (req,res) => {
+  const user = req.name;
+  const bio = req.bio;
+  const {id} = req.params;
+
+  if (!user || !bio) {
+    res.status(400).json({message: 'Please provide name and bio for the user'});
+  }else {
+    User.create(user,id)
+      .then(newUser => {res.status(201).json(newUser,id)})
+      .catch(error => {res.status(500).json({error: error.message})})
+  }
+});
+server.get();
+server.get();
+server.delete();
+server.put();
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
