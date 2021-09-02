4fb4cf0834cd40ec553ed6dd287c472dcd016e33
nicholastorr <nick.torres213@gmail.com>
2021-09-01 22:17:15 -0400

    put complete

--- index.js
+++ index.js
@@ -80,6 +80,23 @@ server.delete("/api/users/:id",(req,res)=>{
         })
 })
 
+//put update by id
+server.put("/api/users/:id",(req,res)=>{
+    const idVar = req.params.id
+    const updatedUser = req.body
+    Users.update(idVar,updatedUser)
+        .then(user=>{
+            if(!user){
+                res.status(404).json(`User ${idVar} does not exist`)
+            }else{
+                res.json(user)
+            }
+        })
+        .catch(err=>{
+            res.status(500).json({message:err.message})
+        })
+})
+
 
 // START YOUR SERVER HERE
 server.listen(port, () => {
