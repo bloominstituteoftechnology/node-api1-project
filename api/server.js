a76447b99770d60ef450fb41b3e891628fe2aa06
Stan <stan2dor@gmail.com>
2021-09-01 19:28:43 -0700

    CRUD - POST, 1, 2, 3, 4, 5

--- api/server.js
+++ api/server.js
@@ -1,10 +1,35 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
+const User = require("./users/model");
 
 const server = express();
 
+server.use(express.json());
+
+// CRUD Op's
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "error creating user",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
+
 server.use("*", (req, res) => {
-  res.json({ api: "running" });
+  res.status(404).json({ message: "404 Not found )*:" });
 });
 
 module.exports = server;
