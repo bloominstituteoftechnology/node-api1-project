1ba2f952327e3af3ec117f55a5520f64c321bac7
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-02 19:10:18 +0200

    12/16 completed

--- api/server.js
+++ api/server.js
@@ -7,11 +7,36 @@ const server = express()
 server.use(express.json())
 
 
+server.put('/api/users/:id', )
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        console.log('possible user', possibleUser)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            const deletedUser = await User.remove(req.params.id)
+            res.status(200).json(deletedUser)
+        }
+    } catch (err) {
+            res.status(500).json({
+                message: "The users information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+            })
+    
+    }
+})
+
+
 server.post('/api/users', (req, res) => {
     const user = req.body
     if (!user.name || !user.bio) {
-        res.status(422).json({
-            message: "Please provide name and bio for the user"
+        res.status(400).json({
+            message: "Please provide name and bio for the user",
         })
     } else {
     User.insert(user)
