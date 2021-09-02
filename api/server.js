90e4816da519203455ecf35bba170c2a902757ef
Joey Bertschler <joey.bertschler@gmail.com>
2021-09-02 16:11:49 +0200

    Fleshing

--- api/server.js
+++ api/server.js
@@ -5,7 +5,10 @@ const express = require('express')
 const server = express()
 
 
-server.get('/', )
+server.get('/api/users', (req,res) => {
+    //console.log('getting all users')
+    res.json('users')
+} )
 
 server.use('*', (req, res) => {
     res.status(404).json({
--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
+
+console.log('Initializing')
+console.log('Nodemon')
+
+server.listen(port, ()=>{
+    console.log(`listening on ${port}`)
+})
