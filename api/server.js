900078e4cba265b93173fd652610965237079785
Stan <stan2dor@gmail.com>
2021-09-01 18:52:33 -0700

    project and server setup

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use("*", (req, res) => {
+  res.json({ api: "running" });
+});
+
+module.exports = server;
