8141e6d6b094575194ce8f9fb524f0aadd499658
ACarrill42 <austincarrill@gmail.com>
2021-09-01 21:52:45 -0500

    finished server.js

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.post('/api/users', async (req,res) => {
+server.post('/api/users', (req,res) => {
   const user = req.name;
   const bio = req.bio;
   const {id} = req.params;
@@ -19,9 +19,57 @@ server.post('/api/users', async (req,res) => {
       .catch(error => {res.status(500).json({error: error.message})})
   }
 });
-server.get();
-server.get();
-server.delete();
-server.put();
+
+server.get('/api/users', (req,res) => {
+  res.status(500).json({message: 'Ths users information could not be retrieved'})
+});
+
+server.get('/api/users/:id', (req,res) => {
+  const {id} = req.params;
+
+  User.findById(id)
+    .then(user => {
+      res.status(404).json({message: `The user with the sprecified ID ${user.id} does not exist`})
+    })
+    .catch(error => {
+      res.status(500).json({message: `${error}`});
+    })
+});
+
+server.delete('/api/users/:id', (req,res) => {
+  const {id} = req.params;
+
+  User.remove(id)
+  .then(deleted => {
+    if(deleted) {
+      res.status(200).json(deleted)
+    }else{
+      res.status(404).json({message: 'ID does not exist'})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({message: `${error}`})
+  })
+});
+
+server.put('/api/users/:id', (req,res) => {
+  const {id} = req.params;
+  const updates = req.body;
+  if(!id) {
+    res.status(404).json({message: 'could not find ID'})
+  } else if(!updates.name || !updates.bio) {
+    res.status(400).json({message: 'Please provide name and bio'})
+  } else {
+      User.update(id, updates)
+    .then(update => {
+      if (update) {
+        res.status(200).json(update)
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: `${error}`})
+    })
+  }
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
