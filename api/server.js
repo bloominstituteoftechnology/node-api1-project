ab1ef7067fb011ffa4abbb32ea7346406704fa29
Rick <ricksmycodeguy@gmail.com>
2021-09-26 16:46:27 -0500

    GET /users endpoint working

--- api/server.js
+++ api/server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const User = require('./users/model.js')
 const server = express();
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'Sanity Test. You Are Sane!!' })
+  })
+
+server.get('/users', (req, res) =>{
+    User.find()
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(res.status(500), error => console.log(error));
+})
 
 module.exports = server; 
