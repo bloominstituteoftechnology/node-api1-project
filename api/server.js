524cfc39375ddef5482ce043a6ec908a56bed0c2
Rick <ricksmycodeguy@gmail.com>
2021-09-26 17:16:45 -0500

    GET users/:id is stubbed out and working

--- api/server.js
+++ api/server.js
@@ -5,14 +5,20 @@ server.use(express.json())
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'Sanity Test. You Are Sane!!' })
-  })
-
-server.get('/users', (req, res) =>{
-    User.find()
-    .then(user => {
-        res.status(200).json(user)
-    })
-    .catch(res.status(500), error => console.log(error));
 })
 
-module.exports = server; 
+server.get('/users', (req, res) => {
+    User.find()
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(res.status(500), error => console.log(error));
+})
+
+server.get('/users/:id', (req, res) => {
+    res.json({ message: 'get user by id is working' })
+})
+
+
+
+module.exports = server;
