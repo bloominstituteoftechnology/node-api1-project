da07ebe215f2410a26aea8490116c406c1f75adf
Klove Adams <kloveadams@gmail.com>
2021-09-27 17:19:57 -0400

    the put and delete tests are passing

--- api/server.js
+++ api/server.js
@@ -9,12 +9,11 @@ server.use(express.json());
 server.get("/api/users", async (req, res) => {
   try {
     const users = await User.find()
-    console.log(users)
     res.status(200).json(users)
   } catch (err) {
     res.status(500).json({
       message: err.message,
-      customMessage: "something bad happened while getting users"
+      customMessage: "The users information could not be retrieved"
     })
   }
 })
@@ -33,7 +32,7 @@ server.get("/api/users/:id", async (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: err.message,
-      customMessage: "something bad happened while getting user by id"
+      customMessage: "The user information could not be retrieved"
     })
   }
 })
@@ -53,6 +52,50 @@ server.post("/api/users", async (req, res) => {
     res.status(500).json({
       message: err.message,
       customMessage: "something bad happened while creating user"
+    })
+  }
+})
+
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    const { id } = req.params
+    if (!name || !bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user"
+      })
+    } else {
+      const updatedUser = await User.update(id, { name, bio })
+      if (!updatedUser) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist"
+        })
+      } else {
+        res.json(updatedUser)
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      customMessage: "The user information could not be modified"
+    })
+  }
+})
+
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const removeUser = await User.remove(req.params.id)
+    if (!removeUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+      res.json(removeUser)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      customMessage: "The user could not be removed"
     })
   }
 })
