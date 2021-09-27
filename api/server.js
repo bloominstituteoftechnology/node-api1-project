0bb42b6878e84ffdf0d6c13def25e537a4a04f6b
Klove Adams <kloveadams@gmail.com>
2021-09-27 16:55:39 -0400

    the post and both get tests are passing

--- api/server.js
+++ api/server.js
@@ -14,7 +14,45 @@ server.get("/api/users", async (req, res) => {
   } catch (err) {
     res.status(500).json({
       message: err.message,
-      customMessage: 'something bad happened while getting users'
+      customMessage: "something bad happened while getting users"
+    })
+  }
+})
+
+server.get("/api/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await User.findById(id)
+    if (!user) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    } else {
+      res.status(200).json(user)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      customMessage: "something bad happened while getting user by id"
+    })
+  }
+})
+
+server.post("/api/users", async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    if (!name || !bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user"
+      })
+    } else {
+      const newUser = await User.insert({ name, bio })
+      res.status(201).json(newUser)
+    }
+  }catch (err) {
+    res.status(500).json({
+      message: err.message,
+      customMessage: "something bad happened while creating user"
     })
   }
 })
