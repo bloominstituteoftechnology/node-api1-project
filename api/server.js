fdacdd367fd7d3517df3ab3decbaae9e14eaacf4
dhef415 <dhef415@gmail.com>
2021-09-27 11:44:39 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,44 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'error getting users',
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist' })
+      }
+      res.json(user)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'error getting user',
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+})
+
+server.use('*', (req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+module.exports = server
