f83c6926b48d4159e9675c7ab436f679c4a659ad
courtneyharris <harriscourtney98@gmail.com>
2021-09-27 17:41:15 -0400

    Post is done and work on Get has started

--- api/server.js
+++ api/server.js
@@ -1,14 +1,31 @@
 // BUILD YOUR SERVER HERE
 
 //Imports
-const express = require('express') 
-const Users = require('./model.js')
+const express = require('express')
+const Users = require('./users/model.js')
 
 const server = express()
 
 server.use(express.json())
 
 //Post 
-server.post
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        if (!name || !bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+        } else {
+            const newUser = await Users.insert({ name, bio })
+            res.status(201).json(newUser)
+        } 
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+            customMessage: "The users information could not be retrieved"
+        })
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
