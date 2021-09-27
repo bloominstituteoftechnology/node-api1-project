40de64f515bedf593d4a0263c22975f604e3b2a4
Klove Adams <kloveadams@gmail.com>
2021-09-27 16:27:36 -0400

    using find func to get the array of users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const User = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find()
+    console.log(users)
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      customMessage: 'something bad happened while getting users'
+    })
+  }
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require('./api/server');
 
+console.log("Inside index.js");
+
 const port = 5000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+  })
