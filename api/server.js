5c57b398c20b9a7bead936f166f52cecba214e92
Name <85176084+socket8333@users.noreply.github.com>
2021-09-27 15:14:07 -0700

    secon commit made some progress on first endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,34 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const Users = require('./users/model')
+
+//Instance of Express App
+const server = express()
+
+server.get('./', (req,res)=>{
+    console.log(`This is a ${req.method}`)
+})
+// POST   | /api/users     | Creates a user using the information sent inside the `request body`. 
+
+server.post('/api/users', async (req, res) => {
+    try {
+        const {id, name, bio} = req.body;
+        if (!name || !bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        }
+    }
+})
+
+// | GET    | /api/users     | Returns an array users.                                                                                |
+
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
+
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
+
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
+console.log(`First console log`)
+console.log(process.env.LANG) // computer stuff
+console.log(process.argv[2]) // first arg from command line
+
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
