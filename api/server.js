43837d6be6410539fa096c2ffeba12ddea947dc7
dhef415 <dhef415@gmail.com>
2021-09-27 12:05:41 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -4,6 +4,27 @@ const server = express()
 
 server.use(express.json())
 
+server.post('/api/users', (req, res) => {
+  const user = req.body
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: 'Please provide name and bio for the user',
+    })
+  } else {
+    User.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'There was an error while saving the user to the database',
+          err: err.message,
+          stack: err.stack,
+        })
+      })
+  }
+})
+
 server.get('/api/users', (req, res) => {
   User.find()
     .then((users) => {
@@ -22,9 +43,9 @@ server.get('/api/users/:id', (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
       if (!user) {
-        res
-          .status(404)
-          .json({ message: 'The user with the specified ID does not exist' })
+        res.status(404).json({
+          message: 'The user with the specified ID does not exist',
+        })
       }
       res.json(user)
     })
