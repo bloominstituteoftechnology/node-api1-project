50466eaaf4a0e98d84aa5e585981efd7f416c388
dhef415 <dhef415@gmail.com>
2021-09-27 12:55:25 -0700

    Stuck on test 15

--- api/server.js
+++ api/server.js
@@ -4,6 +4,53 @@ const server = express()
 
 server.use(express.json())
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const toUpdate = await User.findById(req.params.id)
+        if (!toUpdate) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            }) 
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body
+                    )
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The user information could not be modified',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const toBeDeleted = await User.findById(req.params.id)
+    if (!toBeDeleted) {
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist',
+        })
+    } else {
+        const deletedUser = await User.remove(toBeDeleted.id)
+        res.status(200).json(deletedUser)
+    }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The user could not be removed'
+        })
+    }
+})
+
 server.post('/api/users', (req, res) => {
   const user = req.body
   if (!user.name || !user.bio) {
