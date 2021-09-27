48d2b269e68710b146abb552529c3fe0e275c528
Rick <ricksmycodeguy@gmail.com>
2021-09-27 14:09:18 -0500

    add GET for /api/user/:id

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const server = express();
 server.use(express.json())
 
 server.get('/', (req, res) => {
+    console.log(`this is a ${req.method} request`);
     res.status(200).json({ message: 'Sanity Test. You Are Sane!!' })
 })
 
@@ -16,20 +17,42 @@ server.get('/api/users', (req, res) => {
 })
 
 server.get('/api/users/:id', (req, res) => {
-    res.json({ message: 'get user by id is working' })
+    // res.status(200).json({ message: 'user/:id working!!' })
+    console.log('this is the id', req.params.id)
+    User.findById(req.params.id)
+        .then(user => {
+            console.log(user)
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                console.log('index.js ln:27 ALERT')
+                res.status(404).json({ message: 'not found' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: err.message
+            })
+        })
 })
 
-server.post('/api/users', (req, res) =>{
-    res.json({ message: 'POST new dog working' })
-    
+server.post('/api/users', (req, res) => {
+    // res.json({ message: 'POST new dog working' })
+    const newUser = req.body
+    User.insert(newUser)
+    .then(user => {
+        res.status(201).json(user)
+    })
+
 })
 
 server.put('/api/users/:id', (req, res) => {
-    res.json({message: 'PUT update to existing user working!'})
+    res.json({ message: 'PUT update to existing user working!' })
 })
 
-server.delete('/api/users/:id', (req, res) =>{
-    res.json({message: `DELETE user with id ${req.params.id}`})
+server.delete('/api/users/:id', (req, res) => {
+    res.json({ message: `DELETE user with id ${req.params.id}` })
 })
 
 module.exports = server;
