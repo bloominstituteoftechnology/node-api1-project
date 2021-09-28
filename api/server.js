9e7bdd4f6bf7bce0bed43233fd4bb01e4f6d336e
Sebastian  Leal <sebastianleal@Sebastians-MacBook-Pro.local>
2021-09-27 17:15:54 -0700

    grabbed id and users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+const User = require('./users/model') // grab them from model
+const server = express()
+
+server.get('/api/users', (req, res) => {
+    res.json('users')
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+ 
