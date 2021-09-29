eace2a643e8f27d88876f6f74bfe0f59734460e2
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-09-29 19:43:02 -0400

    finished

--- api/server.js
+++ api/server.js
@@ -5,7 +5,22 @@ const User = require("./users/model");
 
 server.use(express.json());
 
-server.post;
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name || !newUser.bio) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and bio for the user" });
+  } else {
+    User.insert(newUser)
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message });
+      });
+  }
+});
 
 server.get("/api/users", (req, res) => {
   User.find()
@@ -39,8 +54,39 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
-// server.delete;
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await User.remove(id);
+    if (!deleteUser) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      res.status(200).json(deleteUser);
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-// server.put;
+server.put("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    if (!changes.name || !changes.bio) {
+      res.status(422).json("Name and bio required");
+    } else {
+      const updateUser = await User.update(id, changes);
+      if (!updateUser) {
+        res.status(500).json("User doesn't exist");
+      } else {
+        res.status(200).json(updateUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
