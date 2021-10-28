9f6544811f2679f57c66a51e72af657db7d41cc4
Bri <brittanymorris@MacBook-Pro.local>
2021-10-28 17:14:47 -0600

    finished the server functions.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,76 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+const users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//Creates a user using the information sent inside the `request body`.                                   |
+server.post('/api/users',(req, res) => {
+    users.insert(req.params.name, req.params.bio)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+    })
+});
+
+// Returns an array users.
+server.get('/api/users',(req,res) => {
+    users.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({ message: err.message})
+    })
+});
+
+//Returns the user object with the specified id
+server.get('/api/users/:id', (req, res) => {
+    users.findById(req.params.id)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: 'not found'})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: err.message })
+        })
+})
+
+//Removes the user with the specified `id` and returns the deleted user.
+server.delete('/api/users/:id', (req, res) => {
+    users.delete(req.params.id)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: `the user ${req.params.id} is not in the database`
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: err.message })
+        })
+})
+
+//Updates the user with the specified `id` 
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { changes } = req.body
+    try{
+        const result = await users.update(id, changes)
+        res.status(200).json(result)
+    } catch {
+        console.log(err)
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const { response } = require('./api/server');
 const server = require('./api/server');
 
-const port = 5000;
+const PORT = 5000;
 
 // START YOUR SERVER HERE
+
+
+server.get('/api/users', (req,res) => {
+    response.status(200).json()
+})
+
+server.listen(PORT, () => {
+    console.log('server is running')
+})
