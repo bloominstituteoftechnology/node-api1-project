e9c615f3322571551e385b84b2dc1da9875c2656
Greg <gbrewton1980@gmail.com>
2021-10-27 18:51:41 -0600

    build(env): port
    creates node env 'development'

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const server = require("./api/server");
+const PORT = process.env.PORT || 5280;
+console.log(process.env.PORT)
 
-server.listen(5280, () => {
-  console.log(`Server is Listening on 5280`);
+server.listen(PORT, () => {
+  console.log(`Server is Listening on ${PORT}`);
 });
