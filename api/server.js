2e3abeabff53c3b5a41cc55c8ccc42ad4982df44
Greg <gbrewton1980@gmail.com>
2021-10-27 19:10:02 -0600

    scaffolds endpoints

--- api/server.js
+++ api/server.js
@@ -1,4 +1,36 @@
 const express = require("express");
 const server = express();
 
+require("colors");
+
+server.use(express.json());
+
+// | Method | URL            | Description                                                                                            |
+// | ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
+// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
+// | GET    | /api/users     | Returns an array users.                                                                                |
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
+
+server.post("/api/users", (req, res) => {
+  res.send("POST to /api/users");
+});
+
+server.get("/api/users", (req, res) => {
+  res.send("GET to /api/users");
+});
+
+server.get("/api/users/:id", (req, res) => {
+  res.send("GET to /api/users/:id");
+});
+
+server.delete("/api/users", (req, res) => {
+  res.send("DELETE to /api/users/:id");
+});
+
+server.put("/api/users", (req, res) => {
+  res.send("PUT to /api/users/:id");
+});
+
 module.exports = server;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const server = require("./api/server");
 const PORT = process.env.PORT || 5280;
-console.log(process.env.PORT)
+console.log(process.env.PORT);
 
 server.listen(PORT, () => {
-  console.log(`Server is Listening on ${PORT}`);
+  console.log(
+    `\n** Server is Listening on port ${PORT} in ${process.env.NODE_ENV} **`
+      .america
+  );
 });
