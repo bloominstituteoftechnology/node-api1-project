bcd7ee6fe4b1439729546f0ddf3af621aea3be97
Christian <christiansmith2394@gmail.com>
2021-11-30 10:28:01 -0500

    5/16 passing

--- api/server.js
+++ api/server.js
@@ -1,3 +1,42 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+// GET ALL USERS
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then((users) => {
+        res.json(users)
+    })
+    .catch((err) => {
+        res.status(500).json({
+            message: "The users information could not be retrieved",
+            error: err.message
+        })
+    })
+})
+
+//GET USER BY ID
+
+server.get('/api/users/:id', async (req, res) => {
+    User.findById()
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.status(200).json(user)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user information could not be retrieved",
+            error: err.message
+        })
+    }
+})
+
+module.exports = server
