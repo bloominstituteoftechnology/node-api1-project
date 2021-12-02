70d458363f0415e8bfa41f748fe166d980c23d28
jose-mercado <josefmercado99@gmail.com>
2021-12-01 21:41:49 -0800

    finished

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server');
+const server = require('./api/server.js');
 
-const port = 9000;
+const port = 5000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
