b5e26511806a9fb463748e7e5207776a2153dac9
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-01 20:17:40 -0500

    added in PUT endpoint, all passing tests except Delete, but delete is working on Postman...

--- api/server.js
+++ api/server.js
@@ -58,7 +58,7 @@ server.post("/api/users", (req, res) => {
 
 })
 
-// DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user.
+// DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user. in Postman = user/id - no s after user
 server.delete("/api/user/:id", async (req, res) => {
   try{
     const {id} = req.params
@@ -74,10 +74,32 @@ server.delete("/api/user/:id", async (req, res) => {
 
 })
 
+// PUT	/api/users/:id	Updates the user with the specified id using data from the request body. Returns the modified user
+server.put("/api/users/:id", async (req, res) => {
+  const {id} = req.params;
+  const changes = req.body;
+
+  try{
+    if(!changes.name || !changes.bio){
+      res.status(400).json({message: "Please provide name and bio for the user" })
+    } else {
+      const updatedUser = await User.update(id, changes)
+      if(!updatedUser){
+        res.status(404).json({message: "The user with the specified ID does not exist"})
+      }else {
+        res.status(200).json(updatedUser)
+      }
+    }
+  } catch(err){
+    res.status(500).json({message:"The user information could not be modified"})
+  }
+})
+
+
 
 // [GET] / (Catch-All endpoint)
-server.use("*", (req, res) => {
-  res.status(404).json({ message: "404 not found" })
-})
+// server.use("*", (req, res) => {
+//   res.status(404).json({ message: "404 not found" })
+// })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
