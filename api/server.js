cc0a6053e96c8f6396a7e3b9ce3bba9b9a928581
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-12-01 23:43:24 -0600

    building server, 2 GET, one to check if server is live, one to get array of users, thena POST to add new user

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express")
+const userServer = require("./users/model")
+
+const server = express()
+
+server.use(express.json())
+
+//[POST] /api/users 
+server.post("/api/users", (req, res) => {
+	const newUser = req.body
+	userServer.insert(newUser)
+		.then(user => {
+			res.json(user)
+		})
+		.catch(err => {
+			res.status(500).json({message: err.message})
+		})
+})
+
+//[GET] /api/users
+server.get("/api/users", (req, res) => {
+	userServer.find()
+		.then(users => {
+			res.status(200).json(users)
+		})
+		.catch(err => {
+			res.status(500).json({message: err.message})
+		})
+	
+})
+
+//[GET] / makes sure server works
+server.use("*", (req, res) => {
+	res.status(404).json({message: "server is live"})
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
