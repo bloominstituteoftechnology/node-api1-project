f752233c5579df5b96933794c8d6b4aa04d2033c
tamara-taylor <ttaylor8522@gmail.com>
2021-12-02 21:00:08 -0500

    finished mvp

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server');
-
-const port = 9000;
+const server = require('./api/server.js');
+const port = 5000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
