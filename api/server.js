8d7d3d85c87c83ba12d790853027ec91cdd1f9f7
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-04 17:58:15 -0800

    got both gets and post working

--- api/server.js
+++ api/server.js
@@ -6,7 +6,34 @@ const User = require("./users/model")
 // INSTANCE OF EXPRESS
 const server = express()
 
+// GLOBAL MIDDLEWARE
+server.use(express.json())
+
 // ENDPOINTS
+// POST
+server.post('/api/users', (req, res)=>{
+    const user = req.body;
+    if (!user.name || !user.bio){
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    }else{
+        User.insert(user)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'There was an error while saving the user to the database',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+    
+})
+
+
 // GET USERS
 server.get('/api/users', (req, res)=>{
     User.find()
@@ -16,30 +43,35 @@ server.get('/api/users', (req, res)=>{
 
     .catch(err => {
         res.status(500).json({
-            message: 'error getting users',
+            message: "The users information could not be retrieved",
             err: err.message,
             stack: err.stack,
         })
     })
 })
 
-// GET USERS ??? Why is this undefined ???
+
+// GET USER ID
 server.get('/api/users/:id', (req, res)=>{
     User.findById(req.params.id)
     .then(user => {
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        }
         res.json(user)
     })
 
     .catch(err => {
         res.status(500).json({
-            message: 'error getting user',
+            message: "The user information could not be retrieved",
             err: err.message,
             stack: err.stack,
         })
     })
 })
-// GLOBAL MIDDLEWARE
-server.use(express.json())
+
 
 // ENDPOINTS
 // Test 
