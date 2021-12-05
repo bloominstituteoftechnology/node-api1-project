0fc913748a5a8ae4f94e8dfcd16011e5c0e23f26
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-12-04 16:19:41 -0800

    ran get endpoint

--- api/server.js
+++ api/server.js
@@ -6,6 +6,23 @@ const User = require("./users/model")
 // INSTANCE OF EXPRESS
 const server = express()
 
+// ENDPOINTS
+// GET
+server.get('/api/users', (req, res)=>{
+    User.find()
+    .then(users => {
+        res.json(users)    
+    })
+
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
 // GLOBAL MIDDLEWARE
 server.use(express.json())
 
