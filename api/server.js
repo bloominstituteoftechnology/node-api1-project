e9c9197769b09410d0955c69107d3c45403df7ab
LirongCui <lirongcoding@gmail.com>
2021-12-06 02:12:20 -0800

    get

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
