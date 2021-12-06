50eedf8b4658750e01b53d92f6c226cb77b24d94
LirongCui <lirongcoding@gmail.com>
2021-12-06 02:35:48 -0800

    delete

--- api/server.js
+++ api/server.js
@@ -4,6 +4,18 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.delete('/api/users/:id', async (req, res) => {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser){
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist'
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+})
+
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
