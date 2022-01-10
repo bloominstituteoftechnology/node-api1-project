f55540367dfca489c68ad502dbfb4090d75c5448
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-10 17:27:06 -0500

    get endpoint working and tested

--- api/server.js
+++ api/server.js
@@ -1,8 +1,18 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
+const Users = require("./users/model");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
