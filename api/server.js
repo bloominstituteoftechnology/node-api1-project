8e435c824748c9f150c40d13e8922ff0c6446185
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-10 18:07:04 -0500

    post request working and passing

--- api/server.js
+++ api/server.js
@@ -18,9 +18,34 @@ server.get("/api/users", async (req, res) => {
 server.get("/api/users/:id", async (req, res) => {
   try {
     const user = await Users.findById(req.params.id);
-    res.json(user);
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else {
+      res.json(user);
+    }
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res
+      .status(500)
+      .json({ message: "The user information could not be retrieved" });
+  }
+});
+
+server.post("/api/users", async (req, res) => {
+  try {
+    const newUser = await Users.insert(req.body);
+    if (!newUser.name || !newUser.bio) {
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
+    } else {
+      res.status(201).json(newUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
   }
 });
 
