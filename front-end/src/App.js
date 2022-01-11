acf4523ed628911001753e0a719a869c3040ea20
Javier Rosado <Javy@users-MacBook-Pro.local>
2022-01-10 22:35:46 -0500

    able to add user and delete user

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,66 @@
 import "./App.css";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Users from "./Users";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({
+    name: "",
+    bio: "",
+  });
 
-  axios
-    .get("http://localhost:9000/api/users")
-    .then((res) => {
-      setUsers(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
+  const handleChange = (e) => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value,
     });
+    console.log(newUser);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [users]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:9000/api/users", newUser)
+      .then((res) => {
+        setNewUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className='App'>
       <form>
-        <input placeholder='name' />
-        <input placeholder='bio' />
-        <button>Add</button>
+        <input
+          onChange={handleChange}
+          name='name'
+          value={newUser.name}
+          placeholder='name'
+        />
+        <input
+          onChange={handleChange}
+          name='bio'
+          value={newUser.bio}
+          placeholder='bio'
+        />
+        <button onClick={handleSubmit}>Add</button>
         <button>Delete</button>
       </form>
       {users.map((user) => (
-        <div>{user.name}</div>
+        <Users key={user.id} user={user} setUsers={setUsers} />
       ))}
     </div>
   );
