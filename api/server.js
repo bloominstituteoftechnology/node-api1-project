f04892486bc1f836f6cbedca916a482d42e756f9
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-12 14:50:04 -0800

    getting all users

--- api/server.js
+++ api/server.js
@@ -6,4 +6,22 @@ const server = express()
 
 server.use(express.json())
 
+server.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+server.get('/api/users', (req, res) => {
+    console.log('getting users');
+    User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack
+        })
+    })
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
