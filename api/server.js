83666e879ba359ed300378d2c9a7a9f00ab3c91b
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-12 18:49:12 -0800

    get by id and post working

--- api/server.js
+++ api/server.js
@@ -24,4 +24,44 @@ server.get('/api/users', (req, res) => {
         })
     })
 })
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        }
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack
+        })
+    })
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the use'
+        })
+    }else{
+        User.insert(user)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error creating user',
+                err: err.message,
+                stack: err.stack
+            })
+        })
+    }
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
