208ee7eb1a23e216217716837b26253b04c8f6b5
Cynthia Coronado <cynthia.softball12@gmail.com>
2022-01-12 19:05:32 -0800

    put working

--- api/server.js
+++ api/server.js
@@ -64,4 +64,30 @@ server.post('/api/users', (req, res) => {
         })
     }
 })
+
+server.put('/api/user/:id', async (req, res) => {
+    try{
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        }else{
+            if(!req.body.name || req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                })
+            }else{
+                const updatedUser = await User.insert(req.params.id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch(err){
+        res.status(500).json({
+            message: 'error with updated user',
+            err: err.message,
+            stack: err.stack
+        })
+    }
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
