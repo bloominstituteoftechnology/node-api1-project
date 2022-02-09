66e9576c94e8855269a4860a983e7da25e4b573a
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-02-09 13:30:41 -0600

    made changes to index and server in order to get server running

--- api/server.js
+++ api/server.js
@@ -1,3 +1,7 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json())
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
