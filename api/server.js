fb793283d37c6332e8c7ec7495ec581752fc75f5
Jacob Lang <jacoblang72@comcast.net>
2023-09-27 21:45:57 -0400

    done delete and post

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const Data = require("./users/model.js")
 const server = express();
 
 server.use(express.json()); 
-
+//!get
 server.get("/api/users",async(req,res)=> {
     try {
         const users = await Data.find();
@@ -26,5 +26,35 @@ server.get("/api/users/:id",async(req,res)=> {
         res.status(500).json({ message: "The user could not be removed" })
     }
 })
+//!get
+//!post
+server.post("/api/users",async(req,res)=> {
+    try {
+        const {name, bio} = req.body;
+        if (!name || !bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" });
+        } else {
+            const newUser = await Data.insert({name : name,bio :bio});
+            res.status(201).json(newUser)
+        }
+    } catch {
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
+//!post
+//!delete
+server.delete("/api/users/:id",async(req,res)=> {
+    try {
+        const {id} = req.params;
+        const deletedUser = await Data.remove(id);
+        if (!deletedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" });
+        } else {
+            res.status(200).json(deletedUser); 
+        }
+    } catch {
 
+    }
+})
+//!delete
 module.exports = server; // EXPORT YOUR SERVER instead of {}
