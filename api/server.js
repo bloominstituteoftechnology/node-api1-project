430aab644afa2739f0160ed1691f4f46247c8172
Jacob Lang <jacoblang72@comcast.net>
2023-09-27 21:30:10 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json()); 
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
